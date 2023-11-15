5f548977b42754c91be3d9ddaa07f368a71e9e50
Guillaume Desktop <robertohelper@outlook.com>
2023-11-15 17:35:08 +0100

    base for DeepestLeavesSum

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -13,7 +13,7 @@ namespace LeetCode
     {
         public static void Main()
         {
-            Runner.RunLeetCodeSolution<RichestCustomer>();
+            Runner.RunLeetCodeSolution<DeepestLeavesSum>();
         }
     }
 }
