2acf345c388d8ac506a2852fa3646625f7fd68cc
Guillaume Desktop <robertohelper@outlook.com>
2023-11-15 16:45:56 +0100

    added SmallestEvenNumber

---
+++ LeetCode/1Easy/SmallestEvenNumber.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._1Easy
+{
+    internal class SmallestEvenNumber : IRunProgram
+    {
+        private record Input(int Data, int Result);
+        public void Run()
+        {
+            Input[] inputs = new Input[] 
+            { 
+                new Input(5, 10)
+                ,new Input(6, 6)
+            };
+
+            foreach (var input in inputs)
+            {
+                Console.WriteLine(SmallestEvenMultiple(input.Data));
+                Console.WriteLine(input.Result);
+                Console.WriteLine();
+            }
+        }
+
+        public int SmallestEvenMultiple(int n)
+        {
+            return ((n & 1) + 1) * n;
+        }
+
+        public int SmallestEvenMultipleternary(int n)
+        {
+            return (n & 1) == 0 ? n : n * 2;
+        }
+    }
+}
