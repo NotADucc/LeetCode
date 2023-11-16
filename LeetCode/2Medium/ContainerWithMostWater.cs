99af5da8c4fddb63d3718cc33ce6cfdeb1c47c4a
Guillaume Desktop <robertohelper@outlook.com>
2023-11-16 22:58:29 +0100

    TLE

---
+++ LeetCode/2Medium/ContainerWithMostWater.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode._2Medium
+{
+    public class ContainerWithMostWater : IRunProgram
+    {
+        private record Input(int[] Data, int Result);
+        public void Run()
+        {
+            Input[] inputs = new Input[]
+            {
+                new Input(new int[]{ 1,8,6,2,5,4,8,3,7}, 49)
+                ,new Input(new int[]{ 1,1}, 1)
+                ,new Input(new int[]{ 1,8,6,2,5,4,8,3,8}, 56)
+                ,new Input(new int[]{ 2,3,4,5,18,17,6}, 17)
+            };
+
+            foreach (var item in inputs)
+            {
+                Console.WriteLine(MaxArea(item.Data));
+                Console.WriteLine(item.Result);
+                Console.WriteLine();
+            }
+        }
+
+        //TLE
+        public int MaxArea(int[] height)
+        {
+            int v = 0;
+            int l = height.Length;
+
+            for (int i = 0; i < l; i++)
+            {
+                for (int j = i + 1; j < l; j++)
+                {
+                    int smallerPillar = Math.Min(height[i], height[j]);
+                    v = Math.Max(smallerPillar * (j - i), v);
+                }
+            }
+
+            return v;
+        }
+
+        //TODO 2 Pointer
+    }
+}
