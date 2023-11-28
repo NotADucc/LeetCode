92cfb5ce3eb177548d21547981f970011c2c1311
Guillaume Desktop <robertohelper@outlook.com>
2023-11-28 22:54:11 +0100

    added symmetric tree

---
+++ LeetCode/1Easy/SymmetricTree.cs
@@ -0,0 +1,55 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._1Easy
+{
+    public class SymmetricTree : IRunProgram
+    {
+        private record Input(TreeNode Root, bool Result);
+        public void Run()
+        {
+            Input[] inputs = new Input[]
+            {
+                new Input(new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3))), true)
+                , new Input(new TreeNode(1, new TreeNode(2, null, new TreeNode(3)), new TreeNode(2, null, new TreeNode(3))), false)
+                , new Input(new TreeNode(1, new TreeNode(2, new TreeNode(2)), new TreeNode(2, new TreeNode(2))), false)
+            };
+            foreach (var input in inputs)
+            {
+                Console.WriteLine(IsSymmetric(input.Root));
+                Console.WriteLine(input.Result);
+                Console.WriteLine();
+            }
+        }
+        public bool IsSymmetric(TreeNode root)
+        {
+            return Helper(root.left, root.right);
+        }
+        private bool Helper(TreeNode leftNode, TreeNode rightNode)
+        {
+            if (leftNode is null || rightNode is null)
+            {
+                return leftNode == rightNode;
+            }
+
+            if (leftNode.val != rightNode.val)
+            {
+                return false;
+            }
+
+            bool b = true;
+
+            b = Helper(leftNode.left, rightNode.right);
+            if (b)
+            {
+                b = Helper(leftNode.right, rightNode.left);
+            }
+
+            return b;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -13,7 +13,7 @@ namespace LeetCode
     {
         public static void Main()
         {
-            Runner.RunLeetCodeSolution<MaxDepthBinaryTree>();
+            Runner.RunLeetCodeSolution<SymmetricTree>();
         }
     }
 }
