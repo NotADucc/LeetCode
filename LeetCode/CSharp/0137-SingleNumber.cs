c8eb3cc6126d055fca13881fb44ab51724f123cd
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 18:55:40 +0200

    feature: added 0137, can be improved

---
+++ LeetCode/CSharp/0137-SingleNumber.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0137 : IRunProgram
+    {
+        public void Run()
+        {
+            SingleNumber([2, 2, 2, 1]).Print();
+            SingleNumber([4, 1, 2, 2, 1, 1, 2]).Print();
+            SingleNumber([0, 1, 0, 1, 0, 1, 99]).Print();
+        }
+        public int SingleNumber(int[] nums)
+        {
+            return nums
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => x.Count())
+                .First(x => x.Value == 1)
+                .Key;
+        }
+    }
+}
