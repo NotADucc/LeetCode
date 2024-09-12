b83bd64a4f4a03edef085776f460a01faa4b4fde
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:12:45 +0200

    feature: added 2130

---
+++ LeetCode/CSharp/2130-MaximumTwinSumOfALinkedList.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2130 : IRunProgram
+    {
+        public void Run()
+        {
+            PairSum(ListNodeHelper.New(10)).Print();
+        }
+
+        public int PairSum(ListNode head)
+        {
+            int max = 0;
+
+            var slow = head;
+            var fast = head;
+
+            var temp = new List<int>();
+            while (fast?.next is not null)
+            {
+                temp.Add(slow.val);
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            int i = temp.Count - 1;
+            while (slow is not null)
+            {
+                max = Math.Max(max, temp[i] + slow.val);
+                slow = slow.next;
+                i--;
+            }
+
+            return max;
+        }
+    }
+}
