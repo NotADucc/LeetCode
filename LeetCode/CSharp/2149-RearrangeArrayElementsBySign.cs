8cab8321d0e004ca4d8b261a6682590aab22c6f7
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:13:31 +0200

    feature: added 2149

---
+++ LeetCode/CSharp/2149-RearrangeArrayElementsBySign.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2149 : IRunProgram
+    {
+        public void Run()
+        {
+            RearrangeArray([3, 1, -2, -5, 2, -4]).Print();
+        }
+
+        public int[] RearrangeArray(int[] nums)
+        {
+            var output = new int[nums.Length];
+
+            int pos = 0, neg = 1;
+
+            foreach (int num in nums)
+            {
+                if (num < 0)
+                {
+                    output[neg] = num;
+                    neg += 2;
+                }
+                else
+                {
+                    output[pos] = num;
+                    pos += 2;
+                }
+            }
+
+            return output;
+        }
+    }
+}
