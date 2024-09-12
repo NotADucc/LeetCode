77d7693bd2398d6745ed54bc97b1b440f2c7050d
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:15:13 +0200

    feature: added 2161

---
+++ LeetCode/CSharp/2161-PartitionArrayAccordingToGivenPivot.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2161 : IRunProgram
+    {
+        public void Run()
+        {
+            PivotArray([9, 12, 5, 10, 14, 3, 10], 10).Print();
+        }
+
+        public int[] PivotArray(int[] nums, int pivot)
+        {
+            var small = new List<int>();
+            var big = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > pivot)
+                {
+                    big.Add(nums[i]);
+                }
+                else if (nums[i] == pivot) { }
+                else
+                {
+                    small.Add(nums[i]);
+                }
+            }
+
+            small.AddRange(new int[nums.Length - (small.Count + big.Count)].Select(x => pivot));
+            small.AddRange(big);
+
+            return small.ToArray();
+        }
+    }
+}
