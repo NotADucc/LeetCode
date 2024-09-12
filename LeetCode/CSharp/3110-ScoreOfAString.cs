621cfa6182e360bca48f94df9208b5fe028b9851
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:09:56 +0200

    feature: added 3110

---
+++ LeetCode/CSharp/3110-ScoreOfAString.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution3110 : IRunProgram
+    {
+        public void Run()
+        {
+            ScoreOfString("hello").Print();
+            ScoreOfString("zaz").Print();
+        }
+
+        public int ScoreOfString(string s)
+        {
+            int score = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 >= s.Length)
+                {
+                    break;
+                }
+
+                score += Math.Abs(s[i] - s[i + 1]);
+            }
+
+            return score;
+        }
+    }
+}
