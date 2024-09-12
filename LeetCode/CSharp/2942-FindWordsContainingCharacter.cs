fe0791473ca22108f7336552a4ee8272e2f89f18
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:49:09 +0200

    feature: added 2942

---
+++ LeetCode/CSharp/2942-FindWordsContainingCharacter.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2942 : IRunProgram
+    {
+        public void Run()
+        {
+            FindWordsContaining(["leet", "code"], 'e').Print();
+        }
+
+        public IList<int> FindWordsContaining(string[] words, char x)
+        {
+            var output = new List<int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].IndexOf(x) > -1)
+                {
+                    output.Add(i);
+                }
+            }
+
+            return output;
+        }
+    }
+}
