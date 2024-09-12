8fae995fd2f876eb493c9b797b46984eb0fc58b1
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:24:05 +0200

    feature: added 0735

---
+++ LeetCode/CSharp/0735-AsteroidCollision.cs
@@ -0,0 +1,57 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0735: IRunProgram
+    {
+        public void Run()
+        {
+            AsteroidCollision([5, 10, -5]).Print();
+            AsteroidCollision([8, -8]).Print();
+        }
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (int asteroid in asteroids)
+            {
+                if (asteroid < 0)
+                {
+                    bool addAst = true;
+                    while (stack.Count > 0)
+                    {
+                        if (stack.Peek() < 0) { break; }
+                        int prev = stack.Pop();
+                        int difference = prev + asteroid;
+                        if (difference > 0)
+                        {
+                            addAst = false;
+                            stack.Push(prev);
+                            break;
+                        }
+                        else if (difference == 0)
+                        {
+                            addAst = false;
+                            break;
+                        }
+                    }
+                    if (addAst)
+                    {
+                        stack.Push(asteroid);
+                    }
+                }
+                else
+                {
+                    stack.Push(asteroid);
+                }
+            }
+            return stack.Reverse().ToArray();
+        }
+    }
+}
