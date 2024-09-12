bde6f1a616d74b7f19813a10c50aaa555eeb5b3c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:56:27 +0200

    feature: added 2610

---
+++ LeetCode/CSharp/2610-ConvertAnArrayIntoA2DArrayWithConditions.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2610 : IRunProgram
+    {
+        public void Run()
+        {
+            FindMatrix([1, 3, 4, 1, 2, 3, 1]).PrintNested();
+        }
+
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            //key is value
+            //value is index
+            var dct = new Dictionary<int, int>();
+            var output = new List<IList<int>>()
+        {
+            new List<int>(),
+        };
+            foreach (int num in nums)
+            {
+                if (!dct.ContainsKey(num))
+                {
+                    dct.Add(num, 0);
+                }
+                else
+                {
+                    dct[num]++;
+                }
+                if (dct[num] > output.Count - 1)
+                {
+                    output.Add(new List<int>());
+                }
+                output[dct[num]].Add(num);
+            }
+
+            return output;
+        }
+    }
+}
