675055fc92c4b7b04c46c006606a592afa8e1887
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:25:14 +0200

    feature: added 0231, can most likely be improved

---
+++ LeetCode/CSharp/0231-PowerOfTwo.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0231 : IRunProgram
+    {
+        public void Run()
+        {
+            IsPowerOfTwo(4).Print();
+        }
+
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n == 1)
+            {
+                return true;
+            }
+            int i = Convert.ToString(n, 2).Length - 1;
+            return ((n >> i) << i) == n && n != 0 && n != -2147483648;
+        }
+    }
+}
