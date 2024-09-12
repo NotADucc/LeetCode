83092af59d0d778d7ef82296ef4c1fcd86ae14d3
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:13:07 +0200

    feature: added 3190

---
+++ LeetCode/CSharp/3190-FindMinimumOperationsToMakeAllElementsDivisibleByThree.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution3190 : IRunProgram
+    {
+        public void Run()
+        {
+            MinimumOperations([1, 2, 3, 4]).Print();
+        }
+
+        public int MinimumOperations(int[] nums)
+        {
+            int count = 0;
+            foreach (var num in nums)
+            {
+                if (num % 3 != 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
