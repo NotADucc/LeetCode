a6d64a473b9cec96a94ce41d6291c8667d1232da
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:43:17 +0200

    feature: added 0905

---
+++ LeetCode/CSharp/0905-SortArrayByParity.cs
@@ -0,0 +1,57 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0905: IRunProgram
+    {
+        public void Run()
+        {
+            SortArrayByParity([3, 1, 2, 4]).Print();
+        }
+        public int[] SortArrayByParity(int[] nums)
+        {
+            int left = 0, right = nums.Length - 1;
+
+            while (left < right)
+            {
+                if (IsEven(nums[left]))
+                {
+                    left++;
+                }
+                else
+                {
+                    if (IsEven(nums[right]))
+                    {
+                        var temp = nums[left];
+                        nums[left] = nums[right];
+                        nums[right] = temp;
+                    }
+                }
+                if (!IsEven(nums[right]))
+                {
+                    right--;
+                }
+                else
+                {
+                    if (!IsEven(nums[left]))
+                    {
+                        var temp = nums[left];
+                        nums[left] = nums[right];
+                        nums[right] = temp;
+                    }
+                }
+            }
+
+            return nums;
+        }
+
+        public bool IsEven(int input) => (input & 1) == 0;
+    }
+}
