7bffbb33801db8c48e70b5c728ab3497d964c348
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:41:13 +0200

    feature: added 0876

---
+++ LeetCode/CSharp/0876-MiddleOfTheLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0876: IRunProgram
+    {
+        public void Run()
+        {
+            MiddleNode(ListNodeHelper.New(10)).Print();
+        }
+        public ListNode MiddleNode(ListNode head)
+        {
+            var slow = head;
+            var fast = head.next;
+
+            while (fast is not null)
+            {
+                slow = slow.next;
+                fast = fast.next?.next;
+            }
+
+            return slow;
+        }
+    }
+}
