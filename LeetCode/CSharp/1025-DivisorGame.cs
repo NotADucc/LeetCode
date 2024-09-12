dd0828181524294b148a35aa3f10259f33226f6c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:50:40 +0200

    feature: added 1025

---
+++ LeetCode/CSharp/1025-DivisorGame.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1025: IRunProgram
+    {
+        public void Run()
+        {
+            
+        }
+
+        public bool DivisorGame(int n)
+        {
+            return (n & 1) == 0;
+        }
+    }
+}
