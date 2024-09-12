5eb152be3289222eaaed8053f42cac2453028f5f
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 21:57:49 +0200

    feature: added 1332

---
+++ LeetCode/CSharp/1332-RemovePalindromicSubsequences.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1332 : IRunProgram
+    {
+        public void Run()
+        {
+            RemovePalindromeSub("ababa").Print();
+        }
+        public int RemovePalindromeSub(string s)
+        {
+            int left = 0, right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return 2;
+                }
+                left++;
+                right--;
+            }
+
+            return 1;
+        }
+    }
+}
