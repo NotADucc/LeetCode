922dd8b9a6f17d7f5f17796c612f0d6a0b055d85
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 18:37:01 +0200

    feature: added 0029, can maybe be improved or people cheesed it skewing the scores

---
+++ LeetCode/CSharp/0029-DivideTwoIntegers.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0029 : IRunProgram
+    {
+        public void Run()
+        {
+            Divide(10, 3).Print();
+            Divide(10, 2).Print();
+        }
+
+        public int Divide(int dividend, int divisor)
+        {
+            if (dividend == -2147483648 && divisor == -1) { return int.MaxValue; }
+
+            bool neg = divisor < 0 ^ dividend < 0;
+
+            long dividend_temp = Math.Abs((long)dividend);
+            if (divisor == 1 || divisor == -1) { return neg ? ~((int)dividend_temp - 1) : (int)dividend_temp; }
+
+            int output = 0;
+
+            long divisor_temp = Math.Abs((long)divisor);
+
+            while (dividend_temp >= divisor_temp)
+            {
+                output++;
+                dividend_temp -= divisor_temp;
+            }
+
+            return neg ? ~(output - 1) : output;
+        }
+    }
+}
