fbdf5324b5fe02d70b968f09997fff47791ca160
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:31:38 +0200

    feature: added 2389

---
+++ LeetCode/CSharp/2389-LongestSubsequenceWithLimitedSum.cs
@@ -0,0 +1,55 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2389 : IRunProgram
+    {
+        public void Run()
+        {
+            AnswerQueries([4, 5, 2, 1], [3, 10, 21]).Print();
+        }
+        public int[] AnswerQueries(int[] nums, int[] queries)
+        {
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                queries[i] = Helper(nums, queries[i]);
+            }
+            return queries;
+        }
+
+        private int Helper(int[] arr, int target)
+        {
+            int left = 0, right = arr.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) >> 1;
+                if (arr[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else if (arr[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return mid + 1;
+                }
+            }
+            return left;
+        }
+    }
+}
