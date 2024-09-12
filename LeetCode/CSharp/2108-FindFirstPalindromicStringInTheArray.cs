4b3bfa6786a372f270fe0f9e8a543b9e78b918f1
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:10:15 +0200

    feature: added 2108

---
+++ LeetCode/CSharp/2108-FindFirstPalindromicStringInTheArray.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2108 : IRunProgram
+    {
+        public void Run()
+        {
+            FirstPalindrome(["abc", "car", "ada", "racecar", "cool"]).Print();
+        }
+
+        public string FirstPalindrome(string[] words)
+        {
+            return words.FirstOrDefault(x => x.Equals(new string(x.Reverse().ToArray()))) ?? string.Empty;
+        }
+    }
+}
