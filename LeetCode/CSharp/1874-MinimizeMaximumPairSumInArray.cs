890bb448407ea85754a3462eb9f4a3a810cb8ad2
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:45:43 +0200

    feature: added 1877

---
+++ LeetCode/CSharp/1874-MinimizeMaximumPairSumInArray.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1877 : IRunProgram
+    {
+        public void Run()
+        {
+            MinPairSum([3, 5, 2, 3]).Print();
+        }
+
+        public int MinPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int max = int.MinValue;
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                max = Math.Max(max, nums[left] + nums[right]);
+                left++;
+                right--;
+            }
+
+
+            return max;
+        }
+    }
+}
