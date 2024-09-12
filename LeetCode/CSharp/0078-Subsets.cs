bc06cb7c2921fd142db296d5e599bdd46506cd2d
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 17:11:54 +0200

    feature: added 0078

---
+++ LeetCode/CSharp/0078-Subsets.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0078 : IRunProgram
+    {
+        public void Run()
+        {
+            Subsets([1, 2, 3]).PrintNested();
+            Subsets([0]).PrintNested();
+        }
+
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var output = new List<IList<int>>();
+
+            Helper(output, [], nums, 0);
+
+            return output;
+        }
+
+        private void Helper(List<IList<int>> output, List<int> current, int[] nums, int index)
+        {
+            output.Add(new List<int>(current));
+
+            for (int i = index; i < nums.Length; i++)
+            {
+                current.Add(nums[i]);
+                Helper(output, current, nums, i + 1);
+                current.Remove(nums[i]);
+            }
+        }
+    }
+}
