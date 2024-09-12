6178219f85e5610ea206d33fd58d4c859873b9bb
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:11:41 +0200

    feature: added 1472 && 1460

---
+++ LeetCode/CSharp/1460-MakeTwoArraysEqualByReversingSubarrays.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1460 : IRunProgram
+    {
+        public void Run()
+        {
+            CanBeEqual([1, 2, 3, 4], [2, 4, 1, 3]).Print();
+        }
+        public bool CanBeEqualCheese(int[] target, int[] arr)
+        {
+            double t = 0;
+            for (int i = 0; i < target.Length; i++)
+            {
+                t += Math.Pow(target[i], 2.1) - Math.Pow(arr[i], 2.1);
+            }
+            return (int)t == 0;
+        }
+
+        public bool CanBeEqual(int[] target, int[] arr)
+        {
+            //key is value
+            //value is freq
+            var dct = new Dictionary<int, int>();
+            foreach (var targetValue in target)
+            {
+                dct.TryAdd(targetValue, 0);
+                dct[targetValue]++;
+            }
+            foreach (var arrValue in arr)
+            {
+                if (dct.TryGetValue(arrValue, out int freq))
+                {
+                    freq--;
+                    if (freq == 0)
+                    {
+                        dct.Remove(arrValue);
+                    }
+                    else
+                    {
+                        dct[arrValue] = freq;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+            return dct.Count == 0;
+        }
+    }
+}
---
+++ LeetCode/CSharp/1472-DesignBrowserHistory.cs
@@ -0,0 +1,63 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1472 : IRunProgram
+    {
+        public void Run()
+        {
+
+        }
+    }
+    public class BrowserHistory
+    {
+
+        Stack<string> history = new Stack<string>();
+        Stack<string> future = new Stack<string>();
+        string current = "";
+        public BrowserHistory(string homepage)
+        {
+            current = homepage;
+        }
+
+        public void Visit(string url)
+        {
+            history.Push(current);
+            future.Clear();
+            current = url;
+        }
+
+        public string Back(int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                if (history.Count <= 0)
+                {
+                    break;
+                }
+                future.Push(current);
+                current = history.Pop();
+            }
+            return current;
+        }
+
+        public string Forward(int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                if (future.Count <= 0)
+                {
+                    break;
+                }
+                history.Push(current);
+                current = future.Pop();
+            }
+            return current;
+        }
+    }
+}
