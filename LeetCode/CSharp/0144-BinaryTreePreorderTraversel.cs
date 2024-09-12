3dce94dd597016c6c5f50f99a008c6715b719467
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 18:57:48 +0200

    feature: added 0144

---
+++ LeetCode/CSharp/0144-BinaryTreePreorderTraversel.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0144 : IRunProgram
+    {
+        public void Run()
+        {
+            PreorderTraversal(new TreeNode(1, new TreeNode(2), new TreeNode(3))).Print();
+        }
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var output = new List<int>();
+            Helper(output, root);
+            return output;
+        }
+
+        private void Helper(List<int> output, TreeNode root)
+        {
+            if (root is null)
+            {
+                return;
+            }
+
+            output.Add(root.val);
+            Helper(output, root.left);
+            Helper(output, root.right);
+        }
+    }
+}
