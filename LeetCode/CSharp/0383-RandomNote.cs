0eddbdad02c918232f722fe004f0b4d7f5ec1743
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:47:12 +0200

    feature: added 383

---
+++ LeetCode/CSharp/0383-RandomNote.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0383 : IRunProgram
+    {
+        public void Run()
+        {
+            CanConstruct("a", "b").Print();
+            CanConstruct("aa", "ab").Print();
+            CanConstruct("aa", "aab").Print();
+        }
+
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (magazine.Length < ransomNote.Length)
+            {
+                return false;
+            }
+
+            Span<char> ransomSpan = stackalloc char[ransomNote.Length];
+            ransomNote.AsSpan().CopyTo(ransomSpan);
+
+            Span<char> magazineSpan = stackalloc char[magazine.Length];
+            magazine.AsSpan().CopyTo(magazineSpan);
+
+            ransomSpan.Sort();
+            magazineSpan.Sort();
+
+            int ransomIndex = 0;
+            for (int i = 0; i < magazine.Length && ransomIndex < ransomSpan.Length; i++)
+            {
+                if (magazineSpan[i] == ransomSpan[ransomIndex])
+                {
+                    ransomIndex++;
+                }
+            }
+
+            return ransomIndex == ransomNote.Length;
+        }
+    }
+}
