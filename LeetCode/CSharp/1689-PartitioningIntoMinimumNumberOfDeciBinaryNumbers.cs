564a21e6ca4a21a551c40b3d368462dd2945da8c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:29:17 +0200

    feature: added 1689

---
+++ LeetCode/CSharp/1689-PartitioningIntoMinimumNumberOfDeciBinaryNumbers.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1689 : IRunProgram
+    {
+        public void Run()
+        {
+            MinPartitions("32").Print();
+        }
+
+        public int MinPartitions(string n)
+        {
+            return Convert.ToInt32(n.Max());
+        }
+    }
+}
