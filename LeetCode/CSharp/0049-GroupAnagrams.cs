c9ab404a218e5a103ecd1a24c18031c86fa72361
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 16:55:59 +0200

    feature: added 0049

---
+++ LeetCode/CSharp/0049-GroupAnagrams.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0049 : IRunProgram
+    {
+        public void Run()
+        {
+            GroupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]).PrintNested();
+            GroupAnagrams(["a"]).PrintNested();
+            GroupAnagrams([""]).PrintNested();
+        }
+
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dct = new Dictionary<int, IList<string>>();
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                var hash = HashCode.Combine(string.Concat(strs[i].OrderBy(x => x)));
+                if (!dct.TryAdd(hash, new List<string>() { strs[i] }))
+                {
+                    dct[hash].Add(strs[i]);
+                }
+            }
+
+            return dct.Values.ToList();
+        }
+    }
+}
