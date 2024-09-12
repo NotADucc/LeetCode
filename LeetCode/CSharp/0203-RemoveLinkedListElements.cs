e17e1e9854ef5e657f1968d03cccc9e0d0d58b0b
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 01:38:53 +0200

    feature: added 0203

---
+++ LeetCode/CSharp/0203-RemoveLinkedListElements.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Shared;
+using System.Data;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0203: IRunProgram
+{
+    public void Run()
+    {
+        RemoveElements(ListNodeHelper.New(1), 1).Print();
+        RemoveElements(ListNodeHelper.New(5), 1).Print();
+        RemoveElements(ListNodeHelper.New(5), 5).Print();
+    }
+
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        ListNode dummy = new ListNode(0, head);
+        ListNode current = dummy;
+
+        while (current.next is not null)
+        {
+            if (current.next.val == val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+        }
+        return dummy.next;
+    }
+}
