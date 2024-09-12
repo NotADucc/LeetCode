7e220a7fda6ef663a6f35e36b486a25ba3f3b127
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:55:58 +0200

    feature: added 1980

---
+++ LeetCode/CSharp/1980-FindUniqueBinaryString.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1980 : IRunProgram
+    {
+        public void Run()
+        {
+            FindDifferentBinaryString(["01", "10"]).Print();
+            FindDifferentBinaryString(["00", "10"]).Print();
+        }
+        public string FindDifferentBinaryString(string[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = Convert.ToInt32(nums[i], 2);
+                set.Add(num);
+            }
+
+            int output = 1;
+            for (int i = 0; i < nums.Length * nums.Length; i++)
+            {
+                if (!set.Contains(i))
+                {
+                    output = i;
+                    break;
+                }
+            }
+
+            string s = Convert.ToString(output, 2);
+
+            return new string('0', nums.Length - s.Length) + s;
+        }
+    }
+}
