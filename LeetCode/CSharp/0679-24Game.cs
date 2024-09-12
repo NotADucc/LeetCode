1c75fa0c15b5bb76a11a297f99325ef4619934d3
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:18:04 +0200

    feature: added 0679

---
+++ LeetCode/CSharp/0679-24Game.cs
@@ -0,0 +1,72 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0679 : IRunProgram
+    {
+        public void Run()
+        {
+            JudgePoint24([4, 1, 8, 7]).Print();
+            JudgePoint24([1, 2, 1, 2]).Print();
+        }
+        public bool JudgePoint24(int[] cards)
+        {
+            bool found = false;
+            Helper(ref found, cards.ToList(), 0);
+            return found;
+        }
+
+        const int TARGET = 24;
+        private void Helper(ref bool found, List<int> cards, double curr)
+        {
+            if (cards.Count == 0 && Math.Round(curr, MidpointRounding.AwayFromZero) == TARGET)
+            {
+                found = true;
+            }
+            else
+            {
+                if (found) return;
+                for (int i = 0; i < cards.Count; i++)
+                {
+                    int card_val = cards[i];
+                    cards.Remove(card_val);
+
+                    Helper(ref found, cards, curr + card_val);
+                    Helper(ref found, cards, curr - card_val);
+                    if (curr != 0)
+                    {
+                        Helper(ref found, cards, curr / card_val);
+                        Helper(ref found, cards, curr * card_val);
+                        Helper(ref found, cards, card_val / curr);
+                    }
+
+                    if (i < cards.Count)
+                    {
+                        int next_card_val = cards[i];
+
+                        cards.Remove(next_card_val);
+
+                        Helper(ref found, cards, curr * (card_val + next_card_val));
+                        if (card_val - next_card_val != 0)
+                        {
+                            Helper(ref found, cards, curr / (card_val - next_card_val));
+                            Helper(ref found, cards, curr * (card_val - next_card_val));
+                            Helper(ref found, cards, (card_val - next_card_val) / curr);
+                        }
+
+                        cards.Insert(i, next_card_val);
+                    }
+
+                    cards.Insert(i, card_val);
+                }
+            }
+        }
+    }
+}
