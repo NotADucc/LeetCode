de71d08c27efb4d92e2640f7250725213468b846
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:24:38 +0200

    feature: added 2243

---
+++ LeetCode/CSharp/2243-CalculateDigitSumOfAString.cs
@@ -0,0 +1,51 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2243 : IRunProgram
+    {
+        public void Run()
+        {
+            DigitSum("11111222223", 3).Print();
+        }
+
+        public string DigitSum(string s, int k)
+        {
+            while (s.Length > k)
+            {
+                int k_count = s.Length / k;
+                k_count = s.Length % k == 0 ? k_count : k_count + 1;
+
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < k_count - 1; i++)
+                {
+                    Helper(s.AsSpan().Slice(i * k, k), sb);
+                }
+
+                Helper(s.AsSpan().Slice((k_count - 1) * k), sb);
+
+                s = sb.ToString();
+            }
+
+            return s;
+        }
+
+        private void Helper(ReadOnlySpan<char> num, StringBuilder sb)
+        {
+            int temp = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                temp += num[i] - '0';
+            }
+            sb.Append(temp);
+        }
+    }
+}
