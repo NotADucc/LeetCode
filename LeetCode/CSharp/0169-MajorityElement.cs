fbc3effab53f49bf7e92be0f885b532f0ae44034
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:04:58 +0200

    feature: added 0169

---
+++ LeetCode/CSharp/0169-MajorityElement.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0169 : IRunProgram
+    {
+        public void Run()
+        {
+            MajorityElement([3, 2, 3]).Print();
+            MajorityElement([2, 2, 1, 1, 1, 2, 2]).Print();
+        }
+        public int MajorityElement(int[] nums)
+        {
+            Array.Sort(nums);
+            int t = nums[0];
+            int c = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (t - nums[i] == 0)
+                {
+                    c++;
+                    if (c > nums.Length / 2)
+                    {
+                        return nums[i];
+                    }
+                    continue;
+                }
+                c = 1;
+                t = nums[i];
+            }
+            return t;
+        }
+    }
+}
