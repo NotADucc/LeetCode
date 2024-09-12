675a6d80a4c6410a562e73ea866be80603f159b3
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:53:35 +0200

    feature: added 0442

---
+++ LeetCode/CSharp/0442-FindAllDuplicatesInAnArray.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0442 : IRunProgram
+    {
+        public void Run()
+        {
+            FindDuplicates([4, 3, 2, 7, 8, 2, 3, 1]).Print();
+            FindDuplicates([1, 1, 2]).Print();
+        }
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            var counter = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if (!counter.ContainsKey(num))
+                {
+                    counter.Add(num, 0);
+                }
+                counter[num]++;
+            }
+
+            return counter.Where(x => x.Value > 1).Select(x => x.Key).ToList();
+        }
+    }
+}
