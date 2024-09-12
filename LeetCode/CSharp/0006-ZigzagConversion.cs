1449908fde957827f4361a53e45fc661f01840be
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 16:16:15 +0200

    feature: added 0006

---
+++ LeetCode/CSharp/0006-ZigzagConversion.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0006 : IRunProgram
+    {
+        public void Run()
+        {
+            Convert("PAYPALISHIRING", 3).Print();
+            Convert("PAYPALISHIRING", 4).Print();
+        }
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1) { return s; }
+            int numRows_2 = (numRows - 1) << 1;
+            var output = new StringBuilder[numRows];
+            for (int i = 0; i < numRows; i++) { output[i] = new StringBuilder(); }
+            for (int i = 0; i < s.Length; i++)
+            {
+                int rem = i % numRows_2;
+                if (rem < numRows)
+                {
+                    output[rem].Append(s[i]);
+                }
+                else
+                {
+                    output[numRows_2 - rem].Append(s[i]);
+                }
+            }
+
+            return string.Concat(output.Select(x => x.ToString()));
+        }
+    }
+}
