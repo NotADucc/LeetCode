53680f72eae2a187d7cd270ff595f2a90be7f236
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:21:34 +0200

    feature: added 0709

---
+++ LeetCode/CSharp/0709-ToLowerCase.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0709: IRunProgram
+    {
+        public void Run()
+        {
+            ToLowerCase("AAAAA").Print();
+        }
+        public string ToLowerCase(string s)
+        {
+            return s.ToLower();
+        }
+    }
+}
