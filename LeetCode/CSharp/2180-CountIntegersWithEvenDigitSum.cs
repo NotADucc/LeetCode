63b2605289d7455e55154f4bb8056282ff9576ef
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:18:15 +0200

    feature: added 2180

---
+++ LeetCode/CSharp/2180-CountIntegersWithEvenDigitSum.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2180 : IRunProgram
+    {
+        public void Run()
+        {
+            CountEven(4).Print();
+            CountEven(30).Print();
+        }
+
+        public int CountEven(int num)
+        {
+            int ans = 0;
+            int temp_num = num;
+            while (num > 0)
+            {
+                ans += num % 10;
+                num /= 10;
+            }
+
+            return (temp_num - ((ans & 1) == 1 ? 1 : 0)) >> 1;
+        }
+    }
+}
