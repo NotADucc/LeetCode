36e33bc24e2c95059eb75b93f40806f8a335d4a0
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:55:30 +0200

    feature: added 2582

---
+++ LeetCode/CSharp/2582-PassThePillow.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2582 : IRunProgram
+    {
+        public void Run()
+        {
+            PassThePillow(2, 90).Print();
+        }
+
+        public int PassThePillow(int n, int time)
+        {
+            int new_n = n - 1;
+            int full_passes = time / new_n;
+
+            return IsEven(full_passes)
+                ? (time % (new_n * 2)) + 1
+                : n - (time % new_n);
+        }
+
+        private bool IsEven(int input) => (input & 1) == 0;
+    }
+}
