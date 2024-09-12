3d7de490314d82075e3506704af8f1876b543d79
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:33:32 +0200

    feature: added 0257

---
+++ LeetCode/CSharp/0257-BinaryTreePaths.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0257 : IRunProgram
+    {
+        public void Run()
+        {
+            BinaryTreePaths(new TreeNode(1)).Print();
+            BinaryTreePaths(new TreeNode(1, new TreeNode(2), new TreeNode(3))).Print();
+        }
+
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            List<string> paths = new List<string>();
+
+            Helper(paths, new StringBuilder(), root);
+
+            return paths;
+        }
+        private void Helper(List<string> output, StringBuilder sb, TreeNode? root)
+        {
+            if (root is null) { return; }
+
+            ReadOnlySpan<char> val = sb.Length == 0 ? root.val.ToString() : $"->{root.val}";
+            sb.Append(val);
+
+            if (root.left is null && root.right is null)
+            {
+                output.Add(sb.ToString());
+            }
+            else
+            {
+                Helper(output, sb, root.left);
+                Helper(output, sb, root.right);
+            }
+
+            sb.Remove(sb.Length - val.Length, val.Length);
+        }
+    }
+}
