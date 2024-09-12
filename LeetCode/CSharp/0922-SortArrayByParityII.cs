5420d72deccd2898468a6be05850bc78c26ecead
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:44:02 +0200

    feature: added 0922

---
+++ LeetCode/CSharp/0922-SortArrayByParityII.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0922: IRunProgram
+    {
+        public void Run()
+        {
+            SortArrayByParityII([4, 2, 5, 7]).Print();
+        }
+        public int[] SortArrayByParityII(int[] nums)
+        {
+            int even = 0, odd = 1;
+            var output = new int[nums.Length];
+            foreach (var num in nums)
+            {
+                if (IsEven(num))
+                {
+                    output[even] = num;
+                    even += 2;
+                }
+                else
+                {
+                    output[odd] = num;
+                    odd += 2;
+                }
+            }
+
+            return output;
+        }
+
+        public bool IsEven(int input) => (input & 1) == 0;
+    }
+}
