e293d66f8d4aac199969edc76ccf8b50e276960b
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 16:34:28 +0200

    feature: added 0026

---
+++ LeetCode/CSharp/0026-RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0026: IRunProgram
+    {
+        public void Run()
+        {
+            RemoveDuplicates([1, 1, 2]).Print();
+            RemoveDuplicates([1, 1, 1, 2]).Print();
+            RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]).Print();
+        }
+
+        public int RemoveDuplicates(int[] nums)
+        {
+            int trueIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[trueIndex] == nums[i])
+                {
+                    continue;
+                }
+                trueIndex++;
+                nums[trueIndex] = nums[i];
+            }
+            trueIndex++;
+            return trueIndex;
+        }
+    }
+}
