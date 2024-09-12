23082d1d72458ecaf7aa5ad56406dcd070ddfd1d
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:53:48 +0200

    feature: added 1963

---
+++ LeetCode/CSharp/1963-MinimumNumberOfSwapsToMakeTheStringBalanced.cs
@@ -0,0 +1,53 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1963 : IRunProgram
+    {
+        public void Run()
+        {
+            MinSwaps("][][").Print();
+            MinSwaps("]]][[[").Print();
+        }
+        public unsafe int MinSwaps(string s)
+        {
+            Span<char> sSpan;
+            fixed (char* ptr = s)
+            {
+                sSpan = new Span<char>(ptr, s.Length);
+            }
+
+            int evenDelta = 0, left = 0, right = sSpan.Length - 1;
+            int output = 0;
+            while (left < right)
+            {
+                evenDelta = sSpan[left] == ']' ? evenDelta + 1 : evenDelta - 1;
+                if (evenDelta > 0)
+                {
+                    output++;
+                    while (left < right)
+                    {
+                        if (sSpan[right] == '[')
+                        {
+                            sSpan[right] = ']';
+                            right--;
+                            break;
+                        }
+                        right--;
+                    }
+                    evenDelta -= 2;
+                }
+                left++;
+            }
+
+            return output;
+        }
+    }
+}
