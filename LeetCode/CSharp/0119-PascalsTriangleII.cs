d4151fbcae9c44995e7211c763e4f883aefcad1e
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 18:49:20 +0200

    feature: added 0119

---
+++ LeetCode/CSharp/0119-PascalsTriangleII.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0119 : IRunProgram
+    {
+        public void Run()
+        {
+            GetRow(5).Print();
+            GetRow(1).Print();
+        }
+
+        public IList<int> GetRow(int rowIndex)
+        {
+            rowIndex++;
+            var output = new List<IList<int>>(rowIndex)
+            {
+                new List<int>() { 1 }
+            };
+            for (int i = 1; i < rowIndex; i++)
+            {
+                var temp = new List<int>();
+                temp.Add(1);
+                for (int j = 1; j < i; j++)
+                {
+                    temp.Add(output[i - 1][j - 1] + output[i - 1][j]);
+                }
+                temp.Add(1);
+                output.Add(temp);
+            }
+            return output[^1];
+        }
+    }
+}
