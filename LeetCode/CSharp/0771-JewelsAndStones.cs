7e934865c76c854716392d7a8a4025c1339dddec
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:26:49 +0200

    feature: added 0771

---
+++ LeetCode/CSharp/0771-JewelsAndStones.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0771: IRunProgram
+    {
+        public void Run()
+        {
+            NumJewelsInStones("aA", "aAAbbbb").Print();
+        }
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int cnt = 0;
+            Dictionary<char, int> dct = new Dictionary<char, int>();
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (!dct.ContainsKey(stones[i]))
+                {
+                    dct.Add(stones[i], 1);
+                    continue;
+                }
+                dct[stones[i]] += 1;
+            }
+            for (int i = 0; i < jewels.Length; i++)
+            {
+                if (dct.ContainsKey(jewels[i]))
+                {
+                    cnt += dct[jewels[i]];
+                }
+            }
+            return cnt;
+        }
+    }
+}
