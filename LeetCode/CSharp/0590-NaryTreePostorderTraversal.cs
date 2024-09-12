c95a2522d3a15359cb2ffc8a57411a836723120d
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:05:02 +0200

    feature: added 0590

---
+++ LeetCode/CSharp/0590-NaryTreePostorderTraversal.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0590 : IRunProgram
+    {
+        public void Run()
+        {
+            
+        }
+        public IList<int> Postorder(Node root)
+        {
+            var output = new List<int>();
+
+            Helper(root, output);
+
+            return output;
+        }
+
+        private void Helper(Node root, List<int> output)
+        {
+            if (root is null)
+            {
+                return;
+            }
+
+            foreach (var child in root.children)
+            {
+                Helper(child, output);
+            }
+
+            output.Add(root.val);
+        }
+    }
+}
