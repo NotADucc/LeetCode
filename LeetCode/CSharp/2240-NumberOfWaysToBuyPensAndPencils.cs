9f9548d841587092fa936e67144f701365e487e8
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:23:48 +0200

    feature: added 2240

---
+++ LeetCode/CSharp/2240-NumberOfWaysToBuyPensAndPencils.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2240 : IRunProgram
+    {
+        public void Run()
+        {
+            WaysToBuyPensPencils(20, 10, 5).Print();
+        }
+
+        public long WaysToBuyPensPencils(int total, int cost1, int cost2)
+        {
+            int total_pens = total / cost1;
+            long output = total / cost2 + 1;
+
+            for (int i = 1; i <= total_pens; i++)
+            {
+                output += (total - (i * cost1)) / cost2 + 1;
+            }
+
+            return output;
+        }
+    }
+}
