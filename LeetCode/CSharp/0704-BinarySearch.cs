c06bb716ea3b15bc5a197963f3b7f5ea793ed998
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:20:59 +0200

    feature: added 0704

---
+++ LeetCode/CSharp/0704-BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0704: IRunProgram
+    {
+        public void Run()
+        {
+            Search([-1, 0, 3, 5, 9, 12], 9).Print();
+        }
+        public int Search(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) >> 1;
+                if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else if (nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            return -1;
+        }
+    }
+}
