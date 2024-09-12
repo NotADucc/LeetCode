af2e7828b4268a4a9ed8abc481a77f28a494fb0f
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:46:22 +0200

    feature: added 374

---
+++ LeetCode/CSharp/0374-GuessumberHigherOrLower.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0374 : IRunProgram
+    {
+        public void Run()
+        {
+            GuessNumber(4).Print();
+            GuessNumber(90).Print();
+        }
+
+        public int GuessNumber(int n)
+        {
+            int left = 0, right = n - 1;
+
+            while (left <= right)
+            {
+                int mid = (int)(((long)left + right) >> 1);
+                int gue = guess(mid);
+                if (gue == 1)
+                {
+                    left = mid + 1;
+                }
+                else if (gue == -1)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return left;
+        }
+
+        // so compiler doesnt cry
+        private int guess(int mid) { return 1; }
+    }
+}
