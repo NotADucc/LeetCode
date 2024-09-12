040e026533d3be2d9e3ebf0d6fc50c3b3b882da1
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 16:35:42 +0200

    feature: added 0027

---
+++ LeetCode/CSharp/0027-RemoveElement.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0027: IRunProgram
+    {
+        public void Run()
+        {
+            RemoveElement([1, 1, 2], 2).Print();
+            RemoveElement([3, 2, 2, 3], 3).Print();
+            RemoveElement([0, 1, 2, 2, 3, 0, 4, 2], 2).Print();
+        }
+
+        public int RemoveElement(int[] nums, int val)
+        {
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+            return j;
+        }
+    }
+}
