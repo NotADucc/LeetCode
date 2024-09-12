650d33768f9529fa02f434ebf12402b188249629
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:33:55 +0200

    feature: added 1732

---
+++ LeetCode/CSharp/1732-FindTheHighestAltitude.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1721 : IRunProgram
+    {
+        public void Run()
+        {
+            LargestAltitude([-5, 1, 5, 0, -7]).Print();
+        }
+
+        public int LargestAltitude(int[] gain)
+        {
+            int max = 0, curr = 0;
+            for (int i = 0; i < gain.Length; i++)
+            {
+                curr += gain[i];
+                if (curr > max)
+                {
+                    max = curr;
+                }
+            }
+            return max;
+        }
+    }
+}
