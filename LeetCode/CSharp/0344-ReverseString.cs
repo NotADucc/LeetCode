2962a99eec57faca873cff1cf44e23a8db37d090
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:41:51 +0200

    feature: added 344

---
+++ LeetCode/CSharp/0344-ReverseString.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0344 : IRunProgram
+    {
+        public void Run()
+        {
+            ReverseString("Hello".ToArray());
+            ReverseString("Hannah".ToArray());
+        }
+
+        public void ReverseString(char[] s)
+        {
+            char l;
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                l = s[i];
+                s[i] = s[^(i + 1)];
+                s[^(i + 1)] = l;
+            }
+        }
+    }
+}
