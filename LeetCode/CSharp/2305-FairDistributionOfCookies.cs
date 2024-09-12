39b8f86d5a0ad9679ce2a98f9afb7bff3dbe5fc2
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:26:52 +0200

    feature: added 2305

---
+++ LeetCode/CSharp/2305-FairDistributionOfCookies.cs
@@ -0,0 +1,48 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2305 : IRunProgram
+    {
+        public void Run()
+        {
+            DistributeCookies([8, 15, 10, 20, 8], 2).Print();
+        }
+
+        public int DistributeCookies(int[] cookies, int k)
+        {
+            int output = int.MaxValue, max = 0;
+            int[] children = new int[k];
+            Helper(ref output, cookies, children, max, 0);
+            return output;
+        }
+
+        private void Helper(ref int output, int[] cookies, int[] children, int max, int index)
+        {
+            if (cookies.Length == index)
+            {
+                output = Math.Min(output, max);
+            }
+            else
+            {
+                for (int j = 0; j < children.Length; j++)
+                {
+                    unsafe
+                    {
+                        children[j] += cookies[index];
+                        int temp_max = Math.Max(max, children[j]);
+                        Helper(ref output, cookies, children, temp_max, index + 1);
+                        children[j] -= cookies[index];
+                    }
+                }
+            }
+        }
+    }
+}
