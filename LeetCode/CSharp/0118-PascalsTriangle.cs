efb3db1114e4a03731754ef2905bd7468ddc1e40
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 18:48:36 +0200

    feature: added 0118

---
+++ LeetCode/CSharp/0118-PascalsTriangle.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0118 : IRunProgram
+    {
+        public void Run()
+        {
+            Generate(5).PrintNested();
+            Generate(1).PrintNested();
+        }
+
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var output = new List<IList<int>>(numRows);
+            output.Add(new List<int>() { 1 });
+            for (int i = 1; i < numRows; i++)
+            {
+                var temp = new List<int>();
+                temp.Add(1);
+                for (int j = 1; j < i; j++)
+                {
+                    temp.Add(output[i - 1][j - 1] + output[i - 1][j]);
+                }
+                temp.Add(1);
+                output.Add(temp);
+            }
+            return output;
+        }
+    }
+}
