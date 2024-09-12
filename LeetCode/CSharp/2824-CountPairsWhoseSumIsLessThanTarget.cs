c49122c2e4aad3b5df9d7227080c396f58f6f681
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:01:26 +0200

    feature: added 2824

---
+++ LeetCode/CSharp/2824-CountPairsWhoseSumIsLessThanTarget.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Shared;
+
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2824 : IRunProgram
+    {
+        public void Run()
+        {
+            CountPairs([-1, 1, 2, 3, 1], 2).Print();
+        }
+
+        public int CountPairs(IList<int> nums, int target)
+        {
+            var numList = nums as List<int>;
+
+            numList.Sort();
+
+            int count = 0;
+
+            int left = 0;
+            int right = numList.Count - 1;
+
+            while (left < numList.Count)
+            {
+                if (numList[left] + numList[right] < target)
+                {
+                    count += right - left;
+
+                    left++;
+                    right = numList.Count - 1;
+                }
+                else
+                {
+                    if (left >= right)
+                    {
+                        left++;
+                        right = numList.Count - 1;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
