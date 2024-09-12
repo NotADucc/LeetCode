f1efba064f37235f24fae4e94a1cc6eb047da0bb
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:04:59 +0200

    feature: added 1389

---
+++ LeetCode/CSharp/1389-CreateTargetArrayInTheGivenOrder.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1389 : IRunProgram
+    {
+        public void Run()
+        {
+            CreateTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]);
+        }
+        public int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            if (nums.Length == 1)
+            {
+                return nums;
+            }
+
+            var output = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                int ind = index[i];
+
+                output.Insert(ind, num);
+            }
+
+            return output.ToArray();
+        }
+    }
+}
