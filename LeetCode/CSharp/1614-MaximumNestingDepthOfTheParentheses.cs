4b4d59fe992b21d2022d1b0d91eb35eb168c176a
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:21:22 +0200

    feature: added 1614

---
+++ LeetCode/CSharp/1614-MaximumNestingDepthOfTheParentheses.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1614 : IRunProgram
+    {
+        public void Run()
+        {
+            MaxDepth("(1+(2*3)+((8)/4))+1").Print();
+        }
+
+        public int MaxDepth(string s)
+        {
+            int max = 0;
+            int current = 0;
+
+            foreach (var ch in s)
+            {
+                if (ch == '(')
+                {
+                    current++;
+                    max = Math.Max(max, current);
+                }
+                else if (ch == ')')
+                {
+                    current--;
+                }
+            }
+
+            return max;
+        }
+    }
+}
