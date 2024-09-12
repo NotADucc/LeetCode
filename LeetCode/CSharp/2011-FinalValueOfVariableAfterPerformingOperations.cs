2ffc68ddb6ad8fc6ea9a38624c481c569c72e684
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:59:56 +0200

    feature: added 2011

---
+++ LeetCode/CSharp/2011-FinalValueOfVariableAfterPerformingOperations.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2011 : IRunProgram
+    {
+        public void Run()
+        {
+            FinalValueAfterOperations(["--X", "X++", "X++"]).Print();
+        }
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int x = 0;
+            foreach (var item in operations)
+            {
+                if (item[0] == '-' || item[^1] == '-')
+                {
+                    x--;
+                }
+                else
+                {
+                    x++;
+                }
+            }
+            return x;
+        }
+    }
+}
