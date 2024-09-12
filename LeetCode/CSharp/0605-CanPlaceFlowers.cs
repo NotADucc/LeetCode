970287e3d7f8b25f6f05f07888b1b431633afa7c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:10:15 +0200

    feature: added 0605

---
+++ LeetCode/CSharp/0605-CanPlaceFlowers.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0605 : IRunProgram
+    {
+        public void Run()
+        {
+            CanPlaceFlowers([1, 0, 0, 0, 1], 1).Print();
+            CanPlaceFlowers([1, 0, 0, 0, 1], 2).Print();
+        }
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            if (flowerbed.Length == 1)
+            {
+                return (flowerbed[0] == 0 && n <= 1) || (flowerbed[0] == 1 && n == 0);
+            }
+
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                bool start = (i == 0 || flowerbed[i - 1] == 0) && (i + 1 < flowerbed.Length && flowerbed[i] == 0 && flowerbed[i + 1] == 0);
+                bool end = (i == flowerbed.Length - 1 && flowerbed[i - 1] == 0 && flowerbed[i] == 0);
+                if (start || end)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+
+            return n <= 0;
+        }
+    }
+}
