6a81837d7ae2ec419355f1497a48f239e747d93d
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:39:16 +0200

    feature: added 2486

---
+++ LeetCode/CSharp/2486-AppendCharactersToStringToMakeSubsequence.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2486 : IRunProgram
+    {
+        public void Run()
+        {
+            AppendCharacters("coaching", "coding").Print();
+        }
+        public int AppendCharacters(string s, string t)
+        {
+            int s_left = 0, t_left = 0;
+
+            while (s_left < s.Length && t_left < t.Length)
+            {
+                if (s[s_left] == t[t_left])
+                {
+                    t_left++;
+                }
+                s_left++;
+            }
+
+            return t.Length - t_left;
+        }
+    }
+}
