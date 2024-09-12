853c673d7c7a22ef5f5b7a1c37bcfa090f7a9644
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:31:07 +0200

    feature: added 0821

---
+++ LeetCode/CSharp/0821-ShortestDistanceToACharacter.cs
@@ -0,0 +1,68 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0821: IRunProgram
+    {
+        public void Run()
+        {
+            ShortestToChar("loveleetcode", 'e').Print();
+        }
+        public int[] ShortestToChar(string s, char c)
+        {
+            var output = new int[s.Length];
+
+            int old_left = -1, left = -1;
+
+            for (int current_char_index = 0; current_char_index < s.Length; current_char_index++)
+            {
+                //refresh left when current index is outside bounds of our window
+                if (current_char_index > left)
+                {
+                    old_left = left;
+                    left++;
+                    //seek next bound
+                    while (left < s.Length)
+                    {
+                        if (s[left] == c)
+                        {
+                            break;
+                        }
+                        left++;
+                    }
+                    //reset left since there are no more c's left
+                    if (left == s.Length)
+                    {
+                        left = old_left;
+                    }
+                }
+
+                int difference;
+                if (old_left == -1)
+                {
+                    difference = left - current_char_index;
+                }
+                //only make a comparison if theres a distinct lefts
+                else if (left != old_left)
+                {
+                    difference = Math.Min(left - current_char_index, current_char_index - old_left);
+                }
+                else
+                {
+                    difference = current_char_index - old_left;
+                }
+
+                output[current_char_index] = difference;
+            }
+
+            return output;
+        }
+    }
+}
