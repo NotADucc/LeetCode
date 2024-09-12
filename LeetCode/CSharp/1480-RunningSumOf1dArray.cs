75e5aaeab8db16ef91bb405baf0b7b66fb38fa99
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:13:20 +0200

    feature: added 1480

---
+++ LeetCode/CSharp/1480-RunningSumOf1dArray.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1480 : IRunProgram
+    {
+        public void Run()
+        {
+            RunningSum([1, 2, 3, 4]).Print();
+        }
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 0; i < nums.Count() - 1; i++)
+            {
+                nums[i + 1] += nums[i];
+            }
+
+            return nums;
+        }
+    }
+}
