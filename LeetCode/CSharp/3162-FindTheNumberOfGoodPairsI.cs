586402807074eedd53f4819fe7f982ac5c185cf4
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:12:21 +0200

    feature: added 3162

---
+++ LeetCode/CSharp/3162-FindTheNumberOfGoodPairsI.cs
@@ -0,0 +1,48 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution3162 : IRunProgram
+    {
+        public void Run()
+        {
+            NumberOfPairs([1, 3, 4], [1, 3, 4], 1).Print();
+        }
+
+        public int NumberOfPairs(int[] nums1, int[] nums2, int k)
+        {
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+
+            int output = 0;
+            int i = 0, j = 0;
+
+            while (i < nums1.Length)
+            {
+                int quotient = nums1[i];
+                int divisor = nums2[j] * k;
+                if (quotient % divisor == 0)
+                {
+                    output++;
+                }
+
+                if (divisor > quotient || (j + 1) >= nums2.Length)
+                {
+                    i++;
+                    j = 0;
+                }
+                else
+                {
+                    j = j < nums2.Length ? j + 1 : 0;
+                }
+            }
+
+            return output;
+        }
+    }
+}
