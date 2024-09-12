5a2f47b90c21849b1d9b8b93a67850ba567fa082
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 17:07:26 +0200

    feature: added 0067, can most likely improve

---
+++ LeetCode/CSharp/0067-AddBinary.cs
@@ -0,0 +1,83 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0067 : IRunProgram
+    {
+        public void Run()
+        {
+            AddBinary("11", "1").Print();
+        }
+
+        public string AddBinary(string a, string b)
+        {
+            (string bString, string sString) = a.Length > b.Length ? (a, b) : (b, a);
+            string temp = "";
+            bool carryOver = false;
+            for (int i = 0; i < bString.Length; i++)
+            {
+                if (sString.Length <= i && !carryOver)
+                {
+                    temp = bString[^(i + 1)] + temp;
+                    continue;
+                }
+                if (sString.Length <= i && carryOver)
+                {
+                    carryOver = false;
+                    if (bString[^(i + 1)] == '1')
+                    {
+                        carryOver = true;
+                        temp = "0" + temp;
+                    }
+                    else
+                    {
+                        temp = "1" + temp;
+                    }
+                    continue;
+                }
+                if (!carryOver && sString[^(i + 1)] == '1' && bString[^(i + 1)] == '1')
+                {
+                    carryOver = true;
+                    temp = "0" + temp;
+                    continue;
+                }
+                if (carryOver && sString[^(i + 1)] == '1' && bString[^(i + 1)] == '1')
+                {
+                    carryOver = true;
+                    temp = "1" + temp;
+                    continue;
+                }
+                if (carryOver && (sString[^(i + 1)] == '1' || bString[^(i + 1)] == '1'))
+                {
+                    carryOver = true;
+                    temp = "0" + temp;
+                    continue;
+                }
+                if (carryOver)
+                {
+                    carryOver = false;
+                    temp = "1" + temp;
+                    continue;
+                }
+                if (sString[^(i + 1)] == '1')
+                {
+                    temp = sString[^(i + 1)] + temp;
+                }
+                else
+                {
+                    temp = bString[^(i + 1)] + temp;
+                }
+            }
+            if (carryOver)
+            {
+                temp = "1" + temp;
+            }
+            return temp;
+        }
+    }
+}
