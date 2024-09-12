83f83c9a2268c8623c8a55216ef1242383de3f60
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:16:12 +0200

    feature: added 2177

---
+++ LeetCode/CSharp/2177-FindThreeConsecutiveIntegersThatSumToAGivenNumber.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2177 : IRunProgram
+    {
+        public void Run()
+        {
+            SumOfThree(33).Print();
+        }
+
+        public long[] SumOfThree(long num)
+        {
+            long middle = num / 3;
+            if (middle - 1 + middle + middle + 1 == num)
+            {
+                return [middle - 1, middle, middle + 1];
+            }
+            else
+            {
+                return [];
+            }
+        }
+    }
+}
