c8fea46e6718973358b2b5e2dc1c2e632804c077
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:00:25 +0200

    feature: added 2807

---
+++ LeetCode/CSharp/2807-InsertGreatestCommonDivisorsInLinkedList.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Shared;
+
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2807 : IRunProgram
+    {
+        public void Run()
+        {
+            InsertGreatestCommonDivisors(ListNodeHelper.New(15)).Print();
+        }
+
+        public ListNode InsertGreatestCommonDivisors(ListNode head)
+        {
+            Helper(head);
+            return head;
+        }
+
+        private void Helper(ListNode head)
+        {
+            if (head.next is null) { return; }
+
+
+            (int a, int b) = head.val < head.next.val ? (head.next.val, head.val) : (head.val, head.next.val);
+            while (b != 0)
+            {
+                (a, b) = (b, a % b);
+            }
+
+            ListNode node = new ListNode(a, head.next);
+            head.next = node;
+            Helper(head.next.next);
+        }
+    }
+}
