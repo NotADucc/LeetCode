87cb01a91b5c57b85e16024f83331b00b806c6dc
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:22:47 +0200

    feature: added 2235

---
+++ LeetCode/CSharp/2235-AddTwoIntegers.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2235 : IRunProgram
+    {
+        public void Run()
+        {
+            Sum(10, 7).Print();
+        }
+
+        public int Sum(int num1, int num2) => num1 + num2;
+    }
+}
