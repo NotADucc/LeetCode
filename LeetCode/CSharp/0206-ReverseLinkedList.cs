6566d9f9a683a8a93aa997a688aa3345669487d0
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:16:53 +0200

    feature: added 0206

---
+++ LeetCode/CSharp/0206-ReverseLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0206 : IRunProgram
+    {
+        public void Run()
+        {
+            ReverseList(ListNodeHelper.New(10)).Print();
+        }
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode node = null;
+            Helper(head, ref node);
+            return node;
+        }
+
+        private void Helper(ListNode head, ref ListNode node)
+        {
+            if (head is null) { return; }
+
+            node = new ListNode(head.val, node);
+            head = head.next;
+            Helper(head, ref node);
+        }
+    }
+}
