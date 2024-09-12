8bd420521ce92b76cf58dc6d94d206136d1ddf0f
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:10:42 +0200

    feature: added 3131

---
+++ LeetCode/CSharp/3131-FindTheIntegerAddedToArrayI.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution3131 : IRunProgram
+    {
+        public void Run()
+        {
+            AddedInteger([2, 6, 4], [9, 7, 5]).Print();
+        }
+
+        public int AddedInteger(int[] nums1, int[] nums2)
+        {
+            int num2 = 0, num1 = 0;
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                num1 += nums1[i];
+                num2 += nums2[i];
+            }
+            return (num2 - num1) / nums1.Length;
+        }
+    }
+}
