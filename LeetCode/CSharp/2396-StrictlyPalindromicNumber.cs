1548b9b67a4e759249e8d414f358c2ce99da69a6
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:34:27 +0200

    feature: added 2396

---
+++ LeetCode/CSharp/2396-StrictlyPalindromicNumber.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2396 : IRunProgram
+    {
+        public void Run()
+        {
+            IsStrictlyPalindromic(2).Print();
+        }
+        public bool IsStrictlyPalindromic(int n)
+        {
+            return false;
+        }
+    }
+}
