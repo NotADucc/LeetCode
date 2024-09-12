6b57f75c6be81f5f64f65c4b29d67fb12046827a
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:15:02 +0200

    feature: added 0202

---
+++ LeetCode/CSharp/0202-HappyNumber.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0202 : IRunProgram
+    {
+        public void Run()
+        {
+            IsHappy(19).Print();
+            IsHappy(2).Print();
+        }
+        public bool IsHappy(int n)
+        {
+            var set = new HashSet<int>();
+
+            while (n != 1)
+            {
+                n = Calc(n);
+                if (!set.Add(n))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int Calc(int input)
+        {
+            int output = 0;
+
+            while (input > 0)
+            {
+                int temp = input % 10;
+                input /= 10;
+                output += temp * temp;
+            }
+
+            return output;
+        }
+    }
+}
