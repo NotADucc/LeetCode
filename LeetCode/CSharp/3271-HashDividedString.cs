b797e948508a4ad54b3775e792fac35399f6fb51
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:17:29 +0200

    feature: added 3271

---
+++ LeetCode/CSharp/3271-HashDividedString.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution3271 : IRunProgram
+    {
+        public void Run()
+        {
+            StringHash("abcd", 2).Print();
+        }
+
+        public string StringHash(string s, int k)
+        {
+            Span<char> span = stackalloc char[s.Length / k];
+
+            for (int i = 0; i < s.Length; i += k)
+            {
+                int c = 0;
+                for (int j = 0; j < k; j++)
+                {
+                    c += s[i + j] - 'a';
+                }
+                span[i / k] = (char)(c % 26 + 'a');
+            }
+
+            return span.ToString();
+        }
+    }
+}
