fac3bae86d1ede6fbf12bed89271962e72174af6
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:39:44 +0200

    feature: added 1816

---
+++ LeetCode/CSharp/1816-TruncateSentence.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1816 : IRunProgram
+    {
+        public void Run()
+        {
+            TruncateSentence("Hello how are you Contestent", 1).Print();
+        }
+
+        public string TruncateSentence(string s, int k)
+        {
+            var span = s.AsSpan();
+            int size = 0;
+            k--;
+            foreach (char ch in span)
+            {
+                if (ch == ' ')
+                {
+                    if (k <= 0)
+                    {
+                        break;
+                    }
+                    k--;
+                }
+                size++;
+            }
+
+            return span.Slice(0, size).ToString();
+        }
+    }
+}
