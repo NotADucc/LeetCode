7f7c18abcc9b4a161013535e2bd886e341f2eb06
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:32:36 +0200

    feature: added 0242

---
+++ LeetCode/CSharp/0242-ValidAnagram.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0242 : IRunProgram
+    {
+        public void Run()
+        {
+            IsAnagram("", "").Print();
+            IsAnagram("rat", "car").Print();
+            IsAnagram("anagram", "nagaram").Print();
+        }
+
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) { return false; }
+            var dct = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                dct.TryAdd(s[i], 0);
+                dct.TryAdd(t[i], 0);
+                dct[s[i]]++;
+                dct[t[i]]--;
+            }
+
+            return dct.Values.All(x => x == 0);
+        }
+    }
+}
