c6ec763529d6e019d696eec01be57fc59461f387
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 01:08:12 +0200

    feature: added 0160

---
+++ LeetCode/CSharp/0160-IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0160 : IRunProgram
+    {
+        public void Run()
+        {
+            
+        }
+
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            HashSet<ListNode> visited = new HashSet<ListNode>();
+            while (headA is not null)
+            {
+                visited.Add(headA);
+                headA = headA.next;
+            }
+            while (headB is not null)
+            {
+                if (visited.Contains(headB))
+                {
+                    return headB;
+                }
+                headB = headB.next;
+            }
+            return null;
+        }
+    }
+}
