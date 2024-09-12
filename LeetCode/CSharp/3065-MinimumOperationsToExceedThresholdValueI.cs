b02e73f2ab8f864f1ef1bfab042edf164f5f57a9
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:09:12 +0200

    feature: added 3065

---
+++ LeetCode/CSharp/3065-MinimumOperationsToExceedThresholdValueI.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution3065 : IRunProgram
+    {
+        public void Run()
+        {
+            MinOperations([2, 11, 10, 1, 3], 10).Print();
+        }
+
+        public int MinOperations(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] < k)
+                {
+                    left = mid + 1;
+                }
+                else if (nums[mid] > k)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+    }
+}
