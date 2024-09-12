7c5e7498f1231162a7e4762030f6585c2cb051dc
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:30:00 +0200

    feature: added 2373

---
+++ LeetCode/CSharp/2373-LargestLocalValuesInAMatrix.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2373 : IRunProgram
+    {
+        public void Run()
+        {
+            LargestLocal([[9, 9, 8, 1], [5, 6, 2, 6], [8, 2, 6, 4], [6, 2, 2, 2]]).Print();
+        }
+        public int[][] LargestLocal(int[][] grid)
+        {
+            var maxLocal = new int[grid.Length - 2][];
+
+            for (int i = 0; i < maxLocal.Length; i++)
+            {
+                maxLocal[i] = new int[grid.Length - 2];
+                for (int j = 0; j < maxLocal.Length; j++)
+                {
+                    for (int ii = i; ii < 3 + i; ii++)
+                    {
+                        for (int jj = j; jj < 3 + j; jj++)
+                        {
+                            maxLocal[i][j] = Math.Max(maxLocal[i][j], grid[ii][jj]);
+                        }
+                    }
+                }
+            }
+
+            return maxLocal;
+        }
+    }
+}
