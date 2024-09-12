d4f133acc47a5971d920c2ec12160dd69fcb6b84
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:44:26 +0200

    feature: added 349

---
+++ LeetCode/CSharp/0349-IntersectionOfTwoArrays.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0349 : IRunProgram
+    {
+        public void Run()
+        {
+            Intersection([1, 2, 2, 1], [2, 2]).Print();
+        }
+
+        public int[] Intersection(int[] i, int[] j)
+        {
+            return i.Where(x => j.Contains(x)).Distinct().ToArray();
+        }
+    }
+}
