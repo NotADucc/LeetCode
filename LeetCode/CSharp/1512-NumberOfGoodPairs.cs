5d955d8dc09bde4d98944e6cf40e95c34f0a9a52
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:14:58 +0200

    feature: added 1512

---
+++ LeetCode/CSharp/1512-NumberOfGoodPairs.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1512 : IRunProgram
+    {
+        public void Run()
+        {
+            NumIdenticalPairs([1, 2, 3, 1, 1, 3]).Print();
+        }
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int cnt = 0;
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                for (int j = i + 1; j < nums.Count(); j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        cnt++;
+                    }
+                }
+            }
+            return cnt;
+        }
+    }
+}
