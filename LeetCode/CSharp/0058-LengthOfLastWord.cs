cff695316ac701f647d67462a3c9878393665daf
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 17:04:13 +0200

    feature: added 0058

---
+++ LeetCode/CSharp/0058-LengthOfLastWord.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0058 : IRunProgram
+    {
+        public void Run()
+        {
+            LengthOfLastWord("Hello World").Print();
+            LengthOfLastWord("   fly me   to   the moon  ").Print();
+        }
+
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            string[] arr = s.Split(" ");
+            return arr[^1].Length;
+        }
+    }
+}
