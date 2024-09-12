622996722b29ec826471f8b5b4e5abaef193e55a
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:59:19 +0200

    feature: added 2798

---
+++ LeetCode/CSharp/2798-NumberOfEmployeesWhoMetTheTarget.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Shared;
+
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2798 : IRunProgram
+    {
+        public void Run()
+        {
+            NumberOfEmployeesWhoMetTarget([0, 1, 2, 3, 4], 2).Print();
+        }
+
+        public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            return hours.Count(x => x >= target);
+        }
+    }
+}
