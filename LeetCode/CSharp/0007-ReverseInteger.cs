9a0d38824f37e04c5608b494b1f11769ccc25596
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 16:17:24 +0200

    feature: added 0007

---
+++ LeetCode/CSharp/0007-ReverseInteger.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0007 : IRunProgram
+    {
+        public void Run()
+        {
+            Reverse(123).Print();
+            Reverse(321).Print();
+        }
+        public int Reverse(int x)
+        {
+            int output = 0;
+
+            while (x != 0)
+            {
+                int quot = x % 10;
+                int temp = (output * 10) + quot;
+                x = x / 10;
+                if (
+                    output > 0 && temp < output
+                    || output < 0 && temp > output
+                    || x != 0 && temp > 214_748_365
+                    || x != 0 && temp < -214_748_365
+                )
+                {
+                    return 0;
+                }
+                output = temp;
+            }
+
+            return output;
+        }
+    }
+}
