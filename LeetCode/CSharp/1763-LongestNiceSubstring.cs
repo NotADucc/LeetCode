d58fb6e267ba61ba00722b067ce2a0939ebe8c31
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:34:55 +0200

    feature: added 1763

---
+++ LeetCode/CSharp/1763-LongestNiceSubstring.cs
@@ -0,0 +1,69 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1763 : IRunProgram
+    {
+        public void Run()
+        {
+            LongestNiceSubstring("YazaAay").Print();
+            LongestNiceSubstring("Bb").Print();
+        }
+
+        public string LongestNiceSubstring(string s)
+        {
+            var set = new HashSet<char>();
+            int start = 0, len = 0;
+            bool is_nice = false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                set.Clear();
+                for (int j = i; j < s.Length; j++)
+                {
+                    set.Add(s[j]);
+                    if (char.IsLower(s[j]))
+                    {
+                        is_nice = set.Contains((char)(s[j] - 32));
+                    }
+                    else
+                    {
+                        is_nice = set.Contains((char)(s[j] + 32));
+                    }
+
+                    if (is_nice && j - i + 1 > len)
+                    {
+                        Span<int> freq = stackalloc int[26];
+                        foreach (var ch in set)
+                        {
+                            freq[char.ToLower(ch) - 'a']++;
+                        }
+                        int k = 0;
+                        for (; k < freq.Length; k++)
+                        {
+                            if (freq[k] == 1)
+                            {
+                                break;
+                            }
+                        }
+
+                        if (k == freq.Length)
+                        {
+                            start = i;
+                            len = j - i + 1;
+                        }
+                    }
+                }
+            }
+
+            return s.Substring(start, len);
+        }
+    }
+}
