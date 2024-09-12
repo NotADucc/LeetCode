ebd494ab66d81630c31222bd72f0a6cd4093dab1
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 15:56:56 +0200

    feature: added two sum

---
+++ LeetCode/CSharp/0001-TwoSum.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0001 : IRunProgram
+    {
+        public void Run()
+        {
+            TwoSum([2, 7, 11, 15], 9).Print();
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var deltas = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (deltas.ContainsKey(nums[i]))
+                {
+                    return [deltas[nums[i]], i];
+                }
+                int delta = target - nums[i];
+                deltas.TryAdd(delta, i);
+            }
+
+            return [];
+        }
+    }
+}
