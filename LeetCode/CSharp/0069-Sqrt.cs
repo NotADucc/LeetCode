180d812bb1e9b02bf87aabc7543d347b3c0c8444
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 17:08:13 +0200

    feature: added 0069

---
+++ LeetCode/CSharp/0069-Sqrt.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0069 : IRunProgram
+    {
+        public void Run()
+        {
+            MySqrt(4).Print();
+            MySqrt(8).Print();
+        }
+
+        public int MySqrt(int x)
+        {
+            double left = 0, right = x;
+            while (left < right)
+            {
+                double mid = (left + right) / 2;
+                if (Math.Floor(mid * mid) < x)
+                {
+                    left = mid;
+                }
+                else if (Math.Floor(mid * mid) > x)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    return (int)Math.Floor(mid);
+                }
+            }
+            return (int)Math.Floor(left);
+        }
+    }
+}
