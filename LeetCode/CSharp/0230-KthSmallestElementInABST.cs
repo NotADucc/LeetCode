508c0acb1b0f8ef93ec3ab1c4cefbbb3abecf74b
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:24:18 +0200

    feature: added 0230

---
+++ LeetCode/CSharp/0230-KthSmallestElementInABST.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0230 : IRunProgram
+    {
+        public void Run()
+        {
+            KthSmallest(new TreeNode(1), 1);
+            KthSmallest(new TreeNode(2, new TreeNode(1)), 1);
+        }
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            int i = 0, small = int.MaxValue;
+
+            Helper(root, ref i, ref small, k);
+
+            return small;
+        }
+
+        public void Helper(TreeNode root, ref int i, ref int small, int k)
+        {
+            if (root is null)
+            {
+                return;
+            }
+
+
+            Helper(root.left, ref i, ref small, k);
+            i++;
+            if (i == k) { small = Math.Min(small, root.val); }
+            Helper(root.right, ref i, ref small, k);
+        }
+    }
+}
