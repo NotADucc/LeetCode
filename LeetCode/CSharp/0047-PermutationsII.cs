cb76b693ddb40f06ddf2c1159a4e49ab0256854c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 16:54:58 +0200

    feature: added 0047

---
+++ LeetCode/CSharp/0047-PermutationsII.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0047 : IRunProgram
+    {
+        public void Run()
+        {
+            PermuteUnique([1, 2, 2, 2, 3]).PrintNested();
+            PermuteUnique([0, 1, 1]).PrintNested();
+            PermuteUnique([0]).PrintNested();
+        }
+
+        public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            Array.Sort(nums);
+            var output = new List<IList<int>>();
+            Helper(output, nums, []);
+            return output;
+        }
+
+        private void Helper(List<IList<int>> output, int[] nums, List<int> current)
+        {
+            if (nums.Length == 0)
+            {
+                output.Add(new List<int>(current));
+            }
+            else
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (i > 0 && nums[i] == nums[i - 1])
+                    {
+                        continue;
+                    }
+                    current.Add(nums[i]);
+                    Helper(output, [.. nums[..i], .. nums[(i + 1)..]], current);
+                    int last = current.LastIndexOf(nums[i]);
+                    current.RemoveAt(last);
+                }
+            }
+        }
+    }
+}
