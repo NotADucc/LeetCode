dd8ee34e55f8a994a9c446290f6d2ba7dacc664e
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:42:45 +0200

    feature: added 345

---
+++ LeetCode/CSharp/0345-ReverseVowelsOfAString.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0345 : IRunProgram
+    {
+        public void Run()
+        {
+            ReverseVowels("iceCreAm").Print();
+            ReverseVowels("leetcode").Print();
+        }
+
+        public string ReverseVowels(string s)
+        {
+            var a = s.ToArray();
+            int left = 0;
+            int right = a.Length - 1;
+            while (left < right)
+            {
+                if (!IsVowel(a[left]))
+                {
+                    left++;
+                    continue;
+                }
+
+                if (!IsVowel(a[right]))
+                {
+                    right--;
+                    continue;
+                }
+
+                var temp = a[left];
+                a[left] = a[right];
+                a[right] = temp;
+                left++;
+                right--;
+            }
+
+            return String.Join("", a);
+        }
+
+        private bool IsVowel(char input)
+        {
+            input = char.ToLower(input);
+            return
+            'a' == input
+            | 'e' == input
+            | 'i' == input
+            | 'o' == input
+            | 'u' == input;
+        }
+    }
+}
