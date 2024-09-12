4dd84ee1efc0486cb6b6075c8eff54d9b3d05595
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:25:09 +0200

    feature: added 0739, most likely be improved

---
+++ LeetCode/CSharp/0739-DailyTemperatures.cs
@@ -0,0 +1,56 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0739: IRunProgram
+    {
+        public void Run()
+        {
+            DailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]).Print();
+        }
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int seeker = 1;
+            int prev = int.MinValue;
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                if (i > 0 && prev == temperatures[i])
+                {
+                    temperatures[i] = Math.Max(temperatures[i - 1] - 1, 0);
+                    continue;
+                }
+
+                while (true)
+                {
+                    if (i + seeker >= temperatures.Length)
+                    {
+                        prev = temperatures[i];
+                        temperatures[i] = 0;
+                        seeker = 1;
+                        break;
+                    }
+                    if (temperatures[i + seeker] > temperatures[i])
+                    {
+                        prev = temperatures[i];
+                        temperatures[i] = seeker;
+                        seeker = 1;
+                        break;
+                    }
+                    else
+                    {
+                        seeker++;
+                    }
+                }
+            }
+
+            return temperatures;
+        }
+    }
+}
