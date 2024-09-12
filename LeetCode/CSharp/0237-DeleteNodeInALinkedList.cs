3f7f46fe26e7beb67c0a505b9cca45e0ed43049b
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:29:04 +0200

    feature: added 0237

---
+++ LeetCode/CSharp/0237-DeleteNodeInALinkedList.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0237 : IRunProgram
+    {
+        public void Run()
+        {
+            // kinda cumbersome to replicate
+        }
+
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
