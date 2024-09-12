ddeb1e79b3dbc0a7fb2eae8a943ecd44f48eddd6
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:57:42 +0200

    feature: added 2000

---
+++ LeetCode/CSharp/2000-ReversePrefixOfWord.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2000 : IRunProgram
+    {
+        public void Run()
+        {
+            ReversePrefix("abcdefd", 'd').Print();
+        }
+        public string ReversePrefix(string word, char ch)
+        {
+            int end = Math.Max(word.IndexOf(ch) + 1, 0);
+
+            var prefix = word.Substring(0, end).Reverse().ToArray();
+
+            return new string(prefix) + word.Substring(end);
+        }
+    }
+}
