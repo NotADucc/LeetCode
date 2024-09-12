977d11fc716450a44e4f29bd8ffe4881714fabeb
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:35:43 +0200

    feature: added 1768

---
+++ LeetCode/CSharp/1768-MergeStringsAlternately.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1768 : IRunProgram
+    {
+        public void Run()
+        {
+            MergeAlternately("abc", "pqr").Print();
+        }
+
+        public string MergeAlternately(string word1, string word2)
+        {
+            Span<char> output = stackalloc char[word1.Length + word2.Length];
+
+            int i = 0, w1 = 0, w2 = 0;
+
+            while (i < output.Length)
+            {
+                if ((w1 <= w2 && w1 < word1.Length) || w2 >= word2.Length)
+                {
+                    output[i] = word1[w1];
+                    w1++;
+                }
+                else
+                {
+                    output[i] = word2[w2];
+                    w2++;
+                }
+                i++;
+            }
+
+            return new string(output);
+        }
+    }
+}
