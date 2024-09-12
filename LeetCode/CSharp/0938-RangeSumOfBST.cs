4a7602d397f7069295bbfb93a67f11695ff160a5
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:45:44 +0200

    feature: added 0938

---
+++ LeetCode/CSharp/0938-RangeSumOfBST.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0938: IRunProgram
+    {
+        public void Run()
+        {
+            
+        }
+
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            if (root is null)
+            {
+                return 0;
+            }
+
+            return (root.val >= low && root.val <= high ? root.val : 0) + RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+        }
+    }
+}
