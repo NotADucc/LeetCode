cbb210815b285d8da2158cbd9e7d43a62f7beccb
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 21:44:45 +0200

    feature: added 1079

---
+++ LeetCode/CSharp/1079-LetterTilePossibilities.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1079: IRunProgram
+    {
+        public void Run()
+        {
+            NumTilePossibilities("AAB").Print();
+        }
+
+        public int NumTilePossibilities(string tiles)
+        {
+            int output = 0;
+            Helper(ref output, new string(tiles.OrderBy(c => c).ToArray()), 0);
+            return output;
+        }
+
+        private unsafe void Helper(ref int output, ReadOnlySpan<char> tiles, int index)
+        {
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                if (i > 0 && tiles[i] == tiles[i - 1])
+                {
+                    continue;
+                }
+                output++;
+                Helper(ref output, [.. tiles[..i], .. tiles[(i + 1)..]], i + 1);
+            }
+        }
+    }
+}
