9d65d3fd18ee1257cbabf88d7ce7a4ba747dd852
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:42:17 +0200

    feature: added 1833, cheese

---
+++ LeetCode/CSharp/1833-MaximumIceCreamBars.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1833 : IRunProgram
+    {
+        public void Run()
+        {
+            // TODO: cheese
+            MaxIceCream([1, 3, 2, 4, 1], 7).Print();
+        }
+
+        public int MaxIceCream(int[] costs, int coins)
+        {
+            Array.Sort(costs);
+            int total = 0;
+            foreach (var a in costs)
+            {
+                if (a > coins)
+                {
+                    return total;
+                }
+                coins -= a;
+                total++;
+            }
+            return total;
+        }
+    }
+}
