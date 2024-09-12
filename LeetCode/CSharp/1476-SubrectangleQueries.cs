d3e0ff19db491675e5a4e12097637a7589a38089
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:12:15 +0200

    feature: added 1476

---
+++ LeetCode/CSharp/1476-SubrectangleQueries.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1476 : IRunProgram
+    {
+        public void Run()
+        {
+
+        }
+    }
+    public class SubrectangleQueries
+    {
+        private int[][] rect;
+        public SubrectangleQueries(int[][] rectangle)
+        {
+            rect = rectangle;
+        }
+
+        public void UpdateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+        {
+            for (int row = row1; row <= row2; row++)
+            {
+                for (int col = col1; col <= col2; col++)
+                {
+                    rect[row][col] = newValue;
+                }
+            }
+        }
+
+        public int GetValue(int row, int col)
+        {
+            return rect[row][col];
+        }
+    }
+}
