6f7344db1ee94605b522b35325f1e4240d3c61c3
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 18:35:36 +0200

    feature: added 0088

---
+++ LeetCode/CSharp/0088-MergeSortedArray.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0088 : IRunProgram
+    {
+        public void Run()
+        {
+            Merge([1, 2, 3, 0, 0, 0], 3, [1, 2, 3], 3);
+        }
+
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            if (n == 0) { return; }
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                nums1[m + i] = nums2[i];
+            }
+
+
+            Array.Sort(nums1);
+        }
+    }
+}
