bb357e1d63182b5584739443c8e6ad1c7176c19c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:03:59 +0200

    feature: added 0567

---
+++ LeetCode/CSharp/0567-PermutationInString.cs
@@ -0,0 +1,56 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0567 : IRunProgram
+    {
+        public void Run()
+        {
+            CheckInclusion("ab", "eidbaoo").Print();
+        }
+        public bool CheckInclusion(string s1, string s2)
+        {
+            if (s1.Length > s2.Length) { return false; }
+
+            var dct1 = s1
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => x.Count());
+            var dct2 = new Dictionary<char, int>();
+            for (int i = 0; i < s2.Length; i++)
+            {
+                dct2.TryAdd(s2[i], 0);
+                dct2[s2[i]]++;
+
+                if (i < s1.Length - 1)
+                {
+                    continue;
+                }
+                else if (i > s1.Length - 1)
+                {
+                    int old = i - s1.Length;
+                    dct2[s2[old]]--;
+                }
+
+
+                bool full_match = true;
+                foreach (var kvp in dct1)
+                {
+                    if (!dct2.ContainsKey(kvp.Key) || dct2[kvp.Key] != kvp.Value)
+                    {
+                        full_match = false;
+                        break;
+                    }
+                }
+                if (full_match) { return true; }
+            }
+
+            return false;
+        }
+    }
+}
