1455d5aa56adc10c5a59d826dff7c70033dfd2bc
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:58:31 +0200

    feature: added 0476

---
+++ LeetCode/CSharp/0476-NumberComplement.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0476 : IRunProgram
+    {
+        public void Run()
+        {
+            FindComplement(5).Print();
+            FindComplement(1).Print();
+        }
+        public int FindComplement(int num)
+        {
+            int multi = 1;
+            int rest = 0;
+
+            while (multi + rest < num)
+            {
+                rest += multi;
+                multi <<= 1;
+            }
+            multi += rest;
+            return multi ^ num;
+        }
+    }
+}
