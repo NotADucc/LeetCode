46e23cf48088ab989731eb078f877455bc6e05f8
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 18:45:01 +0200

    TODO: problem 0110

---
+++ LeetCode/CSharp/0110-BalancedBinaryTree.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0110 : IRunProgram
+    {
+        public void Run()
+        {
+            IsBalanced(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))).Print();
+        }
+
+        public bool IsBalanced(TreeNode root)
+        {
+            // TODO
+            return false;
+        }
+    }
+}
