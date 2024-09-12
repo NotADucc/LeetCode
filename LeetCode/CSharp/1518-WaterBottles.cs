5571cc1d74f9d0fadd56ad11f2d407cafec5450b
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:15:50 +0200

    feature: added 1518

---
+++ LeetCode/CSharp/1518-WaterBottles.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1518 : IRunProgram
+    {
+        public void Run()
+        {
+            NumWaterBottles(9, 3).Print();
+        }
+        public int NumWaterBottles(int numBottles, int numExchange)
+        {
+            int output = numBottles;
+
+            while (numBottles >= numExchange)
+            {
+                int rest = numBottles % numExchange;
+                numBottles /= numExchange;
+                output += numBottles;
+                numBottles += rest;
+            }
+
+            return output;
+        }
+    }
+}
