510389e9f79cfdf7965bab14cecab88d4f245733
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:21:52 +0200

    feature: added 2220

---
+++ LeetCode/CSharp/2220-MinimumBitFlipsToConvertNumber.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2220 : IRunProgram
+    {
+        public void Run()
+        {
+            MinBitFlips(10, 7).Print();
+        }
+
+        public int MinBitFlips(int start, int goal)
+        {
+            return int.PopCount(start ^ goal);
+        }
+    }
+}
