41f8be4f86f913d80f953ec831722ea72699d5a4
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:07:54 +0200

    feature: added 2079

---
+++ LeetCode/CSharp/2079-WateringPlants.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2079 : IRunProgram
+    {
+        public void Run()
+        {
+            WateringPlants([2, 2, 3, 3], 5).Print();
+        }
+
+        public int WateringPlants(int[] plants, int capacity)
+        {
+            int currentWater = capacity;
+            int currentSteps = 0;
+
+            for (int i = 0; i < plants.Length; i++)
+            {
+                int plant = plants[i];
+                currentSteps++;
+
+                if (currentWater >= plant)
+                {
+                    currentWater -= plant;
+                    continue;
+                }
+
+                currentSteps += (Math.Max(i, 1) * 2);
+                currentWater = capacity - plant;
+            }
+
+            return currentSteps;
+        }
+    }
+}
