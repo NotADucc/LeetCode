4a0afb40db6691fb77c0a484e957ea5462a38db1
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:19:46 +0200

    feature: added 1598

---
+++ LeetCode/CSharp/1598-CrawlerLogFolder.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1598 : IRunProgram
+    {
+        public void Run()
+        {
+            MinOperations(["d1/", "d2/", "../", "d21/", "./"]).Print();
+        }
+        public int MinOperations(string[] logs)
+        {
+            var output = 0;
+            foreach (var log in logs)
+            {
+                if (log.Equals("../") && output > 0)
+                {
+                    output--;
+                }
+                else if (log.Equals("./") || (log.Equals("../") && output == 0)) { }
+                else
+                {
+                    output++;
+                }
+            }
+            return output;
+        }
+    }
+}
