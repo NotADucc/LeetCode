7c5a804173e783f4fbca8f45e8f1e9398df990f3
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:49:56 +0200

    feature: added 2974

---
+++ LeetCode/CSharp/2974-MinimumNumberGame.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2974 : IRunProgram
+    {
+        public void Run()
+        {
+            NumberGame([5, 4, 2, 3]).Print();
+        }
+
+        public int[] NumberGame(int[] nums)
+        {
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                nums[i] ^= nums[i + 1];
+                nums[i + 1] ^= nums[i];
+                nums[i] ^= nums[i + 1];
+            }
+
+            return nums;
+        }
+    }
+}
