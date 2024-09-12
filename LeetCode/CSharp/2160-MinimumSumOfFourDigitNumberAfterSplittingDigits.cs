0b695797c698e0572686c1cd8c8988cecd534e54
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:14:22 +0200

    feature: added 2160

---
+++ LeetCode/CSharp/2160-MinimumSumOfFourDigitNumberAfterSplittingDigits.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2160 : IRunProgram
+    {
+        public void Run()
+        {
+            MinimumSum(2932).Print();
+        }
+
+        public int MinimumSum(int num)
+        {
+            var temp = num.ToString().ToArray();
+            Array.Sort(temp);
+
+            int n1 = int.Parse($"{temp[0]}{temp[2]}");
+            int n2 = int.Parse($"{temp[1]}{temp[3]}");
+
+            return n1 + n2;
+        }
+    }
+}
