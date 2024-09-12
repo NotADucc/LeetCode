20e0759a763aee9428e83b9f2e729e112204f6ea
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:41:02 +0200

    feature: added 2529

---
+++ LeetCode/CSharp/2529-MaximumCountOfPositiveIntegerAndNegativeInteger.cs
@@ -0,0 +1,76 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2529 : IRunProgram
+    {
+        public void Run()
+        {
+            MaximumCount([-2, -1, -1, 1, 2, 3]).Print();
+        }
+        public int MaximumCount(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0] == 0 ? 0 : 1;
+            }
+            int neg_index = HelperNegative(nums);
+            int pos_index = HelperPositive(nums);
+
+            var neg_total = neg_index;
+            var pos_total = nums.Length - pos_index;
+
+
+            return Math.Max(neg_total, pos_total);
+        }
+
+        int HelperNegative(int[] arr)
+        {
+            int left = 0, right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int m = (left + right) / 2;
+
+                if (int.IsNegative(arr[m]))
+                {
+                    left = m + 1;
+                }
+                else
+                {
+                    right = m - 1;
+                }
+            }
+
+            return left;
+        }
+
+        int HelperPositive(int[] arr)
+        {
+            int left = 0, right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int m = (left + right) / 2;
+
+                if (int.IsPositive(arr[m]) && arr[m] != 0)
+                {
+                    right = m - 1;
+                }
+                else
+                {
+                    left = m + 1;
+                }
+            }
+
+            return left;
+        }
+    }
+}
