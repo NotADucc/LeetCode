71d92d5ba8e0572f024fc5c387fc5faee270dc02
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 21:48:21 +0200

    feature: added 1219

---
+++ LeetCode/CSharp/1219-PathWithMaximumGold.cs
@@ -0,0 +1,55 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1219 : IRunProgram
+    {
+        public void Run()
+        {
+            GetMaximumGold([[0, 6, 0], [5, 8, 7], [0, 9, 0]]).Print();
+        }
+        public int GetMaximumGold(int[][] grid)
+        {
+            int output = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] <= 0)
+                    {
+                        continue;
+                    }
+
+                    Helper(grid, i, j, ref output, 0);
+                }
+            }
+
+            return output;
+        }
+
+        private void Helper(int[][] grid, int i, int j, ref int output, int current)
+        {
+            if (i < 0 || j < 0 || i >= grid.Length || j >= grid[i].Length || grid[i][j] <= 0)
+            {
+                return;
+            }
+
+            int grid_val = grid[i][j];
+            current += grid_val;
+            output = Math.Max(output, current);
+
+            grid[i][j] = -1;
+            Helper(grid, i - 1, j, ref output, current);
+            Helper(grid, i + 1, j, ref output, current);
+            Helper(grid, i, j - 1, ref output, current);
+            Helper(grid, i, j + 1, ref output, current);
+            grid[i][j] = grid_val;
+        }
+    }
+}
