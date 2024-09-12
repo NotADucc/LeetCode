a3f807075be71c84f7f2f2f10b8aab72c848241b
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:00:28 +0200

    feature: added 0520

---
+++ LeetCode/CSharp/0520-DetectCapital.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0520 : IRunProgram
+    {
+        public void Run()
+        {
+            DetectCapitalUse("usa").Print();
+            DetectCapitalUse("USA").Print();
+        }
+        public bool DetectCapitalUse(string word)
+        {
+            return word.Equals(word.ToUpper()) || word[1..].Equals(word[1..].ToLower());
+        }
+    }
+}
