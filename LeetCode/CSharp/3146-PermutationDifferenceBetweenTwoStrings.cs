e80be05378ad88dc5df4782b9273ee866a9f8b0e
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:11:30 +0200

    feature: added 3146

---
+++ LeetCode/CSharp/3146-PermutationDifferenceBetweenTwoStrings.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution3146 : IRunProgram
+    {
+        public void Run()
+        {
+            FindPermutationDifference("abc", "bac").Print();
+        }
+
+        public int FindPermutationDifference(string s, string t)
+        {
+            int delta = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                delta += Math.Abs(t.IndexOf(s[i]) - i);
+            }
+
+            return delta;
+        }
+    }
+}
