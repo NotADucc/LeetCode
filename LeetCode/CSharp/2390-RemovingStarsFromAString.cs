f9a9140c1deb128f0f1d6b978b3a3a888d2c9c65
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:32:32 +0200

    feature: added 2390

---
+++ LeetCode/CSharp/2390-RemovingStarsFromAString.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2390 : IRunProgram
+    {
+        public void Run()
+        {
+            RemoveStars("leet**cod*e").Print();
+        }
+        public string RemoveStars(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (var ch in s)
+            {
+                if (ch == '*')
+                {
+                    if (stack.Count > 0) stack.Pop();
+                }
+                else
+                {
+                    stack.Push(ch);
+                }
+            }
+            Span<char> output = stackalloc char[stack.Count];
+            int i = stack.Count - 1;
+            while (stack.Count > 0)
+            {
+                output[i--] = stack.Pop();
+            }
+            return output.ToString();
+        }
+    }
+}
