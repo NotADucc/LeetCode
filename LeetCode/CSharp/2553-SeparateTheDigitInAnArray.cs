95c7044e7c173a793192ca03eaeb66717ac159a3
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 23:53:00 +0200

    feature: added 2553

---
+++ LeetCode/CSharp/2553-SeparateTheDigitInAnArray.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution2553 : IRunProgram
+    {
+        public void Run()
+        {
+            SeparateDigits([13, 25, 83, 77]).Print();
+        }
+
+        public int[] SeparateDigits(int[] nums)
+        {
+            var output = new List<int>();
+            int list_index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int add = 0;
+                while (nums[i] > 0)
+                {
+                    output.Insert(list_index, nums[i] % 10);
+                    nums[i] /= 10;
+                    add++;
+                }
+                list_index += add;
+            }
+
+            return output.ToArray();
+        }
+    }
+}
