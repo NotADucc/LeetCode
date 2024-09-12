abc9617128665dac184e0748aec12f76a0fc7c24
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:51:27 +0200

    feature: added 1046

---
+++ LeetCode/CSharp/1046-LastStoneWeight.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1046: IRunProgram
+    {
+        public void Run()
+        {
+            LastStoneWeight([2, 7, 4, 1, 8, 1]).Print();
+        }
+
+        public int LastStoneWeight(int[] stones)
+        {
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>(stones.Select(x => (x, x)), Comparer<int>.Create((a, b) => b - a));
+
+            while (queue.Count > 1)
+            {
+                int stone1 = queue.Dequeue();
+                int stone2 = queue.Dequeue();
+
+                int delta = stone1 - stone2;
+                if (delta != 0) { queue.Enqueue(delta, delta); }
+            }
+
+            return queue.Count > 0 ? queue.Peek() : 0;
+        }
+    }
+}
