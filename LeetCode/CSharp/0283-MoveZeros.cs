0ac1fb14f1b077ac8ce24ce6072da555ae788bef
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:37:29 +0200

    feature: added 0283

---
+++ LeetCode/CSharp/0283-MoveZeros.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0283 : IRunProgram
+    {
+        public void Run()
+        {
+            MoveZeroes([0, 1, 0, 3, 12]);
+            MoveZeroes([0]);
+        }
+
+        public void MoveZeroes(int[] nums)
+        {
+
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    bool was0 = nums[index] == 0;
+                    nums[index] = nums[i];
+                    nums[i] = was0 ? 0 : nums[i];
+                    index++;
+                }
+            }
+        }
+    }
+}
