a64a6bb1358f97875dc95aa2a7e64248e322f8ad
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:55:31 +0200

    feature: added 0455

---
+++ LeetCode/CSharp/0455-AssignCookies.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0455 : IRunProgram
+    {
+        public void Run()
+        {
+            FindContentChildren([1, 2, 3], [1, 1]).Print();
+        }
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+
+            int child = 0;
+            int cookie = 0;
+            while (child < g.Length && cookie < s.Length)
+            {
+                if (g[child] > s[cookie])
+                {
+                    cookie++;
+                    continue;
+                }
+
+                child++;
+                cookie++;
+            }
+
+            return child;
+        }
+    }
+}
