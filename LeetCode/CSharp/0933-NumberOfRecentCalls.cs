87599d90a183102e1ec13b60773ca32e82c22887
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:45:11 +0200

    feature: added 0933

---
+++ LeetCode/CSharp/0933-NumberOfRecentCalls.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0933: IRunProgram
+    {
+        public void Run()
+        {
+            
+        }
+    }
+
+    public class RecentCounter
+    {
+        Queue<int> counter;
+        public RecentCounter()
+        {
+            counter = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            int low = Math.Max(t - 3_000, 0);
+            while (counter.Count > 0 && counter.Peek() < low)
+            {
+                counter.Dequeue();
+            }
+            counter.Enqueue(t);
+            return counter.Count;
+        }
+    }
+}
