db006b976b42f21bfd24c72aed220a3b40c5d1b1
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 16:57:35 +0200

    feature: added 0050

---
+++ LeetCode/CSharp/0050-Pow.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    public class Solution0050 : IRunProgram
+    {
+        public void Run()
+        {
+            MyPow(2.0, 10).Print();
+            MyPow(4.0, 10).Print();
+            MyPow(1.0, 10).Print();
+            MyPow(-1.0, 10).Print();
+            MyPow(-1.0, 1).Print();
+            MyPow(-1.0, -1).Print();
+        }
+
+        public double MyPow(double x, int n)
+        {
+            if (x == 1 || x == -1) { return (n & 1) == 0 ? 1 : x; }
+            else if (n == int.MinValue) { return 0; }
+            double output = 1.0;
+            int new_n = n < 0 ? -n : n;
+            for (int i = 0; i < new_n; i++)
+            {
+                output *= x;
+            }
+            return n < 0 ? 1 / output : output;
+        }
+    }
+}
