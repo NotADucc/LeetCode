f95b1961d7e10bd70a96379946b8ed6d0a10c95e
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 21:59:00 +0200

    feature: added 1342

---
+++ LeetCode/CSharp/1342-NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1342 : IRunProgram
+    {
+        public void Run()
+        {
+            NumberOfSteps(2).Print();
+            NumberOfSteps(14).Print();
+        }
+        public int NumberOfSteps(int num)
+        {
+            int c = 0;
+            while (num != 0)
+            {
+                if ((num & 1) == 0)
+                {
+                    num = num >> 1;
+                }
+                else
+                {
+                    num--;
+                }
+                c++;
+            }
+            return c;
+        }
+    }
+}
