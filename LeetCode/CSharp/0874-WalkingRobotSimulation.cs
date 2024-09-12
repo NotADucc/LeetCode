176a0039856699678b25b881dcbeb3fffe96c6ad
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 20:40:24 +0200

    feature: added 0874

---
+++ LeetCode/CSharp/0874-WalkingRobotSimulation.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.CSharp.Shared;
+using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0874: IRunProgram
+    {
+        public void Run()
+        {
+            RobotSim([4, -1, 3], []);
+        }
+        private const int MAGIC_SIZE = 3;
+
+        public int RobotSim(int[] commands, int[][] obstacles)
+        {
+            int output = 0;
+            int direction = 0;
+            (int x, int y) curr_coord = (0, 0);
+            var set = new HashSet<(int, int)>();
+            foreach (var obstacle in obstacles)
+            {
+                set.Add((obstacle[0], obstacle[1]));
+            }
+            (int x, int y)[] changeCoords = new (int, int)[]
+            {
+            (0, 1),     // N
+            (1, 0),     // E
+            (0, -1),    // S
+            (-1, 0)     // W
+            };
+            foreach (var command in commands)
+            {
+                if (command > 0)
+                {
+                    for (int i = 0; i < command; i++)
+                    {
+                        curr_coord.x += changeCoords[direction].x;
+                        curr_coord.y += changeCoords[direction].y;
+                        if (set.Contains(curr_coord))
+                        {
+                            curr_coord.x -= changeCoords[direction].x;
+                            curr_coord.y -= changeCoords[direction].y;
+                            break;
+                        }
+                    }
+                    output = Math.Max(output, curr_coord.x * curr_coord.x + curr_coord.y * curr_coord.y);
+                }
+                else
+                {
+                    direction = command == -1 ? (direction + 1) % 4 : direction - 1 < 0 ? 3 : direction - 1;
+                }
+            }
+            return output;
+        }
+    }
+}
