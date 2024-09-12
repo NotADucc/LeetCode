99b81467857855ed9664224c6d96cabdae6d3244
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 00:15:40 +0200

    feature: added 3232

---
+++ LeetCode/CSharp/3232-FindIfDigitGameCanBeWon.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution3232 : IRunProgram
+    {
+        public void Run()
+        {
+            CanAliceWin([1, 2, 3, 4, 10]).Print();
+        }
+
+        public bool CanAliceWin(int[] nums)
+        {
+            int dig = 0;
+            int ddig = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= 10)
+                {
+                    ddig += nums[i];
+                }
+                else
+                {
+                    dig += nums[i];
+                }
+            }
+
+            return dig != ddig;
+        }
+    }
+}
