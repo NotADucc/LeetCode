76e9769155170671098ff67f82ea19de150fa87b
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:02:07 +0200

    feature: added 0162

---
+++ LeetCode/CSharp/0162-FindPeakElement.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0162 : IRunProgram
+    {
+        public void Run()
+        {
+            FindPeakElement([1, 2, 3, 1]).Print();
+            FindPeakElement([1, 2, 1, 3, 5, 6, 4]).Print();
+        }
+
+        public int FindPeakElement(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    return i;
+                }
+            }
+
+            return nums.Length - 1;
+        }
+    }
+}
