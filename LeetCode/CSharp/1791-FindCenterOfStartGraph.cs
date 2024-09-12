36d9e7042d163288c6527270923c29aa0a1b0437
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:37:44 +0200

    feature: added 1791

---
+++ LeetCode/CSharp/1791-FindCenterOfStartGraph.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1791 : IRunProgram
+    {
+        public void Run()
+        {
+            FindCenter([[1, 2], [2, 3], [4, 2]]).Print();
+        }
+
+        public int FindCenter(int[][] edges)
+        {
+            return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+        }
+    }
+}
