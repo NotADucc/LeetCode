22322e8f53f7c9c417934c7202f84b22b7926a35
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 19:05:56 +0200

    feature: added 0171

---
+++ LeetCode/CSharp/0171-ExcelSheetColumnNumber.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution0171 : IRunProgram
+    {
+        public void Run()
+        {
+            TitleToNumber("A").Print();
+            TitleToNumber("AB").Print();
+            TitleToNumber("ZZ").Print();
+            TitleToNumber("ZZA").Print();
+        }
+        public int TitleToNumber(string columnTitle)
+        {
+            int output = 0;
+
+            for (int i = columnTitle.Length - 1; i >= 0; i--)
+            {
+                output += (Math.Max(((int)Math.Pow(26, (columnTitle.Length - 1 - i))), 1)) * TransformChToInt(columnTitle[i]);
+            }
+
+            return output;
+        }
+
+        private int TransformChToInt(char ch) => ch - 64;
+    }
+}
