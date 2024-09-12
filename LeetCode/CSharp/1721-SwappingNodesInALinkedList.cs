8d2695ed56c28754ceb21dfc119dec7a423d3459
Guillaume Desktop <robertohelper@outlook.com>
2024-09-12 22:32:59 +0200

    feature: added 1721

---
+++ LeetCode/CSharp/1721-SwappingNodesInALinkedList.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CSharp
+{
+    internal class Solution1721 : IRunProgram
+    {
+        public void Run()
+        {
+            SwapNodes(ListNodeHelper.New(10), 3).Print();
+        }
+
+        public ListNode SwapNodes(ListNode head, int k)
+        {
+            var lst = new List<ListNode>();
+            var slow = head;
+            while (slow is not null)
+            {
+                lst.Add(slow);
+                slow = slow.next;
+            }
+
+            var n1 = lst[k - 1];
+            var n2 = lst[lst.Count - k];
+            var temp = n1.val;
+            n1.val = n2.val;
+            n2.val = temp;
+
+            return head;
+        }
+    }
+}
