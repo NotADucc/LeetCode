17d76eb7d52095196b19c63a3839e958396ded40
Guillaume Desktop <robertohelper@outlook.com>
2024-09-14 00:01:32 +0200

    feature: added 0389

---
+++ LeetCode/CSharp/0389-FindTheDifference.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0389 : IRunProgram
+{
+    public void Run()
+    {
+        FindTheDifference("abcd", "abcde").Print();
+        FindTheDifference("", "y").Print();
+    }
+
+    public char FindTheDifference(string s, string t)
+    {
+        (ReadOnlySpan<char> small, ReadOnlySpan<char> big) = s.Length < t.Length ? (s, t) : (t, s);
+
+        int output = 0;
+        for (int i = 0; i < small.Length; i++)
+        {
+            output += big[i];
+            output -= small[i];
+        }
+        output += big[^1];
+
+        return (char)output;
+    }
+}
