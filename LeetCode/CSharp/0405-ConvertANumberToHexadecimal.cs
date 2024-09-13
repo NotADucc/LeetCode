0f4640879f30083f531d8875f7b7d8b0a50b8f10
Guillaume Desktop <robertohelper@outlook.com>
2024-09-14 00:08:53 +0200

    feature: added 0405

---
+++ LeetCode/CSharp/0405-ConvertANumberToHexadecimal.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0405 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public string ToHex(int num) => num.ToString("x");
+}
