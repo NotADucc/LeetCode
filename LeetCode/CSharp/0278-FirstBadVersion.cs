bb3321ec2543e8dca131672a99ea1f027faff60d
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 14:14:58 +0200

    feature: added 0278

---
+++ LeetCode/CSharp/0278-FirstBadVersion.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0278 : IRunProgram
+{
+    public void Run()
+    {
+        FirstBadVersion(2126753390).Print();
+    }
+
+    public int FirstBadVersion(int n)
+    {
+        int left = 1, right = n - 1;
+        while (left <= right)
+        {
+            int mid = (int)(((long)left + right) >> 1);
+            bool is_bad = IsBadVersion(mid);
+            if (!is_bad)
+            {
+                left = mid + 1;
+            }
+            else 
+            { 
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    private bool IsBadVersion(int version) => version >= 1702766719;
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,6 +7,6 @@ public class Program
 {
     public static void Main()
     {
-        Runner.RunLeetCodeSolution<Solution0206>();
+        Runner.RunLeetCodeSolution<Solution0278>();
     }
 }
