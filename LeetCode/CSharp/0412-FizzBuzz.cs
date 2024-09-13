998081ec657662ad57f11d9e1522321fbea70653
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 16:28:06 +0200

    feature: added 0412

---
+++ LeetCode/CSharp/0412-FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Shared;
+using System.Text;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0412 : IRunProgram
+{
+    public void Run()
+    {
+        FizzBuzz(3).Print();
+    }
+
+    public IList<string> FizzBuzz(int n)
+    {
+        var output = new List<string>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0)
+            {
+                sb.Append("Fizz");
+            }
+            if (i % 5 == 0)
+            {
+                sb.Append("Buzz");
+            }
+            output.Add(sb.Length == 0 ? i.ToString() : sb.ToString());
+            sb.Clear();
+        }
+        return output;
+    }
+}
