830051253e7a93709c3fe42a3482177024cd54d7
Guillaume Desktop <robertohelper@outlook.com>
2024-09-14 00:34:02 +0200

    feature: added 0434

---
+++ LeetCode/CSharp/0434-NumberOfSegmentsInAString.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0434 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+    public int CountSegments(string s)
+    {
+        return s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Count();
+    }
+}
