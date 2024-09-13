2c3a987aaa4f436ec89e6b9a9ea25ed6a7a3a4bd
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 19:56:39 +0200

    feature: added benchmark setup for csharp

---
+++ LeetCode/CSharp/Benchmark/Benchmarkattributes.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+
+namespace LeetCode.CSharp.Benchmark;
+
+[MemoryDiagnoser]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+[CategoriesColumn]
+[Config(typeof(Config))]
+public class BenchmarkAttributes { }
---
+++ LeetCode/CSharp/Benchmark/Config.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+
+namespace LeetCode.CSharp.Benchmark;
+
+public sealed class Config : ManualConfig
+{
+    public Config()
+    {
+        SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithRatioStyle(RatioStyle.Trend);
+    }
+}
