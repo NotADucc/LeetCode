0fb87ccf1d51ef8e5e98caf95b2e38e12284a67d
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 02:13:39 +0200

    feature: added 1310, bruteforce

---
+++ LeetCode/CSharp/1310-XORQueriesOfASubarray.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution1310 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public int[] XorQueries(int[] arr, int[][] queries)
+    {
+        int[] output = new int[queries.Length];
+
+        for (int i = 0; i < queries.Length; i++)
+        {
+            for (int j = queries[i][0]; j <= queries[i][1]; j++)
+            {
+                output[i] ^= arr[j];
+            }
+        }
+
+        return output;
+    }
+}
