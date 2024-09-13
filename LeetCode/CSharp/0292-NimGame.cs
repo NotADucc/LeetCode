693f7a0d7995e641986ff3e6f14dc7537d98fe76
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 14:25:50 +0200

    feature: added 0292

---
+++ LeetCode/CSharp/0292-NimGame.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0292 : IRunProgram
+{
+    public void Run()
+    {
+        CanWinNim(1).Print();
+        CanWinNim(2).Print();
+        CanWinNim(3).Print();
+        CanWinNim(4).Print();
+        CanWinNim(9).Print();
+        CanWinNim(90).Print();
+    }
+
+    public bool CanWinNim(int n)
+    {
+        return n % 4 != 0;
+    }
+}
