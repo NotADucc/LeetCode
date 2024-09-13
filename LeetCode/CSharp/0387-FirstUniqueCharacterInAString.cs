c2504800ed269b0fdc26b3d881ca6786d5dddc3c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 17:36:00 +0200

    feature: added 0387

--- LeetCode/CSharp/0387-FirstUniqueCharacterInAString.cs
+++ LeetCode/CSharp/0387-FirstUniqueCharacterInAString.cs
@@ -6,11 +6,21 @@ internal class Solution0387 : IRunProgram
 {
     public void Run()
     {
-        
+        FirstUniqChar("leetcode").Print();
+        FirstUniqChar("aabb").Print();
     }
-
     public int FirstUniqChar(string s)
     {
+        Dictionary<char, int> freq = new Dictionary<char, int>();
 
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!freq.TryAdd(s[i], i))
+            {
+                freq[s[i]] = -1;
+            }
+        }
+
+        return freq.FirstOrDefault(x => x.Value > -1, defaultValue: new KeyValuePair<char, int>('&', -1)).Value;
     }
 }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,6 +7,6 @@ public class Program
 {
     public static void Main()
     {
-        Runner.RunLeetCodeSolution<Solution0367>();
+        Runner.RunLeetCodeSolution<Solution0387>();
     }
 }
