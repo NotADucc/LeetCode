098ded0f4caf20c7df67432d74dd638686456f75
Guillaume Desktop <robertohelper@outlook.com>
2024-09-13 15:54:55 +0200

    feature: added 0342, didnt like

---
+++ LeetCode/CSharp/0342-PowerOfFour.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0342 : IRunProgram
+{
+    public void Run()
+    {
+        Math.Log(4, 4).Print();
+        Math.Pow(1, 4).Print();
+    }
+
+    public bool IsPowerOfFour(int n)
+    {
+        return Math.Pow(Math.Ceiling(Math.Log(n, 4)), 4) == n && n != 0;
+    }
+}
