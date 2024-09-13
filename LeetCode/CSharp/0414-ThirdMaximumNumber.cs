33f7a4b74e0858f2f2d7f08fd7be88187bfd6721
Guillaume Desktop <robertohelper@outlook.com>
2024-09-14 00:15:39 +0200

    feature: added 0414

---
+++ LeetCode/CSharp/0414-ThirdMaximumNumber.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0414 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public int ThirdMax(int[] nums)
+    {
+        nums = nums.Distinct().ToArray();
+        Array.Sort(nums);
+        if (nums.Length < 3) return nums[^1];
+
+        return nums[^3];
+    }
+}
