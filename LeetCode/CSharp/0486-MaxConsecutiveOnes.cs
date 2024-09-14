63bef61e91826498ab1dc2565ac71cb61468f60f
Guillaume Desktop <robertohelper@outlook.com>
2024-09-14 16:09:59 +0200

    feature: added 0485

---
+++ LeetCode/CSharp/0486-MaxConsecutiveOnes.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0485 : IRunProgram
+{
+    public void Run()
+    {
+        FindMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]).Print();
+        FindMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]).Print();
+        FindMaxConsecutiveOnes([0, 1, 1, 0, 1]).Print();
+        FindMaxConsecutiveOnes([1, 1, 1, 1, 1]).Print();
+    }
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int output = 0, current = 0; const int MAX = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            current = nums[i] == 1 ? current + 1 : 0;
+            output = Math.Max(output, current);
+        }
+        return output;
+    }
+}
