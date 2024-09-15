82233f255f35e4d9ccfff710867d71a4813328a6
Guillaume Desktop <robertohelper@outlook.com>
2024-09-15 18:57:26 +0200

    TODO: 0334

---
+++ LeetCode/CSharp/0334-IncreasingTripletSubsequence.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0334 : IRunProgram
+{
+    public void Run()
+    {
+        IncreasingTriplet([1, 2, 3, 4, 5]).Print();
+        IncreasingTriplet([5, 4, 3, 2, 1]).Print();
+        IncreasingTriplet([2, 1, 5, 0, 4, 6]).Print();
+    }
+
+    public bool IncreasingTriplet(int[] nums)
+    {
+        // TODO
+        return false;
+    }
+}
