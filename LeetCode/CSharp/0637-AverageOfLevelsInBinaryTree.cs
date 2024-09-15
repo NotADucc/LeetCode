07453fd3f6e4e70c2fcfdfcd247af9fca8769808
Guillaume Desktop <robertohelper@outlook.com>
2024-09-15 22:49:12 +0200

    feature: added 0637

---
+++ LeetCode/CSharp/0637-AverageOfLevelsInBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0637 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public IList<double> AverageOfLevels(TreeNode root)
+    {
+        List<long[]> output = new List<long[]>();
+        Helper(root, output, 0);
+        return output.Select(i => (double)i[0] / i[1]).ToList();
+    }
+
+    private void Helper(TreeNode root, List<long[]> lst, int index) 
+    {
+        if (root is null) return;
+        if (index >= lst.Count) lst.Add(new long[2]);
+
+        lst[index][0] += root.val;
+        lst[index][1]++;
+        Helper(root.left, lst, index + 1);
+        Helper(root.right, lst, index + 1);
+    }
+}
