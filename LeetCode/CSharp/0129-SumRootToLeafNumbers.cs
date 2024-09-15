b9eacbb4c4e8fbeb202e6a37e247a9f46f5e7dfa
Guillaume Desktop <robertohelper@outlook.com>
2024-09-15 23:22:04 +0200

    feature: added 0129

---
+++ LeetCode/CSharp/0129-SumRootToLeafNumbers.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.CSharp;
+
+internal class Solution0129 : IRunProgram
+{
+    public void Run()
+    {
+        SumNumbers(new TreeNode(1, new TreeNode(1))).Print();
+        SumNumbers(new TreeNode(1, new TreeNode(2), new TreeNode(3))).Print();
+    }
+
+    public int SumNumbers(TreeNode root)
+    {
+        if (root.left is null && root.right is null) return root.val;
+        return Helper(root.left, root.val) + Helper(root.right, root.val);
+    }
+
+    private int Helper(TreeNode root, int curr)
+    {
+        if (root is null) return 0;
+        curr = curr * 10 + root.val;
+        if (root.left is null && root.right is null) return curr;
+        return Helper(root.left, curr) + Helper(root.right, curr);
+    }
+}
