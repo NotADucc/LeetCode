cc814b66fe0b7d2af694f8cbaa0604432fc398cf
Guillaume Desktop <robertohelper@outlook.com>
2024-09-18 00:37:16 +0200

    feature: added 0189

---
+++ CSharp/LeetCode/Solutions/0189-rotate-array.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0189 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public void Rotate(int[] nums, int k)
+    {
+        k = k % nums.Length;
+        int end = nums.Length - k;
+        int[] arr = [.. nums[end..], .. nums[..end]];
+        Array.Copy(arr, nums, arr.Length);
+    }
+}
