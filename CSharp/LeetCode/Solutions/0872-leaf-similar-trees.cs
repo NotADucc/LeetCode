c17b8ef7136315cb92b171e04eab503bd0c3503d
Guillaume Desktop <robertohelper@outlook.com>
2024-09-17 19:42:49 +0200

    feature: added 0872

---
+++ CSharp/LeetCode/Solutions/0872-leaf-similar-trees.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0872 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        return GetLeafs(root1).SequenceEqual(GetLeafs(root2));
+    }
+    public IEnumerable<int> GetLeafs(TreeNode root)
+    {
+        if (root is not null)
+        {
+            if (root.left is null && root.right is null) yield return root.val;
+
+            if (root.left is not null)
+            {
+                foreach (var leaf in GetLeafs(root.left))
+                {
+                    yield return leaf;
+                }
+            }
+
+            if (root.right is not null)
+            {
+                foreach (var leaf in GetLeafs(root.right))
+                {
+                    yield return leaf;
+                }
+            }
+        }
+    }
+}
