cdd8fd65d5b2ab1a557f052b05b97874ac0f968c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-17 14:38:20 +0200

    feature: added Solution3042

---
+++ CSharp/LeetCode/Solutions/3042-count-prefix-and-suffix-pairs-i.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution3042 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public int CountPrefixSuffixPairs(string[] words)
+    {
+        int total = 0;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = i + 1; j < words.Length; j++)
+            {
+                if (words[j].IndexOf(words[i]) == 0 && words[j].LastIndexOf(words[i]) == words[j].Length - words[i].Length)
+                {
+                    total++;
+                }
+            }
+        }
+
+        return total;
+    }
+}
