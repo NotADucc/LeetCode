2174ace4f139b94874e679577997b954a216a3be
Guillaume Desktop <robertohelper@outlook.com>
2024-09-18 01:52:09 +0200

    feature: added 0530

---
+++ CSharp/LeetCode/Solutions/0530-minimum-absolute-difference-in-bst.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions
+{
+    internal class Solution0530 : IRunProgram
+    {
+        public void Run()
+        {
+
+        }
+
+        public int GetMinimumDifference(TreeNode root)
+        {
+            int output = int.MaxValue;
+            var lst = new List<int>();
+            Helper(root, lst);
+            for (int i = 0; i < lst.Count - 1; i++)
+            {
+                output = Math.Min(output, lst[i + 1] - lst[i]);
+            }
+            return output;
+        }
+        public void Helper(TreeNode root, List<int> lst)
+        {
+            if (root is null) return;
+            Helper(root.left, lst);
+            lst.Add(root.val);
+            Helper(root.right, lst);
+        }
+    }
+}
