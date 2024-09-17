7f874ec2d9376ac62d5840f64f5b3cf41ae699a5
Guillaume Desktop <robertohelper@outlook.com>
2024-09-17 18:38:37 +0200

    feature: added 1493

---
+++ CSharp/LeetCode/Solutions/1493-longest-subarray-of-1s-after-deleting-one-element.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1493 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public int LongestSubarray(int[] nums)
+    {
+        Span<int> freqs = stackalloc int[2];
+        int l = 0, r = 0, output = 0, k = 1;
+        while (r <= nums.Length)
+        {
+            if (freqs[0] <= k)
+            {
+                if (r < nums.Length) freqs[nums[r]]++;
+                r++;
+                output = Math.Max(output, r - 2 - l);
+            }
+            else
+            {
+                while (freqs[0] > k)
+                {
+                    freqs[nums[l++]]--;
+                }
+            }
+        }
+        return output;
+    }
+}
