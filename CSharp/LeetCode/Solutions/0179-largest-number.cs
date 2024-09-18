a15a708c2d01af78b0a4692693de45b2821d443c
Guillaume Desktop <robertohelper@outlook.com>
2024-09-18 20:37:39 +0200

    feature: added 0179

---
+++ CSharp/LeetCode/Solutions/0179-largest-number.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Shared;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0179 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public static string LargestNumber(int[] nums)
+    {
+        string[] temp = nums
+            .Select(x => x.ToString())
+            .ToArray();
+
+        Array.Sort(temp, (x, y) => string.Compare(y + x, x + y));
+
+        Console.WriteLine(string.Join(", ", temp));
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < temp.Length; i++)
+        {
+            sb.Append(temp[i]);
+        }
+        return sb.ToString();
+    }
+}
