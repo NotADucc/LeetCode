722621f3fd7dc288750601e4ca4c5f1c7f8677da
Guillaume Desktop <robertohelper@outlook.com>
2024-09-18 20:37:00 +0200

    feature: added 0746

---
+++ CSharp/LeetCode/Solutions/0746-min-cost-climbing-stairs.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0746 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        int[] n_cost = new int[cost.Length + 1];
+        Array.Copy(cost, n_cost, cost.Length);
+
+        for (int i = n_cost.Length - 4; i >= 0; i--)
+        {
+            n_cost[i] += Math.Min(n_cost[i + 1], n_cost[i + 2]);
+        }
+
+        return Math.Min(n_cost[0], n_cost[1]);
+    }
+}
