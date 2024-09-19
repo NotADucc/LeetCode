0db94bfa105f73f2978d43e524cf88ea6193082f
Guillaume Desktop <robertohelper@outlook.com>
2024-09-19 21:57:29 +0200

    feature: added 1232

---
+++ CSharp/LeetCode/Solutions/1232-check-if-it-is-a-straight-line.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1232 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public bool CheckStraightLine(int[][] coordinates)
+    {
+        if (coordinates.Length < 3) return true;
+        int x0 = coordinates[0][0], x1 = coordinates[1][0];
+        int y0 = coordinates[0][1], y1 = coordinates[1][1];
+        for (int i = 2; i < coordinates.Length; i++)
+        {
+            int x2 = coordinates[i][0], y2 = coordinates[i][1];
+            if ((y2 - y1) * (x1 - x0) != (y1 - y0) * (x2 - x1)) return false;
+        }
+        return true;
+    }
+}
