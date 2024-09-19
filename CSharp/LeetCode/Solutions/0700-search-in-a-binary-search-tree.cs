b2b4a07e99b6a240932dbe028340efa56491ddf2
Guillaume Desktop <robertohelper@outlook.com>
2024-09-19 22:06:11 +0200

    feature: added 0700

---
+++ CSharp/LeetCode/Solutions/0700-search-in-a-binary-search-tree.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0700 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        if (root is null) return null;
+
+        if (root.val == val) return root;
+
+        return root.val > val ? SearchBST(root.left, val) : SearchBST(root.right, val);
+    }
+
+    public TreeNode SearchBSTBad(TreeNode root, int val)
+    {
+        if (root is null) return null;
+
+        if (root.val == val) return root;
+
+        return SearchBST(root.left, val) ?? SearchBST(root.right, val);
+    }
+}
