8738fee3c5778528ca55c618c4061493b3a4e943
Guillaume Desktop <robertohelper@outlook.com>
2024-09-19 21:13:05 +0200

    feature: added 0448

---
+++ CSharp/LeetCode/Solutions/0448-find-all-numbers-disappeared-in-an-array.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0448 : IRunProgram
+{
+    public void Run()
+    {
+        FindDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]).Print();
+        FindDisappearedNumbers([1, 1]).Print();
+    }
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        List<int> output = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int num = Math.Abs(nums[i]) - 1;
+            if (nums[num] > 0) nums[num] = -nums[num];
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+        }
+
+        Console.WriteLine(string.Join(", ", nums));
+
+        return output;
+    }
+
+}
