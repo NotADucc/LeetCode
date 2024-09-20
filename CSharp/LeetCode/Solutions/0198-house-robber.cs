0404a1c54cd78dc645ee1f8ad733dc954ccd4dc6
Guillaume Desktop <robertohelper@outlook.com>
2024-09-21 00:32:18 +0200

    feature: added 0198

---
+++ CSharp/LeetCode/Solutions/0198-house-robber.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0198 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int Rob(int[] nums)
+    {
+        int rob1 = 0, rob2 = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int temp = Math.Max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        return rob2;
+    }
+}
