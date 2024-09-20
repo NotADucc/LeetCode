cf47ccd2bcdecc38e37bbfafa89e8202ff1c7a8e
Guillaume Desktop <robertohelper@outlook.com>
2024-09-20 19:04:16 +0200

    feature: added 214

--- CSharp/LeetCode/Program.cs
+++ CSharp/LeetCode/Program.cs
@@ -8,6 +8,6 @@ public class Program
     public static void Main()
     {
         //BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmark0003>();
-        Runner.RunLeetCodeSolution<Solution0448>();
+        Runner.RunLeetCodeSolution<Solution0214>();
     }
 }
---
+++ CSharp/LeetCode/Solutions/0214-shortest-palindrome.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Solutions.Shared;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0214 : IRunProgram
+{
+    public void Run()
+    {
+        ShortestPalindrome("aacecaaa").Print();
+    }
+
+    public string ShortestPalindrome(string s)
+    {
+        int l = 0, r = s.Length;
+        ReadOnlySpan<char> span = s;
+        ReadOnlySpan<char> reversed = new string(s.Reverse().ToArray());
+        while (r > 0)
+        {
+            ReadOnlySpan<char> slice1 = span.Slice(0, r);
+            ReadOnlySpan<char> slice2 = reversed.Slice(l, reversed.Length - l);
+            if (IsEqual(slice1, slice2))
+            {
+                break;
+            }
+            else
+            {
+                l++; r--;
+            }
+        }
+
+        return reversed.Slice(0, l).ToString() + s;
+    }
+
+    private bool IsEqual(ReadOnlySpan<char> s1, ReadOnlySpan<char> s2)
+    {
+        for (int i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] != s2[i]) return false;
+        }
+        return true;
+    }
+}
