119e0c5e7253430742f8542a58d4e1f969b3ff34
Guillaume Desktop <robertohelper@outlook.com>
2024-09-21 03:02:12 +0200

    feature: added 0386

---
+++ CSharp/LeetCode/Solutions/0386-lexicographical-numbers.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0386 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public IList<int> LexicalOrder(int n)
+    {
+        IList<int> output = new List<int>(n);
+        DFS(output, 1, n);
+        return output;
+    }
+    private void DFS(IList<int> output, int current, int n)
+    {
+        if (current <= n)
+        {
+            output.Add(current);
+            DFS(output, current * 10, n);
+            for (int i = 1; i < 10; i++)
+            {
+                int temp = current + i;
+                if (temp > n || output.Count >= n) break;
+                output.Add(temp);
+                DFS(output, temp * 10, n);
+            }
+        }
+    }
+}
