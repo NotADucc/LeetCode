7d3ca41cd936ac071452e311749e3fdaac043e4b
Guillaume Desktop <robertohelper@outlook.com>
2024-09-23 00:06:41 +0200

    feature: added 3295

---
+++ CSharp/LeetCode/Solutions/3295-report-span-message.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution3295 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public bool ReportSpam(string[] message, string[] bannedWords)
+    {
+        HashSet<string> set = new HashSet<string>(bannedWords);
+        int count = 0;
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            if (set.Contains(message[i]))
+            {
+                count++;
+                if (count > 1) return true;
+            }
+        }
+
+        return false;
+    }
+}
