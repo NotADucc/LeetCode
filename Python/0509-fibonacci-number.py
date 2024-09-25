bcd49ea56539b9bda348f4ae1899ca3e8fb28f54
Guillaume Laptop <robertohelper@outlook.com>
2024-09-25 13:31:53 +0200

    feature: added 0509 python

---
+++ Python/0509-fibonacci-number.py
@@ -0,0 +1,13 @@
+def fib(self, n: int) -> int:
+	dp = [0] * (n + 1)
+	return self.rec(n, dp)
+
+def rec(self, n: int, dp: List[int]):
+	if n == 0 :
+		return 0
+	elif n == 1 :
+		return 1
+	elif dp[n - 1] == 0 :
+		dp[n - 1] = self.rec(n - 1, dp) + self.rec(n - 2, dp)
+		
+	return dp[n - 1]
