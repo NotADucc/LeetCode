5facc927f8eddba9e6d1d97ab73275701c9a5305
Guillaume Desktop <robertohelper@outlook.com>
2024-09-27 23:42:43 +0200

    refactor: 0731, removed extra list

--- CSharp/LeetCode/Solutions/0731-my-calendar-ii.cs
+++ CSharp/LeetCode/Solutions/0731-my-calendar-ii.cs
@@ -11,41 +11,22 @@ internal class Solution0731 : IRunProgram
     internal class MyCalendarTwo
     {
         private List<(int, int)> book = new List<(int, int)>(1001);
-        public MyCalendarTwo()
-        {
-
-        }
+        public MyCalendarTwo() { }
 
         public bool Book(int start, int end)
         {
-            List<(int, int)> temp = new List<(int, int)>();
+            bool Compare(int s1, int s2, int e1, int e2) => (s1 < s2 && e1 > s2) || (s1 < e2 && e1 >= e2) || (s1 >= s2 && e1 <= e2);
             for (int i = 0; i < book.Count; i++)
             {
-                (int booked_start, int booked_end) = book[i];
-                if (
-                    (start < booked_start && end > booked_start)
-                || (start < booked_end && end >= booked_end)
-                || (start >= booked_start && end <= booked_end)
-                )
+                (int s1, int e1) = book[i];
+                if (Compare(start, s1, end, e1))
                 {
-                    temp.Add((booked_start, booked_end));
-                    if (temp.Count > 1)
+                    for (int j = i + 1; j < book.Count; j++)
                     {
-                        for (int j = 0; j < temp.Count; j++)
+                        (int s2, int e2) = book[j];
+                        if (Compare(s1, s2, e1, e2) && Compare(start, s2, end, e2))
                         {
-                            (int s1, int e1) = temp[j];
-                            for (int k = j + 1; k < temp.Count; k++)
-                            {
-                                (int s2, int e2) = temp[k];
-                                if (
-                                    (s1 < s2 && e1 > s2)
-                                || (s1 < e2 && e1 >= e2)
-                                || (s1 >= s2 && e1 <= e2)
-                                )
-                                {
-                                    return false;
-                                }
-                            }
+                            return false;
                         }
                     }
                 }
@@ -55,5 +36,4 @@ internal class Solution0731 : IRunProgram
             return true;
         }
     }
-
 }
