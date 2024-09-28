89286d4917e838c7113545d72309af1f1ec4cc23
Guillaume Desktop <robertohelper@outlook.com>
2024-09-28 12:25:07 +0200

    feature: added 0641

---
+++ CSharp/LeetCode/Solutions/0641-design-circular-deque.cs
@@ -0,0 +1,64 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0641 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    internal class MyCircularDeque
+    {
+        private LinkedList<int> lst = new LinkedList<int>();
+        private int max = 0;
+        public MyCircularDeque(int k)
+        {
+            max = k;
+        }
+
+        public bool InsertFront(int value)
+        {
+            if (IsFull()) return false;
+            lst.AddFirst(value);
+            return true;
+        }
+
+        public bool InsertLast(int value)
+        {
+            if (IsFull()) return false;
+            lst.AddLast(value);
+            return true;
+        }
+
+        public bool DeleteFront()
+        {
+            if (IsEmpty()) return false;
+            lst.RemoveFirst();
+            return true;
+        }
+
+        public bool DeleteLast()
+        {
+            if (IsEmpty()) return false;
+            lst.RemoveLast();
+            return true;
+        }
+
+        public int GetFront()
+        {
+            if (IsEmpty()) return -1;
+            return lst.First!.Value;
+        }
+
+        public int GetRear()
+        {
+            if (IsEmpty()) return -1;
+            return lst.Last!.Value;
+        }
+
+        public bool IsEmpty() => lst.Count == 0;
+
+        public bool IsFull() => lst.Count >= max;
+    }
+}
