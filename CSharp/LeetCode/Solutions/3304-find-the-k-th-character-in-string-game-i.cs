0497923fadaa25d476f403db3615c5545cf494c2
Guillaume Desktop <robertohelper@outlook.com>
2024-09-29 12:04:53 +0200

    feature: added 3304

---
+++ CSharp/LeetCode/Solutions/3304-find-the-k-th-character-in-string-game-i.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Solutions.Shared;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+internal class Solution3304 : IRunProgram
+{
+    public void Run()
+    {
+        KthCharacter(5).Print();
+    }
+    public char KthCharacter(int k)
+    {
+        StringBuilder sb = new StringBuilder("a", capacity: k);
+        while (sb.Length < k)
+        {
+            sb.Append(string.Join("", sb.ToString().Select(Helper)));
+        }
+        return sb[k - 1];
+    }
+    private char Helper(char ch) => ch == 'z' ? 'a' : (char)(ch + 1);
+}
