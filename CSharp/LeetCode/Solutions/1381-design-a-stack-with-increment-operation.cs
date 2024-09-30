d4adae0017e8501c7cbc70a9b1faa667361a7725
Guillaume Desktop <robertohelper@outlook.com>
2024-09-30 16:17:03 +0200

    feature: added 1381, close #19

---
+++ CSharp/LeetCode/Solutions/1381-design-a-stack-with-increment-operation.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1381 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+    internal class CustomStack
+    {
+        private int[] arr;
+        private int max;
+        private int curr;
+        public CustomStack(int maxSize)
+        {
+            curr = -1;
+            max = maxSize;
+            arr = new int[max];
+        }
+
+        public void Push(int x)
+        {
+            if (curr + 1 >= max) return;
+            arr[curr + 1] = x;
+            curr++;
+        }
+
+        public int Pop()
+        {
+            if (curr < 0) return -1;
+            return arr[curr--];
+        }
+
+        public void Increment(int k, int val)
+        {
+            int len = Math.Min(k, max);
+            for (int i = 0; i < len; i++)
+            {
+                arr[i] += val;
+            }
+        }
+    }
+}
