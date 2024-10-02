402c4715b100c971fb8dec7e8069141ac2f22541
Guillaume Laptop <robertohelper@outlook.com>
2024-10-02 13:35:54 +0200

    feature: added 1331, close #20

---
+++ CSharp/LeetCode/Solutions/1331-rank-transform-of-an-array.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1331 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int[] ArrayRankTransform(int[] arr) 
+    {
+        // value: index
+        // prio: val
+        var q = new PriorityQueue<int, int>();
+        for(int i = 0; i < arr.Length; i++)
+        {
+            q.Enqueue(i, arr[i]);
+        }
+        int last_element = int.MaxValue;
+        int rank = 0;
+        while(q.Count > 0)
+        {
+            int i = q.Dequeue();
+            if (last_element != arr[i])
+            {
+                ++rank;
+            }
+            last_element = arr[i];
+            arr[i] = rank;
+        }
+
+        return arr;
+    }
+}
