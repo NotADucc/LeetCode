063be6819982378e0d7619b7e203ccd2b826456f
Guillaume Laptop <robertohelper@outlook.com>
2024-10-04 10:38:25 +0200

    feature: added 2491, close #21

---
+++ CSharp/LeetCode/Solutions/2491-divide-players-into-teams-of-equal-skill.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2491 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public long DividePlayers(int[] skill) 
+    {
+        Array.Sort(skill);
+        int l = 0, r = skill.Length - 1;
+        long output = 0;
+        int lvl = skill[l] + skill[r];
+        while(l < r)
+        {
+            if (skill[l] + skill[r] != lvl)
+            {
+                return -1;
+            }
+            output += skill[l] * skill[r];
+            l++;
+            r--;
+        }
+        return output;
+    }
+}
