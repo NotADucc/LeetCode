952de4cb698fc15fdb2bfd8040a6368101c93efe
Guillaume Laptop <robertohelper@outlook.com>
2024-10-07 08:13:46 +0200

    feature: added 2696 cs, close #22

---
+++ CSharp/LeetCode/Solutions/2696-minimum-string-length-after-removing-substrings.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2696 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MinLength(string s) 
+    {
+        bool changed = true;
+        
+        while (changed)
+        {
+            changed = false;
+            string temp = s.Replace("AB", "");
+            if (temp.Length != s.Length)
+            {
+                changed = true;
+                s = temp;
+            }
+            temp = s.Replace("CD", "");
+            if (temp.Length != s.Length)
+            {
+                changed = true;
+                s = temp;
+            }
+        }
+
+        return s.Length; 
+    }
+}
