d4d1457b2218bab262984cc53a6d3db033f54f1f
Guillaume Desktop <robertohelper@outlook.com>
2024-10-08 21:25:28 +0200

    refactor: cleaned up 0148

--- CSharp/LeetCode/Solutions/0148-sort-list.cs
+++ CSharp/LeetCode/Solutions/0148-sort-list.cs
@@ -10,65 +10,47 @@ internal class Solution0148 : IRunProgram
     }
     public ListNode SortList(ListNode head)
     {
-        if (head is null || head.next is null) return head;
-        (ListNode l, ListNode r) = Split(head);
-        return Merge(SortList(l), SortList(r));
+        if (head is null || head.next is null)
+            return head;
+
+        ListNode middle = GetMiddle(head);
+        ListNode left = head;
+        ListNode right = middle.next;
+        middle.next = null;
+
+        return Merge(SortList(left), SortList(right));
     }
-    private (ListNode, ListNode) Split(ListNode head)
+    private ListNode GetMiddle(ListNode head)
     {
-        ListNode slowwer = head;
         ListNode slow = head;
-        ListNode fast = head;
-        while (fast is not null && fast.next is not null)
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null)
         {
-            if (fast.next.next is not null) slowwer = slowwer.next;
             slow = slow.next;
             fast = fast.next.next;
         }
-        if (fast is not null) slow = slow.next;
-        slowwer.next = null;
-        return (head, slow);
+        return slow;
     }
     private ListNode Merge(ListNode left, ListNode right)
     {
         var head = new ListNode();
-        var curr = head;
+        var tail = head;
         while (left is not null && right is not null)
         {
             if (left.val < right.val)
             {
-                var temp = left.next;
-                left.next = null;
-                curr.next = left;
-                left = temp;
+                tail.next = left;
+                left = left.next;
             }
             else
             {
-                var temp = right.next;
-                right.next = null;
-                curr.next = right;
-                right = temp;
+                tail.next = right;
+                right = right.next;
             }
-            curr = curr.next;
+            tail = tail.next;
         }
 
-        while (left is not null)
-        {
-            var temp = left.next;
-            left.next = null;
-            curr.next = left;
-            left = temp;
-            curr = curr.next;
-        }
-
-        while (right is not null)
-        {
-            var temp = right.next;
-            right.next = null;
-            curr.next = right;
-            right = temp;
-            curr = curr.next;
-        }
+        tail.next = left is not null ? left : right!;
 
         return head.next;
     }
