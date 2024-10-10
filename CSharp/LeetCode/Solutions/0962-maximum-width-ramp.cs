9bd8004457a2f22e136a1e420793f36fb3b99755
Guillaume Laptop <robertohelper@outlook.com>
2024-10-10 08:17:08 +0200

    feature: added 0962, slow

---
+++ CSharp/LeetCode/Solutions/0962-maximum-width-ramp.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0962 : IRunProgram
+{
+    public void Run()
+    {
+		
+    }
+
+    public int MaxWidthRamp(int[] nums) 
+    {   
+        int output = 0;
+        for(int i = 0; i <nums.Length; i++)
+        {
+            if (output >= nums.Length - i) break;
+            for(int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] <= nums[j])
+                {
+                    output = Math.Max(output, j - i);
+                }
+            }
+        }
+        return output;
+    }
+}
