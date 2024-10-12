f00ca1e6d37b97a0374b6c9c7a3aaa24dc2066e1
Guillaume Desktop <robertohelper@outlook.com>
2024-10-12 19:08:54 +0200

    feature: added 3314 and 3315

---
+++ CSharp/LeetCode/Solutions/3315-construct-the-minimum-bitwise-array-ii.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution3315 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int[] MinBitwiseArray(IList<int> nums)
+    {
+        int[] output = new int[nums.Count];
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            int num = nums[i];
+            if ((num & 1) == 0)
+            { 
+                output[i] = -1;
+                continue;
+            }
+            for (int j = 31; j >= 0; j--)
+            {
+                int shift = 1 << j;
+                if (shift > num) continue;
+                int temp = num - shift;
+                int or = temp | (temp + 1);
+                if (or != num) continue;
+
+                output[i] = temp;
+                break;
+            }
+        }
+
+        return output;
+    }
+}
