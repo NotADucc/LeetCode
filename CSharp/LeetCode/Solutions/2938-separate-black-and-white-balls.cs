019b2390e561e1e760ce5b7bb040261d701beace
Guillaume Desktop <robertohelper@outlook.com>
2024-10-15 12:58:09 +0200

    feature: added 2938 cs

---
+++ CSharp/LeetCode/Solutions/2938-separate-black-and-white-balls.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2938 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public long MinimumSteps(string s)
+    {
+        // white = 0
+        // black = 1
+        int left = 0, right = s.Length - 1;
+        long swaps = 0;
+
+
+        while (left < right)
+        {
+            if (s[left] == '0')
+            {
+                left++;
+            }
+            else
+            {
+                if (s[right] == '0')
+                {
+                    swaps += right - left;
+                    left++;
+                }
+                right--;
+            }
+        }
+
+        return swaps;
+    }
+}
