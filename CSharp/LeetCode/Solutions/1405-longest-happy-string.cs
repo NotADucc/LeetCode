3ccf4f4db16a40d5a2f78784a4ff5640461bc9dc
Guillaume Laptop <robertohelper@outlook.com>
2024-10-16 12:59:15 +0200

    feature: added 1405 cs

---
+++ CSharp/LeetCode/Solutions/1405-longest-happy-string.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+public class Solution1405 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public string LongestDiverseString(int a, int b, int c) 
+    {
+        var queue = new PriorityQueue<(char, int), int>();
+        (char l, int f) = ('\0', 0);
+        if (a > 0) queue.Enqueue(('a', a), -a);    
+        if (b > 0) queue.Enqueue(('b', b), -b);    
+        if (c > 0) queue.Enqueue(('c', c), -c); 
+        StringBuilder sb = new StringBuilder();
+        while (queue.Count > 0)
+        {
+            (var letter, var freq) = queue.Dequeue();
+            if (l != '\0')
+            {
+                queue.Enqueue((l, f), -f);
+                (l, f) = ('\0', 0);
+            }
+            if (sb.Length >= 2 && sb[^1] == letter && sb[^2] == letter)
+            {
+                (l, f) = (letter, freq);
+            }
+            else
+            {
+                sb.Append(letter);
+                freq--;
+                if (freq > 0) queue.Enqueue((letter, freq), -freq);
+            }
+        }   
+
+        return sb.ToString();
+    }  
+}
