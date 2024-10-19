e6bafe1f4fa8c3258c97aa54dae61bb736789f3e
Guillaume Desktop <robertohelper@outlook.com>
2024-10-19 20:29:55 +0200

    feature: added 0219 java

---
+++ Java/0219-contains-duplicate-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // key : num
+        // value last index
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+            map.put(num, i);
+        }
+
+        return false;
+    }
+}
