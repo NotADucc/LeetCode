3c8232072d28cddf6cbbf4af758554aa46d617eb
Guillaume Desktop <robertohelper@outlook.com>
2024-10-20 16:47:04 +0200

    feature: added 3324

---
+++ CSharp/LeetCode/Solutions/3324-find-the-sequence-of-strings-appeared-on-the-screen.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Solutions.Shared;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+internal class Solution3324 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public IList<string> StringSequence(string target)
+    {
+        StringBuilder sb = new StringBuilder();
+        List<string> output = new List<string>();
+        while (sb.Length < target.Length || sb[^1] != target[^1])
+        {
+            int index = sb.Length - 1;
+            if (index >= 0 && sb[index] < target[index])
+            {
+                sb[index]++;
+            }
+            else
+            {
+                sb.Append("a");
+            }
+            output.Add(sb.ToString());
+        }
+        return output;
+    }
+}
