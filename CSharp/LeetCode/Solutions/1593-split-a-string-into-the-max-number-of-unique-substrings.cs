b91f71bd9b0d02c01e73bb8ca33288e5cffe37c5
Guillaume Desktop <robertohelper@outlook.com>
2024-10-21 11:29:59 +0200

    feature: added 1593 cs

---
+++ CSharp/LeetCode/Solutions/1593-split-a-string-into-the-max-number-of-unique-substrings.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1593 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public int MaxUniqueSplit(string s)
+    {
+        var set = new HashSet<string>();
+        int output = 0;
+        Backtrack(ref output, set, 0, s);
+        return output;
+    }
+    private void Backtrack(ref int output, HashSet<string> set, int index, string s)
+    {
+        if (output < set.Count)
+        {
+            output = set.Count;
+        }
+        for (int len = 1; index + len <= s.Length; len++)
+        {
+            string sub = s.Substring(index, len);
+            if (set.Add(sub))
+            {
+                Backtrack(ref output, set, index + len, s);
+                set.Remove(sub);
+            }
+        }
+    }
+}
