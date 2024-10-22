a73e0954dac03f297bed72e2e8bf24e86bd7e298
Guillaume Desktop <robertohelper@outlook.com>
2024-10-22 15:13:26 +0200

    refactor: cleaned up 2583 cs

--- CSharp/LeetCode/Solutions/2583-kth-largest-sum-in-a-binary-tree.cs
+++ CSharp/LeetCode/Solutions/2583-kth-largest-sum-in-a-binary-tree.cs
@@ -12,21 +12,19 @@ internal class Solution2583 : IRunProgram
     public long KthLargestLevelSum(TreeNode root, int k)
     {
         var nums = new List<long>();
-        int depth = 0;
-        Traverse(root, nums, ref depth, 0);
+        Traverse(root, nums, 0);
         nums = nums.OrderBy(o => o).ToList();
         return k > nums.Count ? -1 : nums[^k];
     }
 
-    private void Traverse(TreeNode root, List<long> nums, ref int max, int curr)
+    private void Traverse(TreeNode root, List<long> nums, int curr)
     {
         if (root is null) return;
 
-        max = Math.Max(max, curr);
         if (nums.Count <= curr) nums.Add(root.val);
         else nums[curr] += root.val;
 
-        Traverse(root.left, nums, ref max, curr + 1);
-        Traverse(root.right, nums, ref max, curr + 1);
+        Traverse(root.left, nums, curr + 1);
+        Traverse(root.right, nums, curr + 1);
     }
 }
