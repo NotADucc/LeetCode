c148d8239f3af3251063f343df7c40d873f779bc
Guillaume Desktop <robertohelper@outlook.com>
2024-10-22 11:22:34 +0200

    feature: added 2583 cs

---
+++ CSharp/LeetCode/Solutions/2583-kth-largest-sum-in-a-binary-tree.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2583 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public long KthLargestLevelSum(TreeNode root, int k)
+    {
+        var nums = new List<long>();
+        int depth = 0;
+        Traverse(root, nums, ref depth, 0);
+        nums = nums.OrderBy(o => o).ToList();
+        return k > nums.Count ? -1 : nums[^k];
+    }
+
+    private void Traverse(TreeNode root, List<long> nums, ref int max, int curr)
+    {
+        if (root is null) return;
+
+        max = Math.Max(max, curr);
+        if (nums.Count <= curr) nums.Add(root.val);
+        else nums[curr] += root.val;
+
+        Traverse(root.left, nums, ref max, curr + 1);
+        Traverse(root.right, nums, ref max, curr + 1);
+    }
+}
