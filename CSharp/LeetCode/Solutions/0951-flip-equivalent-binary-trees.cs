0b12ba60f09f57fe322ee895b6a669a8e2709128
Guillaume Laptop <robertohelper@outlook.com>
2024-10-24 08:06:53 +0200

    feature: added 0951 cs, probably have to add some references since its been added without VS

---
+++ CSharp/LeetCode/Solutions/0951-flip-equivalent-binary-trees.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0951 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public bool FlipEquiv(TreeNode root1, TreeNode root2) 
+    {
+        if (root1 is null ^ root2 is null) return false;
+        if (root1 is null && root2 is null) return true;
+        if (root1.val != root2.val) return false;
+
+        return 
+            (FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right))
+            || (FlipEquiv(root1.left, root2.right) && FlipEquiv(root1.right, root2.left));
+    }
+}
