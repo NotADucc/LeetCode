f2de9b78f9054bffa1a46cf3c9770fc77893a37f
Guillaume Desktop <robertohelper@outlook.com>
2024-10-25 21:10:39 +0200

    feature: added 1233 cs

---
+++ CSharp/LeetCode/Solutions/1233-remove-sub-folders-from-the-filesystem.cs
@@ -0,0 +1,72 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1233 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public IList<string> RemoveSubfolders(string[] folder)
+    {
+        var output = new List<string>();
+        var trie = new Trie();
+
+        foreach (string sub in folder)
+        {
+            trie.Add(sub.AsSpan().Slice(1));
+        }
+
+        foreach (string sub in folder)
+        {
+            if (!trie.IsSub(sub.AsSpan().Slice(1)))
+            {
+                output.Add(sub);
+            }
+        }
+
+        return output;
+    }
+
+    class Trie
+    {
+        private Trie[] children;
+        private bool end = false;
+        public Trie()
+        {
+            children = new Trie[27];
+        }
+        public void Add(ReadOnlySpan<char> input)
+        {
+            var trie = this;
+            foreach (char ch in input)
+            {
+                int index = ch == '/' ? 26 : ch - 'a';
+                if (trie.children[index] is null)
+                {
+                    trie.children[index] = new Trie();
+                }
+                trie = trie.children[index];
+            }
+            trie.end = true;
+        }
+
+        public bool IsSub(ReadOnlySpan<char> input)
+        {
+            var trie = this;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = input[i] == '/' ? 26 : input[i] - 'a';
+                if (trie.children[index].end && i + 1 < input.Length)
+                {
+                    if (input[i + 1] == '/')
+                        return true;
+                }
+                trie = trie.children[index];
+            }
+
+            return false;
+        }
+    }
+}
