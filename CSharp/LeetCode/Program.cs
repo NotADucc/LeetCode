4be730f3af1369388dcbd03e641fcbdb10f8361f
Guillaume Desktop <robertohelper@outlook.com>
2024-10-28 14:28:37 +0100

    feature: added 2501

--- CSharp/LeetCode/Program.cs
+++ CSharp/LeetCode/Program.cs
@@ -8,6 +8,6 @@ public class Program
     public static void Main()
     {
         //BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmark1545>();
-        Runner.RunLeetCodeSolution<Solution0355>();
+        Runner.RunLeetCodeSolution<Solution2501>();
     }
 }
---
+++ CSharp/LeetCode/Solutions/2501-longest-square-streak-in-an-array.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2501 : IRunProgram
+{
+    public void Run()
+    {
+        LongestSquareStreak([3, 9, 81, 6561]).Print();
+    }
+    public int LongestSquareStreak(int[] nums)
+    {
+        var set = new HashSet<int>(nums);
+        int output = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int current = Help(nums[i], set);
+            output = Math.Max(output, current);
+        }
+
+        return output <= 1 ? -1 : output;
+    }
+
+    private int Help(int num, HashSet<int> nums)
+    {
+        if (num >= 46_340 || !nums.Contains(num * num)) return 1;
+        return 1 + Help(num * num, nums);
+    }
+}
