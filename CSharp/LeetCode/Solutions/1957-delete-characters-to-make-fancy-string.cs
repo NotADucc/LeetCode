6e96fe2df4802eb7eb199a2182ec9a93a4a8fb1d
Guillaume Desktop <robertohelper@outlook.com>
2024-11-01 13:57:01 +0100

    feature: added 1957 cs

---
+++ CSharp/LeetCode/Solutions/1957-delete-characters-to-make-fancy-string.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Solutions.Shared;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1957 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public string MakeFancyString(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (sb.Length < 2 || (sb[sb.Length - 2] != sb[sb.Length - 1] || sb[sb.Length - 1] != s[i]))
+            {
+                sb.Append(s[i]);
+            }
+        }
+        return sb.ToString();
+    }
+}
