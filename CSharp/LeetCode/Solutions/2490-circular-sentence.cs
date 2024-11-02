4ddeaf13be063783593a14e959fd6337356b6013
Guillaume Desktop <robertohelper@outlook.com>
2024-11-02 12:33:19 +0100

    feature: added 2490 cs

---
+++ CSharp/LeetCode/Solutions/2490-circular-sentence.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2490 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public bool IsCircularSentence(string sentence)
+    {
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            if (sentence[i] == ' ')
+            {
+                if (sentence[i - 1] != sentence[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+        return sentence[0] == sentence[^1];
+    }
+}
