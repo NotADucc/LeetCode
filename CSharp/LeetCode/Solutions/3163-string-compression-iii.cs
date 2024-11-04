ffcaf56db0f1f44e474271eac0b4d140ae4197b9
Guillaume Laptop <robertohelper@outlook.com>
2024-11-04 08:09:42 +0100

    feature: added 3163 cs

---
+++ CSharp/LeetCode/Solutions/3163-string-compression-iii.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+public class Solution3163 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public string CompressedString(string word) 
+    {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+
+        for (int i = 0; i < word.Length - 1; i++) 
+        {
+            if (word[i] != word[i + 1] || count >= 9)
+            {
+                sb.Append(count);
+                sb.Append(word[i]);
+                count = 0;
+            }
+            count++;
+        }
+
+        sb.Append(count);
+        sb.Append(word[^1]);
+
+        return sb.ToString();        
+    }
+}
