4454d4c1742cd93b0bd19ca3e81f98a2641583b7
Guillaume Desktop <robertohelper@outlook.com>
2024-11-05 11:10:09 +0100

    feature: added 2914 cs

---
+++ CSharp/LeetCode/Solutions/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2914 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+    public int MinChanges(string s)
+    {
+        int output = 0;
+        for (int i = 0; i < s.Length - 1; i += 2)
+        {
+            if (s[i] != s[i + 1]) output++;
+        }
+        return output;
+    }
+}
