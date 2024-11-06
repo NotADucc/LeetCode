cc44b7243c404c4247c28ee50080b53af2d46b15
Guillaume Laptop <robertohelper@outlook.com>
2024-11-06 14:33:58 +0100

    feature: added 0023 cs

---
+++ CSharp/LeetCode/Solutions/0023-merge-k-sorted-lists.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0023 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public ListNode MergeKLists(ListNode[] lists) 
+    {
+        var q = new PriorityQueue<ListNode, int>();
+        for (int i = 0; i < lists.Length; i++)
+        {
+            if (lists[i] is not null)
+                q.Enqueue(lists[i], lists[i].val);
+        }
+        ListNode anchor = new ListNode(-1);
+        ListNode curr = anchor;
+
+        while (q.Count > 0)
+        {
+            ListNode deq = q.Dequeue();
+            curr.next = deq;
+            if (q.Count == 0)
+            {
+                break;
+            }
+            else
+            {
+                if (deq.next is not null) 
+						q.Enqueue(deq.next, deq.next.val);
+                curr = curr.next;
+            }
+        }
+
+        return anchor.next;
+    }
+}
