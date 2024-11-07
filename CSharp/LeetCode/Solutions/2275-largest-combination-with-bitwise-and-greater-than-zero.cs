f49d8a5a291f771fa8a56e4c58bd58be09475da6
Guillaume Desktop <robertohelper@outlook.com>
2024-11-07 17:07:36 +0100

    feature: added 2275 cs

---
+++ CSharp/LeetCode/Solutions/2275-largest-combination-with-bitwise-and-greater-than-zero.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2275 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    private const int MAX_BITS = 24;
+    public int LargestCombination(int[] candidates)
+    {
+        Span<int> span = stackalloc int[MAX_BITS];
+        int max = 0;
+        foreach (int candidate in candidates)
+        {
+            int cand = candidate;
+            for (int i = MAX_BITS; i >= 0; i--)
+            {
+                int pow = 1 << i;
+                if (pow <= cand)
+                {
+                    span[i]++;
+                    cand -= pow;
+                    max = Math.Max(max, span[i]);
+                    if (candidate == 0) break;
+                }
+            }
+        }
+
+        return max;
+    }
+
+}
