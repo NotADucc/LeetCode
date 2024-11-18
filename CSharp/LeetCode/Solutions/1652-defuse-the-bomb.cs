0d779be0f3cc8c4a4ceb3a317e8e1276bf1d9600
Guillaume Laptop <robertohelper@outlook.com>
2024-11-18 08:46:06 +0100

    feature: added 1652 cs

---
+++ CSharp/LeetCode/Solutions/1652-defuse-the-bomb.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1652 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public int[] Decrypt(int[] code, int k) 
+    {
+        var res = new int[code.Length];
+        if (k == 0) return res;
+        int new_k = Math.Abs(k);
+        int l = k < 0 ? code.Length + k : 1, r = l;
+        for (int j = 1; j <= new_k; j++)
+            res[0] += code[r++];
+        for (int i = 1; i < code.Length; i++)
+        {
+            res[i] += res[i - 1];
+            res[i] -= code[l++ % code.Length];
+            res[i] += code[r++ % code.Length];
+        }   
+
+        return res;     
+    }
+}
