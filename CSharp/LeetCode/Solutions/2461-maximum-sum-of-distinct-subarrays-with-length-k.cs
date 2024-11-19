f0ff255fa28dedb4bcaad68eb74c10761736c8ed
Guillaume Desktop <robertohelper@outlook.com>
2024-11-19 14:43:23 +0100

    feature: added 2461 cs

---
+++ CSharp/LeetCode/Solutions/2461-maximum-sum-of-distinct-subarrays-with-length-k.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2461 : IRunProgram
+{
+    public void Run()
+    {
+        MaximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 3).Print();
+    }
+    public long MaximumSubarraySum(int[] nums, int k)
+    {
+        long res = 0, curr = 0;
+        //key is val
+        //val is index
+        var freq = new Dictionary<int, int>();
+        int l = 0, r = 0;
+
+        while (r < nums.Length)
+        {
+            if (freq.TryAdd(nums[r], r))
+            {
+                curr += nums[r++];
+                if (freq.Count >= k)
+                {
+                    res = Math.Max(res, curr);
+                    int val = nums[l++];
+                    freq.Remove(val);
+                    curr -= val;
+                }
+            }
+            else
+            {
+                int i = freq[nums[r]];
+                while (l <= i)
+                {
+                    int val = nums[l++];
+                    freq.Remove(val);
+                    curr -= val;
+                }
+            }
+        }
+
+        return res;
+    }
+}
