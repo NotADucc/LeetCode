7fa51cd7391fab403b8281a8b4e709f05995c616
Guillaume Desktop <robertohelper@outlook.com>
2024-11-23 22:00:59 +0100

    feature: added 3360 cs

---
+++ CSharp/LeetCode/Solutions/3360-stone-removal-game.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions
+{
+    internal class Solution03360 : IRunProgram
+    {
+        public void Run()
+        {
+
+        }
+
+        public bool CanAliceWin(int n)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                n -= i;
+                if (n < 0)
+                {
+                    return (i & 1) == 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
