4cf2e3fa53ea10ba3e684f89f2972dd9c062d586
Guillaume Desktop <robertohelper@outlook.com>
2024-11-24 11:42:55 +0100

    feature: added 1975 cs

---
+++ CSharp/LeetCode/Solutions/1975-maximum-matrix-sum.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1975 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public long MaxMatrixSum(int[][] matrix)
+    {
+        long res = 0;
+        int negative = 0, smallest = int.MaxValue;
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix.Length; j++)
+            {
+                if (matrix[i][j] < 0)
+                {
+                    matrix[i][j] *= -1;
+                    negative++;
+                }
+                smallest = Math.Min(smallest, matrix[i][j]);
+                res += matrix[i][j];
+            }
+        }
+
+        return (negative & 1) == 1 ? res - (smallest << 1) : res;
+    }
+}
