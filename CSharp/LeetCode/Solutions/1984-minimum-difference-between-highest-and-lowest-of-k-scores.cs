bc486988891b1dc29359021c7ee393202cde1e65
Guillaume Desktop <robertohelper@outlook.com>
2024-11-26 15:38:05 +0100

    feature: added 1984 cs

---
+++ CSharp/LeetCode/Solutions/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions
+{
+    internal class Solution1984 : IRunProgram
+    {
+        public void Run()
+        {
+
+        }
+
+        public int MinimumDifference(int[] nums, int k)
+        {
+            int res = int.MaxValue; k--;
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - k; i++)
+            {
+                res = Math.Min(res, nums[i + k] - nums[i]);
+            }
+
+            return res;
+        }
+    }
+}
