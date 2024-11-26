296d67758be4b4d1ef0b9eb5151e47284cb98c26
Guillaume Desktop <robertohelper@outlook.com>
2024-11-26 15:27:00 +0100

    feature: added 2708 cs

---
+++ CSharp/LeetCode/Solutions/2708-maximum-strength-of-a-group.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2708 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public long MaxStrength(int[] nums)
+    {
+        if (nums.Length == 1) return nums[0];
+
+        long res = 1; bool b = false;
+        Array.Sort(nums);
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if ((i + 1 < nums.Length && nums[i] < 0 && nums[i + 1] < 0) || res < 0 && nums[i] < 0 || nums[i] > 0)
+            {
+                res *= nums[i];
+                b = true;
+            }
+        }
+
+        return b ? res : 0;
+    }
+}
