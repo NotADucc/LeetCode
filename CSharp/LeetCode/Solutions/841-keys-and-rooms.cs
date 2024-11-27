da81891cdfa4db00ef8c8862eb13651e7ad651c7
Guillaume Desktop <robertohelper@outlook.com>
2024-11-27 21:40:39 +0100

    feature: added 0841 cs

---
+++ CSharp/LeetCode/Solutions/841-keys-and-rooms.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0841 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        var visited = new bool[rooms.Count];
+        var q = new Queue<int>();
+
+        visited[0] = true;
+        q.Enqueue(0);
+
+        while (q.Count > 0)
+        {
+            int curr = q.Dequeue();
+            foreach (int room in rooms[curr])
+            {
+                if (!visited[room])
+                {
+                    visited[room] = true;
+                    q.Enqueue(room);
+                }
+            }
+        }
+
+        return visited.All(x => x);
+    }
+}
