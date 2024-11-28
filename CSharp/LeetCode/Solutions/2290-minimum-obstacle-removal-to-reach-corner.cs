c3ba070a79ee28fef03261f10a010a9c4107d8ea
Guillaume Desktop <robertohelper@outlook.com>
2024-11-28 14:54:08 +0100

    refactor: removed DAG in 2290

--- CSharp/LeetCode/Solutions/2290-minimum-obstacle-removal-to-reach-corner.cs
+++ CSharp/LeetCode/Solutions/2290-minimum-obstacle-removal-to-reach-corner.cs
@@ -14,57 +14,48 @@ internal class Solution2290 : IRunProgram
     {
         int n = grid.Length, m = grid[0].Length;
 
-        var dag = new List<(int, int)>[n * m];
         var res = new int[n * m];
-
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < grid[i].Length; j++)
             {
-                dag[i * m + j] = new List<(int, int)>();
                 res[i * m + j] = int.MaxValue;
-                for (int k = 0; k < directions.Length; k++)
-                {
-                    int new_i = i + directions[k][0], new_j = j + directions[k][1];
-                    if (new_i < 0 || new_i >= n || new_j < 0 || new_j >= grid[i].Length) continue;
-
-                    dag[i * m + j].Add((new_i * m + new_j, grid[new_i][new_j]));
-                }
             }
         }
 
-        var q = new PriorityQueue<int, int>();
+        var q = new PriorityQueue<(int, int), int>();
         bool[] visited = new bool[n * m];
 
         res[0] = 0;
-        q.Enqueue(0, 0);
+        q.Enqueue((0, 0), 0);
 
         while (q.Count > 0)
         {
-            int curr = q.Dequeue();
-
-            foreach ((int neighbour, int weight) in dag[curr])
+            (int i, int j) = q.Dequeue();
+            for (int k = 0; k < directions.Length; k++)
             {
-                res[neighbour] = Math.Min(res[neighbour], res[curr] + weight);
-                if (!visited[neighbour] && neighbour < n * m - 1)
+                int new_i = i + directions[k][0], new_j = j + directions[k][1];
+                if (new_i < 0 || new_i >= n || new_j < 0 || new_j >= grid[i].Length) continue;
+
+                res[new_i * m + new_j] = Math.Min(res[new_i * m + new_j], res[i * m + j] + grid[new_i][new_j]);
+                if (!visited[new_i * m + new_j])
                 {
-                    visited[neighbour] = true;
-                    q.Enqueue(neighbour, res[neighbour]);
+                    visited[new_i * m + new_j] = true;
+                    q.Enqueue((new_i, new_j), res[new_i * m + new_j]);
                 }
             }
         }
 
-        //Console.WriteLine(string.Join("\n", dag.Select(x => string.Join(", ", x))));
-        
-        //for (int i = 0; i < n; i++)
-        //{
-        //    for (int j = 0; j < grid[i].Length; j++)
-        //    {
-        //        Console.Write($"| {res[i * m + j]} |");
-        //    }
-        //    Console.WriteLine();
-        //}
-        
+        /*
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                Console.Write($"| {res[i * m + j]} |");
+            }
+            Console.WriteLine();
+        }
+        */
 
         return res[^1];
     }
