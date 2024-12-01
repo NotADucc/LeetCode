e1a3e657f72c6c23fd902c64b91d33b206074519
Guillaume Desktop <robertohelper@outlook.com>
2024-12-01 12:26:50 +0100

    feature: added 1346 cs

---
+++ CSharp/LeetCode/Solutions/1346-check-if-n-and-its-double-exist.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1346 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public bool CheckIfExist(int[] arr)
+    {
+        var set = new HashSet<int>();
+        foreach (int num in arr)
+        {
+            int dbl = num << 1;
+            int hlf = num >> 1;
+            if (set.Contains(dbl) || ((num & 1) == 0 && set.Contains(hlf)))
+            {
+                return true;
+            }
+            set.Add(num);
+        }
+        return false;
+    }
+}
