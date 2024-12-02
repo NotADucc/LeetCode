8b67a93f581d9f8988cbdab6b32e834ab6d6f39e
Guillaume Laptop <robertohelper@outlook.com>
2024-12-02 08:03:22 +0100

    feature: added 1455 cs

---
+++ CSharp/LeetCode/Solutions/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1455 : IRunProgram
+{
+	public void Run()
+	{
+
+	}
+
+	public int IsPrefixOfWord(string sentence, string searchWord) 
+	{
+		var split = sentence.Split(" ");
+		for (int i = 0; i < split.Length; i++)
+		{
+			if (split[i].StartsWith(searchWord))
+			{
+				return i + 1;
+			}
+		}
+
+		return -1;        
+	}
+}
