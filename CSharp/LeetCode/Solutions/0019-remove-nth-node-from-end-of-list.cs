684382dec34bf06095281bf763ecb7d32dee3559
Guillaume Desktop <robertohelper@outlook.com>
2024-12-03 20:26:24 +0100

    refactor: 0019 cs

--- CSharp/LeetCode/Solutions/0019-remove-nth-node-from-end-of-list.cs
+++ CSharp/LeetCode/Solutions/0019-remove-nth-node-from-end-of-list.cs
@@ -13,37 +13,23 @@ internal class Solution0019 : IRunProgram
 
     public ListNode RemoveNthFromEnd(ListNode head, int n)
     {
-        var slow = head;
-        var fast = head;
-        int len = 0;
-        for (int i = 0; i < n; i++)
+        ListNode dummy = new ListNode(0, head);
+        ListNode left = dummy, right = head;
+
+        while (n > 0)
         {
-            fast = fast.next;
-            len++;
+            right = right.next;
+            n--;
         }
 
-        var prev = head;
-        while (fast is not null)
+        while (right is not null)
         {
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next;
-            len++;
+            left = left.next;
+            right = right.next;
         }
 
-        if (len == 1) { return null; }
+        left.next = left.next.next;
 
-        if (slow.next is not null)
-        {
-            slow.val = slow.next.val;
-            slow.next = slow.next.next;
-        }
-        else
-        {
-            slow.val = prev.val;
-            prev.next = prev.next.next;
-        }
-
-        return head;
+        return dummy.next;
     }
 }
