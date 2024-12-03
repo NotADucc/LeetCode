f2d267ee46fcb9731bb61ac0a4bc86a05b65ff4e
Guillaume Desktop <robertohelper@outlook.com>
2024-12-03 12:41:51 +0100

    feature: added 2109 cs

---
+++ CSharp/LeetCode/Solutions/2109-adding-spaces-to-a-string.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2109 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public string AddSpaces(string s, int[] spaces)
+    {
+        StringBuilder sb = new StringBuilder(s.Length + spaces.Length);
+        int space_idx = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (space_idx < spaces.Length && spaces[space_idx] == i)
+            {
+                sb.Append(' ');
+                space_idx++;
+            }
+            sb.Append(s[i]);
+        }
+
+        return sb.ToString();
+    }
+}
