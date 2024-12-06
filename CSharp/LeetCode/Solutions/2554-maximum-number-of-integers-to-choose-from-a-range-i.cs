9d946b34aecbdaf847a1e0abe64f316d70cb8562
Guillaume Desktop <robertohelper@outlook.com>
2024-12-06 20:41:03 +0100

    feature: added 2554 cs

---
+++ CSharp/LeetCode/Solutions/2554-maximum-number-of-integers-to-choose-from-a-range-i.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2554 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public int MaxCount(int[] banned, int n, int maxSum)
+    {
+        HashSet<int> ban = new HashSet<int>(banned);
+        int res = 0, curr = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (!ban.Contains(i) && curr + i <= maxSum)
+            {
+                curr += i;
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
