59cea47abd13a0c18571a9264383fc5d38c61316
Guillaume Laptop <robertohelper@outlook.com>
2024-12-12 08:17:00 +0100

    feature: added 2558 cs

---
+++ CSharp/LeetCode/Solutions/2558-take-gifts-from-the-richest-pile.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2558 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public long PickGifts(int[] gifts, int k) 
+    {
+        long res = 0;
+        var q = new PriorityQueue<int, int>();
+
+        foreach (int gift in gifts)
+        {
+            res += gift;
+            q.Enqueue(gift, -gift);
+        }
+
+        for (; k > 0; k--)
+        {
+            int temp = q.Dequeue();
+            int gift = (int)Math.Sqrt(temp);
+            res -= temp - gift;
+            q.Enqueue(gift, -gift);
+        }
+
+        return res;    
+    }
+}
