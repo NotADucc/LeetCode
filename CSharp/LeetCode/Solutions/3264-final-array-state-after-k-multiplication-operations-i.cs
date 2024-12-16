9434c453a6fcb5e7bd8b1ab3e0a447b69fd913bc
Guillaume Desktop <robertohelper@outlook.com>
2024-12-16 11:23:53 +0100

    feature: added 3264 cs

---
+++ CSharp/LeetCode/Solutions/3264-final-array-state-after-k-multiplication-operations-i.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution3264 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int[] GetFinalState(int[] nums, int k, int multiplier)
+    {
+        int Smallest(int[] arr)
+        {
+            int idx = 0, small = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < small)
+                {
+                    small = arr[i];
+                    idx = i;
+                }
+            }
+            return idx;
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            int idx = Smallest(nums);
+            nums[idx] *= multiplier;
+        }
+
+        return nums;
+    }
+}
