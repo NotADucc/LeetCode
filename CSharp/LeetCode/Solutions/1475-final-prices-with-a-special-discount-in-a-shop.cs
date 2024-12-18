fad0c847cc3594811002b9124f505ac0b916e380
Guillaume Desktop <robertohelper@outlook.com>
2024-12-18 13:26:42 +0100

    feature: added 1475 cs

---
+++ CSharp/LeetCode/Solutions/1475-final-prices-with-a-special-discount-in-a-shop.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1475 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int[] FinalPrices(int[] prices)
+    {
+        var stack = new Stack<int>();
+        for (int i = 0; i < prices.Length; i++)
+        {
+            while (stack.Count > 0 && prices[stack.Peek()] >= prices[i])
+            {
+                prices[stack.Pop()] -= prices[i];
+            }
+            stack.Push(i);
+        }
+
+        return prices;
+    }
+}
