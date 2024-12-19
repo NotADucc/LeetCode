a8bcd05245068f5cefe54acefd5189ff576d494e
Guillaume Desktop <robertohelper@outlook.com>
2024-12-19 14:08:01 +0100

    feature: added 0769 cs

---
+++ CSharp/LeetCode/Solutions/0769-max-chunks-to-make-sorted.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0769 : IRunProgram
+{
+    public void Run()
+    {
+        MaxChunksToSorted([4, 3, 2, 1, 0]).Print();
+        MaxChunksToSorted([1, 0, 2, 3, 4]).Print();
+        MaxChunksToSorted([0, 2, 1]).Print();
+        MaxChunksToSorted([0, 2, 4, 3, 1, 9, 5, 6, 7, 8]).Print();
+    }
+
+    public int MaxChunksToSorted(int[] arr)
+    {
+        int res = 0, max = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            max = Math.Max(max, arr[i]);
+
+            if (max == i)
+            {
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
