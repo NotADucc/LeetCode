016b40de7ae703b2cc876b630f8cf1775e86c8e3
Guillaume Desktop <robertohelper@outlook.com>
2024-12-25 15:15:32 +0100

    feature: added 0515 cs

---
+++ CSharp/LeetCode/Solutions/0515-find-largest-value-in-each-tree-row.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution0515 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public IList<int> LargestValues(TreeNode root)
+    {
+        List<int> res = new List<int>();
+        Rec(res, root);
+        return res;
+    }
+    private void Rec(List<int> res, TreeNode root, int depth = 0)
+    {
+        if (root is null)
+            return;
+
+        if (depth >= res.Count)
+            res.Add(root.val);
+        else
+            res[depth] = Math.Max(res[depth], root.val);
+
+        Rec(res, root.left, depth + 1);
+        Rec(res, root.right, depth + 1);
+    }
+}
