3d6132a4b1f414f99ddcd791925e86cbf828d7c7
Guillaume Desktop <robertohelper@outlook.com>
2024-12-27 01:25:43 +0100

    feature: added 1014 cs

---
+++ CSharp/LeetCode/Solutions/1014-best-sightseeing-pair.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1014 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MaxScoreSightseeingPair(int[] values)
+    {
+        int max_left = values[0], max = 0;
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            int right = values[i] - i;
+            int left = values[i] + i;
+            max = Math.Max(max, max_left + right);
+            max_left = Math.Max(max_left, left);
+        }
+
+        return max;
+    }
+}
