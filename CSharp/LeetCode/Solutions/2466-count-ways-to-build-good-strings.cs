d8b6e4fb3693ff7f9e747088d70e68f6b2c997e8
Guillaume Desktop <robertohelper@outlook.com>
2024-12-30 14:49:46 +0100

    feature: added 2466 cs, beats 0%

---
+++ CSharp/LeetCode/Solutions/2466-count-ways-to-build-good-strings.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2466 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int CountGoodStrings(int low, int high, int zero, int one)
+    {
+        return DP(low, high, zero, one, 0);
+    }
+
+    private Dictionary<int, int> mem = new Dictionary<int, int>();
+    private int DP(int low, int high, int zero, int one, int l)
+    {
+        if (l > high)
+            return 0;
+
+        if (!mem.ContainsKey(l))
+        {
+            int add = l >= low ? 1 : 0;
+            mem[l] = (add + DP(low, high, zero, one, l + zero) + DP(low, high, zero, one, l + one)) % 1_000_000_007;
+        }
+
+        return mem[l];
+    }
+}
