33ee74396483957be73710fad64df07a53e6e9ba
Guillaume Desktop <robertohelper@outlook.com>
2025-01-01 14:13:21 +0100

    feature: added 1422 cs

---
+++ CSharp/LeetCode/Solutions/1422-maximum-score-after-splitting-a-string.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1422 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MaxScore(string s)
+    {
+        int zero_cnt = 0, one_cnt = 0, res = 0;
+
+        foreach (char ch in s)
+        {
+            one_cnt += ch - '0';
+        }
+
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            char ch = s[i];
+            if (ch == '0')
+            {
+                zero_cnt++;
+            }
+            else
+            {
+                one_cnt--;
+            }
+            res = Math.Max(res, zero_cnt + one_cnt);
+        }
+
+        return res;
+    }
+}
