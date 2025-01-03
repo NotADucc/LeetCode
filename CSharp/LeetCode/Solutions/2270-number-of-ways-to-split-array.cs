25aa38291cbbcccc627d0a8542670076b9209e90
Guillaume Desktop <robertohelper@outlook.com>
2025-01-03 13:30:30 +0100

    feature: added 2270 cs

---
+++ CSharp/LeetCode/Solutions/2270-number-of-ways-to-split-array.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2270 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int WaysToSplitArray(int[] nums)
+    {
+        int res = 0;
+        long[] nums_l = new long[nums.Length];
+        nums_l[0] = nums[0];
+
+        for (int i = 1; i < nums_l.Length; i++)
+        {
+            nums_l[i] = nums_l[i - 1] + nums[i];
+        }
+
+        for (int i = 0; i < nums_l.Length - 1; i++)
+        {
+            if (nums_l[i] >= nums_l[^1] - nums_l[i])
+            {
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
