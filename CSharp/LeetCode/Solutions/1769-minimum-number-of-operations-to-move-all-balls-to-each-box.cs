425157203b08699ef901116c0a9743a59b507ed9
Guillaume Desktop <robertohelper@outlook.com>
2025-01-06 11:59:04 +0100

    feature: added 1769 cs naive

---
+++ CSharp/LeetCode/Solutions/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1769 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int[] MinOperations(string boxes)
+    {
+        int[] res = new int[boxes.Length];
+
+        for (int i = 0; i < boxes.Length; i++)
+        {
+            for (int j = 0; j < boxes.Length; j++)
+            {
+                res[i] += boxes[j] == '1' ? Math.Abs(i - j) : 0;
+            }
+        }
+
+        return res;
+    }
+}
