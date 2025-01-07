1f368e79e798f32c6fdcb10680e90f7ecbb768c5
Guillaume Desktop <robertohelper@outlook.com>
2025-01-07 16:00:40 +0100

    feature: added 1408 cs

---
+++ CSharp/LeetCode/Solutions/1408-string-matching-in-an-array.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1408 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public IList<string> StringMatching(string[] words)
+    {
+        List<string> res = new List<string>();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j < words.Length; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                if (words[j].Contains(words[i]))
+                {
+                    res.Add(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
