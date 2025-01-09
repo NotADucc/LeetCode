207589a58d387426e53015b420c0c1ac3945c21a
Guillaume Desktop <robertohelper@outlook.com>
2025-01-09 18:37:35 +0100

    feature: added 2185 cs

---
+++ CSharp/LeetCode/Solutions/2185-counting-words-with-a-given-prefix.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2185 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int PrefixCount(string[] words, string pref)
+    {
+        int res = 0, n = pref.Length;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            var s = words[i].AsSpan();
+            if (s.Length < n)
+                continue;
+            int j = 0;
+            for (; j < n; j++)
+            {
+                if (s[j] != pref[j])
+                {
+                    break;
+                }
+            }
+            if (j >= n) res++;
+        }
+
+        return res;
+    }
+}
