5d70dc90dd70a9a94212958672f8b65df45695d3
Guillaume Desktop <robertohelper@outlook.com>
2025-02-01 14:09:58 +0100

    feature: added 3151 cs

---
+++ CSharp/LeetCode/Solutions/3151-special-array-i.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions
+{
+    internal class Solution3151 : IRunProgram
+    {
+        public void Run()
+        {
+
+        }
+        public bool IsArraySpecial(int[] nums)
+        {
+            bool IsEven(int num) => (num & 1) == 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (IsEven(nums[i]) == IsEven(nums[i + 1]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
