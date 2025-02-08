adc44ac01d79f284b421994853d7586428488841
Guillaume Desktop <robertohelper@outlook.com>
2025-02-08 15:20:04 +0100

    feature: added 2349 cs

---
+++ CSharp/LeetCode/Solutions/2349-design-a-number-container-system.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2349 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public class NumberContainers
+    {
+        //key: idx, val: val
+        private Dictionary<int, int> loc = [];
+        //key: val, val: idxs
+        private Dictionary<int, SortedList<int, int>> indices = [];
+        public NumberContainers() { }
+
+        public void Change(int index, int number)
+        {
+            if (loc.TryGetValue(index, out int curr_num))
+            {
+                indices[curr_num].Remove(index);
+            }
+
+            if (!indices.ContainsKey(number))
+                indices[number] = new SortedList<int, int>();
+            loc[index] = number;
+            indices[number].Add(index, index);
+        }
+
+        public int Find(int number)
+        {
+            if (indices.TryGetValue(number, out var vals) && vals.Count != 0)
+            {
+                return vals.Values[0];
+            }
+            return -1;
+        }
+    }
+}
