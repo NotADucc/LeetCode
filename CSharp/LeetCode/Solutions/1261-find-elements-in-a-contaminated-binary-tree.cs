908878ba02f84bd70f75ebf3ded730b559840b51
Guillaume Desktop <robertohelper@outlook.com>
2025-02-21 19:42:57 +0100

    feature: added 1261 cs

---
+++ CSharp/LeetCode/Solutions/1261-find-elements-in-a-contaminated-binary-tree.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1261 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public class FindElements
+    {
+        HashSet<int> set = [];
+        public FindElements(TreeNode root)
+        {
+            Rec(root, 0);
+        }
+
+        private void Rec(TreeNode root, int val)
+        {
+            if (root is null)
+                return;
+
+            set.Add(val);
+            if (root.left is not null)
+            {
+                Rec(root.left, val * 2 + 1);
+            }
+
+            if (root.right is not null)
+            {
+                Rec(root.right, val * 2 + 2);
+            }
+        }
+
+        public bool Find(int target)
+        {
+            return set.Contains(target);
+        }
+    }
+}
