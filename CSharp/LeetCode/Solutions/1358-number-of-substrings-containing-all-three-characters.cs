79332e342ea749a63d06067156d8fb29864d6fb0
Guillaume Desktop <robertohelper@outlook.com>
2025-03-11 11:18:04 +0100

    feature: added 1358 cs

---
+++ CSharp/LeetCode/Solutions/1358-number-of-substrings-containing-all-three-characters.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1358 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int NumberOfSubstrings(string s)
+    {
+        static int transform(char ch) => ch - 'a';
+        static bool consists(int[] arr) => arr[0] > 0 && arr[1] > 0 && arr[2] > 0;
+        int[] freq = [0, 0, 0];
+        int res = 0, n = s.Length, l = 0;
+
+        for (int r = 0; r < n; r++)
+        {
+            char ch = s[r];
+            int idx = transform(ch);
+            freq[idx]++;
+
+            while (consists(freq))
+            {
+                res += n - r;
+                ch = s[l++];
+                idx = transform(ch);
+                freq[idx]--;
+            }
+        }
+
+        return res;
+    }
+}
