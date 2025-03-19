c1bbf9a3e5bd877ee021d5eb6ce943ba0da02a5f
Guillaume Laptop <robertohelper@outlook.com>
2025-03-19 10:17:20 +0100

    feature: added 3191 cs

---
+++ CSharp/LeetCode/Solutions/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution3191 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int MinOperations(int[] nums)
+    {
+        int res = 0, n = nums.Length;
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (nums[i] == 0)
+            {
+                nums[i] = 1;
+                nums[i + 1] = (nums[i + 1] + 1) % 2;
+                nums[i + 2] = (nums[i + 2] + 1) % 2;
+                res++;
+            }
+        }
+
+        return nums[^1] + nums[^2] == 2 ? res : -1;
+    }
+}
