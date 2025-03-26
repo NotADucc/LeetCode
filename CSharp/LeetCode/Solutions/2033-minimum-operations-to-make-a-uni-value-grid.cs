d2f7d5a145453e0a9bf15db519c39c4008d11b96
Guillaume Laptop <robertohelper@outlook.com>
2025-03-26 12:38:28 +0100

    feature: added 2033 cs

---
+++ CSharp/LeetCode/Solutions/2033-minimum-operations-to-make-a-uni-value-grid.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Shared;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2033 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int MinOperations(int[][] grid, int x) 
+    {
+        int[] flat = grid
+            .SelectMany(x => x)
+            .OrderBy(x => x)
+            .ToArray();
+        int n = flat.Length, res = 0, middle_val = flat[n >> 1];
+
+        for (int i = 0; i < n; i++)
+        {
+            int delta = Math.Abs(flat[i] - middle_val);
+            if (delta % x != 0) return -1;
+            res += delta / x;
+        }
+
+        return res;
+    }
+}
