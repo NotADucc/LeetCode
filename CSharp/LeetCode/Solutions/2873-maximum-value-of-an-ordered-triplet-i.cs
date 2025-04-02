af458f886ad62239daa6b4e57442fd06f9533a49
Guillaume Laptop <robertohelper@outlook.com>
2025-04-02 11:09:50 +0200

    feature: added 2873 cs

---
+++ CSharp/LeetCode/Solutions/2873-maximum-value-of-an-ordered-triplet-i.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2873 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public long MaximumTripletValue(int[] nums)
+    {
+        long res = 0L;
+        int n = nums.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    res = Math.Max(res, ((long)nums[i] - nums[j]) * nums[k]);
+                }
+            }
+        }
+        return res;
+    }
+}
