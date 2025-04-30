3338f4f2b29763d6ef36604be391f29b593dff88
Guillaume Laptop <robertohelper@outlook.com>
2025-04-30 10:44:13 +0200

    feature: addeed 1295 cs

---
+++ CSharp/LeetCode/Solutions/1295-find-numbers-with-even-number-of-digits.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution1295: IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int FindNumbers(int[] nums) 
+    {
+        static bool is_even(int num)
+        {
+            int res = 0;
+            for (; num > 0;res++)
+                num /= 10;
+            return (res & 1) == 0;
+        }
+        
+        int res = 0;
+        foreach (int num in nums)
+            if (is_even(num)) res++;
+
+        return res;
+    }
+}
