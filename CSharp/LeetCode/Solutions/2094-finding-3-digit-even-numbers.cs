7f9ea1f5e1798264ecfe82383ac35bcca234e979
Guillaume Laptop <robertohelper@outlook.com>
2025-05-12 12:41:52 +0200

    feature: added 2094 cs

---
+++ CSharp/LeetCode/Solutions/2094-finding-3-digit-even-numbers.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution2094 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+	public int[] FindEvenNumbers(int[] digits) 
+	{
+        List<int> res = [];
+        Span<int> freq = stackalloc int[10];
+
+        foreach (int digit in digits)
+            freq[digit]++;
+
+        for (int i = 100; i < 1000; i += 2)
+        {
+            Span<int> compare = stackalloc int[10];
+            int dig1 = i / 100, dig2 = (i / 10) % 10, dig3 = i % 10;
+            compare[dig1]++;
+            compare[dig2]++;
+            compare[dig3]++;
+            if (compare[dig1] <= freq[dig1]
+                && compare[dig2] <= freq[dig2]
+                && compare[dig3] <= freq[dig3]) 
+                res.Add(i);
+        }
+
+        return res.ToArray();
+    }
+}
