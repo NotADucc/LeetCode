844a4a1eb99344189c368d5fbff5598857a6fc9a
Guillaume Desktop <robertohelper@outlook.com>
2025-07-08 17:32:29 +0200

    feature: added 0041 cs

---
+++ CSharp/LeetCode/Solutions/0041-first-missing-positive.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution0041 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int FirstMissingPositive(int[] nums)
+    {
+        int n = nums.Length;
+        var missing = new bool[n + 1];
+        foreach (var num in nums)
+        {
+            if (num <= 0 || num > n) continue;
+            missing[num - 1] = true;
+        }
+
+        for (int i = 0; i < missing.Length; i++)
+        {
+            if (!missing[i]) return i + 1;
+        }
+
+        return -1;
+    }
+}
