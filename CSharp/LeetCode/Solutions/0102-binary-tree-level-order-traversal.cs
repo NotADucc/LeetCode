fbdcd03dc3642f8be0712d3b7d6491796203888e
Guillaume Desktop <robertohelper@outlook.com>
2025-07-09 17:13:19 +0200

    feature: added 102 cs

---
+++ CSharp/LeetCode/Solutions/0102-binary-tree-level-order-traversal.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution00102 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        var res = new List<IList<int>>();
+        Helper(res, root, 0);
+        return res;
+    }
+
+    private void Helper(List<IList<int>> res, TreeNode root, int depth)
+    {
+        if (root is null) return;
+
+        if (res.Count <= depth)
+            res.Add([]);
+
+        res[depth].Add(root.val);
+        Helper(res, root.left, depth + 1);
+        Helper(res, root.right, depth + 1);
+    }
+}
