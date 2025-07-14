f233695e99f5cd99662e227ea895f6ec357bee2c
Guillaume Desktop <robertohelper@outlook.com>
2025-07-14 20:38:15 +0200

    feature: added 1290 cs

---
+++ CSharp/LeetCode/Solutions/1290-convert-binary-number-in-a-linked-list-to-integer.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution1290 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int GetDecimalValue(ListNode head)
+    {
+        int res = 0, exp = 31;
+        while (head is not null)
+        {
+            exp--;
+            if (head.val == 1)
+                res |= 1 << exp;
+
+            head = head.next;
+        }
+        res >>= exp;
+        return res;
+    }
+}
