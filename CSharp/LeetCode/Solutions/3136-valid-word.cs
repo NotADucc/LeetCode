5455093a03e53c6ad420579fb724dd72aed9a71d
Guillaume Desktop <robertohelper@outlook.com>
2025-07-15 12:22:53 +0200

    feature: added 3136 cs

---
+++ CSharp/LeetCode/Solutions/3136-valid-word.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3136 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public bool IsValid(string word)
+    {
+        if (word.Length < 3) return false;
+        bool has_vowel = false, has_cons = false;
+
+        foreach (var ch in word)
+        {
+            if (!char.IsLetterOrDigit(ch)) return false;
+            has_vowel |= "aeiouAEIOU".Contains(ch);
+            has_cons |= "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".Contains(ch);
+        }
+
+        return has_vowel && has_cons;
+    }
+}
