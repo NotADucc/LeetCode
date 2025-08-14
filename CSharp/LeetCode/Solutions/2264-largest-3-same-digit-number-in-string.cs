b0bb4d50a94b9262858665ea6af05191c040c762
Guillaume Desktop <robertohelper@outlook.com>
2025-08-15 00:56:31 +0200

    feature: added 2264 cs

---
+++ CSharp/LeetCode/Solutions/2264-largest-3-same-digit-number-in-string.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution2264 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public string LargestGoodInteger(string num)
+    {
+        int n = num.Length;
+        char res = '\0';
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            res = num[i] == num[i + 1] && num[i] == num[i + 2] && res < num[i]
+                ? num[i]
+                : res;
+        }
+
+        return res.Equals('\0') ? string.Empty : new string(res, 3);
+    }
+}
