3ba88cc2760adf1ad2070e5ddfc4c636dcdaa07e
Guillaume Desktop <robertohelper@outlook.com>
2025-09-12 16:02:33 +0200

    feature: added 3227 cs

---
+++ CSharp/LeetCode/Solutions/3227-vowels-game-in-a-string.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3227 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public bool DoesAliceWin(string s)
+    {
+        static bool is_vowel(char ch)
+            => ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+            || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+
+        foreach (char ch in s)
+        {
+            if (is_vowel(ch))
+                return true;
+        }
+
+        return false;
+    }
+}
