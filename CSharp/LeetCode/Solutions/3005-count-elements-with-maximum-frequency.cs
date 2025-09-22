72e0bd214fb2732a944f83017acaf4d5fff6a68a
Guillaume Laptop <robertohelper@outlook.com>
2025-09-22 10:53:04 +0200

    feature: added 3005 cs

---
+++ CSharp/LeetCode/Solutions/3005-count-elements-with-maximum-frequency.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+
+internal class Solution3005 : IRunProgram
+{
+    public void Run()
+    {
+        
+    }
+
+    public int MaxFrequencyElements(int[] nums) 
+	{
+        Span<int> freq = stackalloc int[101];
+        int max = 0, res = 0;
+        foreach (int num in nums)
+        {
+            max = Math.Max(max, ++freq[num]);
+        }
+        for (int i = 0; i < freq.Length; i++)
+        {
+            if (freq[i] == max)
+                res += max;
+        }
+        return res;
+    }
+}
