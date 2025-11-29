c1853d98a42923e0bd520f0835febf3f211d24d2
Guillaume Desktop <robertohelper@outlook.com>
2025-11-29 14:46:15 +0100

    feature: added 3512 cs

---
+++ CSharp/LeetCode/Solutions/3512-minimum-operations-to-make-array-sum-divisible-by-k.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3512 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MinOperations(int[] nums, int k)
+    {
+        return nums.Sum() % k;
+    }
+}
