3415e983c274207de6477e0e344500cc9f3a089e
Guillaume Desktop <robertohelper@outlook.com>
2025-12-07 19:56:58 +0100

    feature: added 1641 cs

---
+++ CSharp/LeetCode/Solutions/1641-count-sorted-vowel-strings.cs
@@ -0,0 +1,52 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution1641 : IRunProgram
+{
+    public void Run()
+    {
+        CountVowelStrings(1).Print();
+        CountVowelStrings(2).Print();
+    }
+
+    public int CountVowelStrings(int n)
+    {
+        return rec(n);
+    }
+    private static int rec(int n)
+    {
+        const int MAX = 5;
+        Dictionary<(int, int), int> memo = [];
+
+        int inner(int curr, int idx)
+        {
+            if (idx >= MAX)
+            {
+                return 0;
+            }
+
+            var key = (curr, idx);
+            if (memo.TryGetValue(key, out int value))
+            {
+                return value;
+            }
+
+            if (n == curr)
+            {
+                memo[key] = 1;
+                return 1;
+            }
+
+            var res = 0;
+
+            for (int i = 0; i < MAX; i++)
+                res += inner(curr + 1, idx + i);
+
+            memo[key] = res;
+
+            return res;
+        }
+
+        return inner(0, 0);
+    }
+}
