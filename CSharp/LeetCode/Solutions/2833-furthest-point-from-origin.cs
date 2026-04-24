0b6d8a70b545e6b73f4f24bc74ea05de3599cb6f
Guillaume Desktop <robertohelper@outlook.com>
2026-04-24 16:09:26 +0200

    feature: added 2833 cs

---
+++ CSharp/LeetCode/Solutions/2833-furthest-point-from-origin.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution2833 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int FurthestDistanceFromOrigin(string moves)
+    {
+        int ans = 0, offset = 0;
+
+        foreach (char ch in moves)
+        {
+            if (ch == 'L')
+            {
+                ans--;
+            }
+            else if (ch == 'R')
+            {
+                ans++;
+            }
+            else
+            {
+                offset++;
+            }
+        }
+
+        return Math.Abs(ans) + offset;
+    }
+}
