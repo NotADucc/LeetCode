354503979d3a746bd0fe4825391284809eaca57b
Guillaume Desktop <robertohelper@outlook.com>
2026-05-15 22:48:36 +0200

    feature: added 0153 cs

---
+++ CSharp/LeetCode/Solutions/0153-find-minimum-in-rotated-sorted-array.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution0153 : IRunProgram
+{
+    public void Run()
+    {
+        FindMin([3, 4, 5, 1, 2]).Print();
+        FindMin([4, 5, 6, 7, 0, 1, 2]).Print();
+        FindMin([11, 13, 15, 17]).Print();
+        FindMin([3, 1, 2]).Print();
+    }
+
+    public int FindMin(int[] nums)
+    {
+        if (nums[0] <= nums[^1])
+            return nums[0];
+
+        int res = int.MaxValue;
+        int l = 0, r = nums.Length - 1;
+        while (l <= r)
+        {
+            int middle = (l + r) >> 1;
+            res = Math.Min(res, nums[middle]);
+            if (nums[middle] >= nums[^1])
+            {
+                l = middle + 1;
+            }
+            else
+            {
+                r = middle - 1;
+            }
+        }
+
+        return res;
+    }
+}
