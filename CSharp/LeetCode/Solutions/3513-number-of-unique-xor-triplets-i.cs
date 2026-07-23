9a1da6104416d6ffde846cdebf1b39a8694114a1
Guillaume Desktop <robertohelper@outlook.com>
2026-07-23 22:42:47 +0200

    feature: added 3513 lc (noticed pattern messing with solution explorer)

---
+++ CSharp/LeetCode/Solutions/3513-number-of-unique-xor-triplets-i.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3513 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int UniqueXorTriplets(int[] nums)
+    {
+        int n = nums.Length;
+        if (n <= 2) return n;
+
+        return 1 << Convert.ToString(n, 2).Length;
+    }
+}
