07dd1f836c56d45a6f8af4bd02d2b8145eb24210
Guillaume Desktop <robertohelper@outlook.com>
2026-07-28 23:55:18 +0200

    refactor: 3517 with 1 sb

--- CSharp/LeetCode/Solutions/3517-smallest-palindromic-rearrangement-i.cs
+++ CSharp/LeetCode/Solutions/3517-smallest-palindromic-rearrangement-i.cs
@@ -12,6 +12,41 @@ internal class Solution3517 : IRunProgram
     public string SmallestPalindrome(string s)
     {
         StringBuilder sb = new StringBuilder();
+        int sb_idx = 0;
+        char pivot = '\0';
+        Span<int> freq = stackalloc int[26];
+
+        foreach (char ch in s)
+            freq[ch - 'a']++;
+
+        for (int i = 0; i < freq.Length; i++)
+        {
+            int cnt = freq[i];
+            if (cnt <= 0)
+                continue;
+
+            char ch = (char)('a' + i);
+
+            if ((cnt & 1) == 1)
+            {
+                pivot = ch;
+                cnt--;
+            }
+            int half = cnt >> 1;
+
+            sb.Insert(sb_idx, new string(ch, cnt));
+            sb_idx += half;
+        }
+
+        if (pivot != '\0')
+            sb.Insert(sb.Length >> 1, pivot);
+
+        return sb.ToString();
+    }
+
+    public string SmallestPalindromeDoubleSB(string s)
+    {
+        StringBuilder sb = new StringBuilder();
         char pivot = '\0';
         Span<int> freq = stackalloc int[26];
 
@@ -34,12 +69,13 @@ internal class Solution3517 : IRunProgram
             }
         }
 
-        if (pivot != '\0')
-            sb.Append(pivot);
-
         var copy_reverse = new StringBuilder(
             new string(sb.ToString().Reverse().ToArray())
         );
+
+        if (pivot != '\0')
+            sb.Append(pivot);
+
         sb.Append(copy_reverse);
 
         return sb.ToString();
