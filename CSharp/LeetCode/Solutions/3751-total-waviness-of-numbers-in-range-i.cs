f70d45d5e5cc964c2d1a9524352fc0b5c9ed0fb9
Guillaume Desktop <robertohelper@outlook.com>
2026-07-30 21:42:58 +0200

    refactor: cleaned up 3751 cs

--- CSharp/LeetCode/Solutions/3751-total-waviness-of-numbers-in-range-i.cs
+++ CSharp/LeetCode/Solutions/3751-total-waviness-of-numbers-in-range-i.cs
@@ -10,8 +10,7 @@ internal class Solution3751 : IRunProgram
     public int TotalWaviness(int num1, int num2)
     {
         int ans = 0;
-        num1 = Math.Max(num1, 100);
-        for (int i = num1; i <= num2; i++)
+        for (int i = Math.Max(num1, 100); i <= num2; i++)
         {
             string s = i.ToString();
             for (int j = 1; j < s.Length - 1; j++)
