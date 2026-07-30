a6caabfd599fda81bf74301a0eae4650ba86cfe3
Guillaume Desktop <robertohelper@outlook.com>
2026-07-30 19:45:37 +0200

    feature: added 3760 cs

---
+++ CSharp/LeetCode/Solutions/0559-maximum-depth-of-n-ary-tree.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution0559 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MaxDepth(Node root)
+    {
+        if (root is null)
+            return 0;
+
+        int d = 0;
+        foreach (var c in root.children)
+            d = Math.Max(d, MaxDepth(c));
+
+        return 1 + d;
+    }
+}
---
+++ CSharp/LeetCode/Solutions/3760-maximum-substrings-with-distinct-start.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3760 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MaxDistinct(string s)
+    {
+        return new HashSet<char>(s).Count;
+    }
+}
