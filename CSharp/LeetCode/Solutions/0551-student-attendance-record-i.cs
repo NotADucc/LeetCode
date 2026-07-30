96dae0963b74307cd77e4909b4ff7fad7cba7e5f
Guillaume Desktop <robertohelper@outlook.com>
2026-07-30 17:54:44 +0200

    feature: added 0551 cs

---
+++ CSharp/LeetCode/Solutions/0551-student-attendance-record-i.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution0551 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public bool CheckRecord(string s)
+    {
+        int a = 0, n = s.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            char ch = s[i];
+            if (ch == 'A') a++;
+            else if (ch == 'P') continue;
+            else if (i + 2 < n && s[i + 1] == 'L' && s[i + 2] == 'L') return false;
+        }
+
+        return a < 2;
+    }
+}
