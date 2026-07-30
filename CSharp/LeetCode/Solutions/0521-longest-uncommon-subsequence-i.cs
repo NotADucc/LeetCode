79a7186a8f84a95d5dc85ca1f93db6b391608171
Guillaume Desktop <robertohelper@outlook.com>
2026-07-30 17:45:56 +0200

    feature: added 0521 cs

---
+++ CSharp/LeetCode/Solutions/0521-longest-uncommon-subsequence-i.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution0521 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int FindLUSlength(string a, string b)
+    {
+        return a == b ?
+            -1 : Math.Max(a.Length, b.Length);
+    }
+}
