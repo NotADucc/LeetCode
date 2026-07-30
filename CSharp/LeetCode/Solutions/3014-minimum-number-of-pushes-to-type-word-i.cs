b6b2f0cb9a025eed51d7debd02a26fab296dc330
Guillaume Desktop <robertohelper@outlook.com>
2026-07-30 15:42:49 +0200

    feature: added 3014 cs

---
+++ CSharp/LeetCode/Solutions/3014-minimum-number-of-pushes-to-type-word-i.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3014 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int MinimumPushes(string word)
+    {
+        int ans = 0, n = word.Length;
+        for (int i = 0; i < n; i++)
+        {
+            ans += (i / 8 + 1);
+        }
+        return ans;
+    }
+}
