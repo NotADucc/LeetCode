60910fbd3603bc436eaf47a598f7f8a8285c4084
Guillaume Desktop <robertohelper@outlook.com>
2026-07-30 17:16:36 +0200

    feature: added 0504 cs

---
+++ CSharp/LeetCode/Solutions/0504-base-7.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution0504 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public string ConvertToBase7(int num)
+    {
+        if (num == 0)
+            return "0";
+
+        string ans = string.Empty;
+        bool is_negative = num < 0;
+        num = Math.Abs(num);
+
+        while (num != 0)
+        {
+            ans = $"{num % 7}{ans}";
+            num /= 7;
+        }
+
+        if (is_negative)
+            ans = $"-{ans}";
+
+        return ans;
+    }
+}
