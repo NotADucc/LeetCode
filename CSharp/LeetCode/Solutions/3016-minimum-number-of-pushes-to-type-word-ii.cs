b38f4e5248356d4eab0679318e886e679814675d
Guillaume Desktop <robertohelper@outlook.com>
2026-07-31 16:15:48 +0200

    feature: solved 3016 cs, initially was solution for 3014

---
+++ CSharp/LeetCode/Solutions/3016-minimum-number-of-pushes-to-type-word-ii.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3016 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MinimumPushes(string word)
+    {
+        Span<int> freq = stackalloc int[26];
+        foreach (char ch in word)
+            freq[ch - 'a']++;
+
+        MemoryExtensions.Sort(freq);
+
+        int ans = 0, n = freq.Length;
+        for (int i = 0; i < n; i++)
+        {
+            int cnt = freq[n - 1 - i];
+            ans += (i / 8 + 1) * cnt;
+        }
+
+        return ans;
+    }
+}
