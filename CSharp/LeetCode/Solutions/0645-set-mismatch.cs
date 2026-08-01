bd0196bcd26a9bacd075afb879759b893081afe3
Guillaume Desktop <robertohelper@outlook.com>
2026-08-01 23:37:36 +0200

    urefactor: cleaned up 0645 cs

--- CSharp/LeetCode/Solutions/0645-set-mismatch.cs
+++ CSharp/LeetCode/Solutions/0645-set-mismatch.cs
@@ -10,11 +10,16 @@ internal class Solution0645 : IRunProgram
 
     public int[] FindErrorNums(int[] nums)
     {
+        HashSet<int> set = new HashSet<int>();
+        int sum = 0, sum_no_duplicate = 0;
+        foreach (var num in nums)
+        {
+            sum += num;
+            if (set.Add(num))
+                sum_no_duplicate += num;
+        }
+
         int n = nums.Length;
-        int sum = nums.Sum();
-        int sum_no_duplicate = nums
-            .Distinct()
-            .Sum();
         int triangle = n * (n + 1) / 2;
 
         return [sum - sum_no_duplicate, triangle - sum_no_duplicate];
