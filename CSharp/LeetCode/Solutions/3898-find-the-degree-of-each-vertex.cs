86588c54272ebc048147a75e250b2b4a34ff1c67
Guillaume Desktop <robertohelper@outlook.com>
2026-08-01 15:26:27 +0200

    changes

---
+++ CSharp/LeetCode/Solutions/3898-find-the-degree-of-each-vertex.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3898 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int[] FindDegrees(int[][] matrix)
+    {
+        int n = matrix.Length, m = matrix[0].Length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                ans[i] += matrix[i][j];
+            }
+        }
+        return ans;
+    }
+}
