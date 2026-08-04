bc0de9a421137f5290d03031947aed322c902a2e
Guillaume Desktop <robertohelper@outlook.com>
2026-08-04 03:35:47 +0200

    feature: added 3731 cs

---
+++ CSharp/LeetCode/Solutions/3731-find-missing-elements.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3731 : IRunProgram
+{
+    public void Run()
+    {
+        FindMissingElements([1, 4, 2, 5]).Print();
+        FindMissingElements([7, 8, 6, 9]).Print();
+        FindMissingElements([5, 1]).Print();
+
+    }
+    public IList<int> FindMissingElements(int[] nums)
+    {
+        List<int> ans = new List<int>();
+        Array.Sort(nums);
+        int min = nums[0], max = nums[nums.Length - 1], idx = 0;
+
+        for (int i = min; i < max; i++)
+        {
+            int num = nums[idx];
+            if (num > i)
+                ans.Add(i);
+            else
+                idx++;
+        }
+
+        return ans;
+    }
+}
