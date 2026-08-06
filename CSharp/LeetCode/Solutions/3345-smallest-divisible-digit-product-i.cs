bab141f4dce36ceab80f196381f1e7954ca3d1a4
Guillaume Desktop <robertohelper@outlook.com>
2026-08-06 17:09:34 +0200

    feature: added 3345 cs

---
+++ CSharp/LeetCode/Solutions/3345-smallest-divisible-digit-product-i.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3345 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int SmallestNumber(int n, int t)
+    {
+        for (int i = n;; i++)
+        {
+            int d = 1, dd = i;
+            while (dd > 0)
+            {
+                d *= dd % 10;
+                dd /= 10;
+            }
+
+            if (d % t == 0)
+                return i;
+        }
+    }
+}
