0a1ccd7d63c724d7d36c3862f78aa333111c2b8b
Guillaume Desktop <robertohelper@outlook.com>
2026-08-22 15:59:06 +0200

    feature: added 3622 cs

---
+++ CSharp/LeetCode/Solutions/3622-check-divisibility-by-digit-sum-and-product.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3622 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public bool CheckDivisibility(int n)
+    {
+        int prod = 1, sum = 0, nn = n;
+
+        while (nn > 0)
+        {
+            int rest = nn % 10;
+            prod *= rest;
+            sum += rest;
+            nn /= 10;
+        }
+
+        return n % (prod + sum) == 0;
+    }
+}
