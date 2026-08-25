222216e6458915749ba2a7b1b7e1daa3af6e8432
Guillaume Desktop <robertohelper@outlook.com>
2026-08-26 00:09:39 +0200

    feature: added 3718 cs

---
+++ CSharp/LeetCode/Solutions/3718-smallest-missing-multiple-of-k.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3718 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int MissingMultiple(int[] nums, int k)
+    {
+        HashSet<int> set = new HashSet<int>(nums);
+
+        for (int i = 1; ; i++)
+        {
+            int num = k * i;
+            if (!set.Contains(num))
+                return num;
+        }
+
+        return -1;
+    }
+}
