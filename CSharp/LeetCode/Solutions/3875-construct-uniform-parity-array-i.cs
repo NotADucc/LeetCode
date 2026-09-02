24c3e2625bc237aa32d9ce1dc12b0260fd2ecefc
Guillaume Desktop <robertohelper@outlook.com>
2026-09-02 14:52:19 +0200

    feature: added 3875 cs

---
+++ CSharp/LeetCode/Solutions/3875-construct-uniform-parity-array-i.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3875 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public bool UniformArray(int[] nums1)
+    {
+        return true;
+    }
+}
