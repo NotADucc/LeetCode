e59a2b7cd14e71623fc460fd4c4610f41fa6c122
Guillaume Desktop <robertohelper@outlook.com>
2026-09-08 22:49:29 +0200

    feature: added 3870 cs

---
+++ CSharp/LeetCode/Solutions/3870-count-commas-in-range.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Solutions.Shared;
+
+namespace LeetCode.Solutions;
+internal class Solution3870 : IRunProgram
+{
+    public void Run()
+    {
+
+    }
+    public int CountCommas(int n)
+        => Math.Max(n - 999, 0);
+}
